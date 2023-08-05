af049563b8d72f971ff87afa72ba4b44515cf7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:21 +0000

    Add package textcl/0.1.2/textcl-0.1.2.tar.gz

---
+++ packages/textcl/textcl-0.1.2.tar.gz/textcl-0.1.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+f=open('requirements.txt')
+req = []
+for line in f:
+    req.append(line.strip())
+f.close()
+
+setup(name='textcl',
+      packages=['textcl'],
+      version='0.1.2',
+      license='MIT',
+      description='Text preprocessing package for use in NLP tasks',
+      author = 'Alina Petukhova',
+      author_email = 'petukhova.alina@gmail.com',
+      url = 'https://github.com/alinapetukhova/textcl',
+      download_url = 'https://github.com/alinapetukhova/textcl/archive/refs/tags/v.0.1.0.tar.gz',
+      keywords = ['NLP', 'Text preprocessing', 'Outlier detection'],
+      install_requires=req,
+      python_requires='>=3.6', 
+      zip_safe=False,
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8'
+      ])
