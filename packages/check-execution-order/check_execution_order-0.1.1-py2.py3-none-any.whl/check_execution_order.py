91c3ac25f1cfa1773f68b40ef08e994083c68bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:13 +0000

    Add package check-execution-order/0.1.1/check_execution_order-0.1.1-py2.py3-none-any.whl

---
+++ packages/check-execution-order/check_execution_order-0.1.1-py2.py3-none-any.whl/check_execution_order.py
@@ -0,0 +1,50 @@
+import argparse
+import json
+import sys
+from typing import Any
+from typing import Mapping
+from typing import Optional
+from typing import Sequence
+
+
+def check_execution_order(
+    content: Mapping[str, Any],
+    file_name: str,
+) -> int:
+    execution_count = 0
+    code_cells = content['cells']
+
+    ret = 0
+    for cell in code_cells:
+        if cell['cell_type'] != 'code':
+            continue
+        new_execution_count = cell['execution_count']
+        if (
+            new_execution_count is not None
+            and not new_execution_count > execution_count
+        ):
+            sys.stdout.write(
+                f'Cell {new_execution_count} comes after '
+                f'{execution_count} in file \'{file_name}\'\n',
+            )
+            ret = 1
+        if new_execution_count is not None:
+            execution_count = new_execution_count
+    return ret
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('files', nargs='*')
+    args = parser.parse_args(argv)
+
+    ret = 0
+    for file in args.files:
+        with open(file) as fd:
+            content = json.load(fd)
+        ret = ret or check_execution_order(content, file)
+    return ret
+
+
+if __name__ == '__main__':
+    exit(main())
