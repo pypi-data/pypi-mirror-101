3ebe6405ae4f218b201b6fef6e68fc7a55140339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:29 +0000

    Add package reader-rbv/0.5.0/reader_rbv-0.5.0-py3-none-any.whl

---
+++ packages/reader-rbv/reader_rbv-0.5.0-py3-none-any.whl/reader_rbv/reader.py
@@ -0,0 +1,71 @@
+import logging
+
+from cachetools import cachedmethod
+from operator import attrgetter
+from requests import Session
+from typing import Mapping, MutableMapping, Optional
+
+from reader_rbv.exception import BookNotFound
+from . import Book, BookCache
+from .constants import HEADERS
+
+
+class Reader(Mapping[str, Book]):
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        cache: Optional[MutableMapping[str, Book]] = BookCache(10, 600),
+        base: str = "http://www.pustaka.ut.ac.id/reader/",
+        session: Session = Session(),
+    ):
+        self.username = username
+        self.password = password
+        self.session = session
+        self.base = base
+        self.logger = logging.getLogger(self.__class__.__qualname__)
+        self.cache = cache
+        if cache is None:
+            self.logger.debug(".cache disabled")
+        self.session.headers.update(HEADERS)
+
+    @cachedmethod(attrgetter("cache"))
+    def get_buku(self, code: str) -> Book:
+        if self.cache is not None:
+            cached_book = Book.from_cache(
+                code=code,
+                username=self.username,
+                password=self.password,
+                base=self.base,
+                session=self.session,
+            )
+            if cached_book is not None:
+                return cached_book
+        params = {"modul": code}
+        self.logger.debug(f"Finding book with code = {code}")
+        res = self.session.get(self.base, params=params)
+        if not res.ok or not res.text:
+            raise BookNotFound(f"Unable to find book with code = {code}")
+        else:
+            self.logger.debug(f"Found book with code = {code}")
+        return Book(
+            code=code,
+            base=self.base,
+            session=self.session,
+            username=self.username,
+            password=self.password,
+        )
+
+    def __del__(self):
+        if self.cache:
+            while self.cache:
+                self.cache.popitem()
+
+    def __getitem__(self, key: str) -> Book:
+        return self.get_buku(key)
+
+    def __len__(self):
+        return len(self.cache)
+
+    def __iter__(self):
+        return iter(self.cache)
