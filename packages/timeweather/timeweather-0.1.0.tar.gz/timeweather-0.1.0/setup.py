93433f8db0dcb41d96911545408a2858c0fd7caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:17 +0000

    Add package timeweather/0.1.0/timeweather-0.1.0.tar.gz

---
+++ packages/timeweather/timeweather-0.1.0.tar.gz/timeweather-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='timeweather',
+      version='0.1.0',
+      description='Weather API Lib',
+      long_description=open('readme.md',encoding="utf-8").read(),
+      long_description_content_type="text/markdown",
+      author='TimeStudio',
+      author_email='sjgzszg@163.com',
+      license='MIT',
+      packages=['timeweather'])
