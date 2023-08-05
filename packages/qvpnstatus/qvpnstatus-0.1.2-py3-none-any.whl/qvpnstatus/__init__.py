2fc6ddfb22babefe1f9bf5af8b301b070d357e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:50 +0000

    Add package qvpnstatus/0.1.2/qvpnstatus-0.1.2-py3-none-any.whl

---
+++ packages/qvpnstatus/qvpnstatus-0.1.2-py3-none-any.whl/qvpnstatus/__init__.py
@@ -0,0 +1,9 @@
+"""
+QVpnStatus package.
+
+VPN Status tray icon for monitoring VPN connection
+"""
+
+from typing import List
+
+__all__: List[str] = []  # noqa: WPS410 (the only __variable__ we use)
