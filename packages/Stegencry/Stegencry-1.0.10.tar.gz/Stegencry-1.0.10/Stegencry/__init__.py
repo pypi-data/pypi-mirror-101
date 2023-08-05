b169670a5a55366699478b6f79eee08c793da239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:15 +0000

    Add package Stegencry/1.0.10/Stegencry-1.0.10.tar.gz

---
+++ packages/Stegencry/Stegencry-1.0.10.tar.gz/Stegencry-1.0.10/Stegencry/__init__.py
@@ -0,0 +1,12 @@
+#╔════════╦═══════════════════════╦════════╗
+#║        ║       Stegencry       ║        ║
+#║   ___  ╚═══════════════════════╝  ___   ║
+#║  (o o)                           (o o)  ║
+#║ (  V  )  Julien Calenge © 2021  (  V  ) ║
+#╠═══m═m═════════════════════════════m═m═══╣
+#╠════ File name: __init__.py              ║
+#╠═══ Description: Not Relevant            ║
+#╚═════════════════════════════════════════╝
+
+from Stegencry.decrypt import decrypt
+from Stegencry.encrypt import encrypt
