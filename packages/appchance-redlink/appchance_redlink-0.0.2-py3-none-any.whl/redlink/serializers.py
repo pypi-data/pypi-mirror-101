89d563d78d9477e9e473111b3588851877d2ceab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:15 +0000

    Add package appchance-redlink/0.0.2/appchance_redlink-0.0.2-py3-none-any.whl

---
+++ packages/appchance-redlink/appchance_redlink-0.0.2-py3-none-any.whl/redlink/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+from redlink.models import RedlinkDevice
+
+
+class RedlinkDeviceSerializer(ModelSerializer):
+    created = serializers.DateTimeField(required=False)
+
+    class Meta:
+        model = RedlinkDevice
+        fields = "__all__"
+        extra_kwargs = {"id": {"read_only": True, "required": False}}
+
+    def validate(self, attrs):
+        self.Meta.model.objects.filter()
+        return attrs
+
+
+class RedlinkCreateDeviceSerializer(ModelSerializer):
+    class Meta:
+        model = RedlinkDevice
+        fields = (
+            "registration_id",
+            "name",
+            "active",
+            "type",
+        )
