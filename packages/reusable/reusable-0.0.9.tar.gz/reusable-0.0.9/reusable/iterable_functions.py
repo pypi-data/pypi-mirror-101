9235154cd8504423de8c5f0d2ff7e847dd2c2bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:39 +0000

    Add package reusable/0.0.9/reusable-0.0.9.tar.gz

---
+++ packages/reusable/reusable-0.0.9.tar.gz/reusable-0.0.9/reusable/iterable_functions.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# Part of reusable package
+#
+# Copyright (c) 2020 - Ohidur Rahman Bappy - MIT License
+"""Iterable Functions.
+
+Contains a handful of iterable functions.
+"""
+
+
+from collections import defaultdict
+
+
+def groupby_count(i, key=None, force_keys=None):
+    """Group a list of elements as per the count
+
+    Usage::
+
+        >>> from reusable.functions import groupby_count
+        >>> print(groupby_count([1,1,1,2,3]))
+        [(1,3),(2,1),(3,1)]
+    """
+    counter = defaultdict(lambda: 0)
+    if not key:
+        def key(o): return o
+
+    for k in i:
+        counter[key(k)] += 1
+
+    if force_keys:
+        for k in force_keys:
+            counter[k] += 0
+
+    return counter.items()
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst.
+    
+    Usage::
+
+        >>> from reusable.functions import chunks
+        >>> li=[1,3,5,6,8]
+        >>> for g in chunks(li,2):
+        ...     print(g)
+    """
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
