220fc3b69e3cb00cd2826045c545bf20d8b65f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:38 +0000

    Add package tmsg-redis/3.9.9/tmsg-redis-3.9.9.tar.gz

---
+++ packages/tmsg-redis/tmsg-redis-3.9.9.tar.gz/tmsg-redis-3.9.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='tmsg-redis',
+
+    version='3.9.9',
+
+    description='A utility function for the coordinates of the Southern African Large Telescope',
+
+    py_modules=['tmsg-redis']
+)
