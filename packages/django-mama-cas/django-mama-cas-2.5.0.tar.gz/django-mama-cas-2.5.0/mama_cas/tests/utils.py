f72511296bdd465729f900e4c0d35b00389d4b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:19 +0000

    Add package django-mama-cas/2.5.0/django-mama-cas-2.5.0.tar.gz

---
+++ packages/django-mama-cas/django-mama-cas-2.5.0.tar.gz/django-mama-cas-2.5.0/mama_cas/tests/utils.py
@@ -0,0 +1,23 @@
+import re
+from urllib.parse import urlencode
+
+from django.urls import reverse
+
+from mama_cas.compat import etree
+
+
+def parse(s):
+    """
+    Parse an XML tree from the given string, removing all
+    of the included namespace strings.
+    """
+    ns = re.compile(r'^{.*?}')
+    et = etree.fromstring(s)
+    for elem in et.iter():
+        elem.tag = ns.sub('', elem.tag)
+    return et
+
+
+def build_url(name, **kwargs):
+    """Build a URL given a view name and kwarg query parameters."""
+    return reverse(name) + '?' + urlencode(kwargs)
---
+++ packages/django-mama-cas/django-mama-cas-2.5.0.tar.gz/django-mama-cas-2.5.0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages
+from setuptools import setup
+
+from mama_cas import __version__ as version
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='django-mama-cas',
+    version=version,
+    description='A Django Central Authentication Service (CAS) single sign-on server',
+    long_description=readme,
+    license='BSD',
+    author='Jason Bittel',
+    author_email='jason.bittel@gmail.com',
+    url='https://github.com/jbittel/django-mama-cas',
+    download_url='https://pypi.python.org/pypi/django-mama-cas/',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['requests>=2.0.0,<3.0.0'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+    ],
+    keywords=['auth', 'authentication', 'cas', 'django', 'single sign-on', 'sso'],
+)
