5c8cb552e47942e13044fcc533b49491f4f16f06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:49 +0000

    Add package bentley-ottmann/2.0.2/bentley_ottmann-2.0.2.tar.gz

---
+++ packages/bentley-ottmann/bentley_ottmann-2.0.2.tar.gz/bentley_ottmann-2.0.2/bentley_ottmann/core/base.py
@@ -0,0 +1,94 @@
+from typing import (Iterable,
+                    List,
+                    Optional,
+                    Sequence)
+
+from ground.base import (Context,
+                         Relation)
+from ground.hints import (Point,
+                          Segment)
+
+from .event import (Event,
+                    LeftEvent)
+from .events_queue import EventsQueue
+from .sweep_line import SweepLine
+from .utils import classify_overlap
+
+
+def sweep(segments: Sequence[Segment],
+          *,
+          context: Context) -> Iterable[LeftEvent]:
+    events_queue = EventsQueue.from_segments(segments,
+                                             context=context)
+    sweep_line = SweepLine(context)
+    start = (events_queue.peek().start
+             if events_queue
+             else None)  # type: Optional[Point]
+    same_start_events = []  # type: List[Event]
+    while events_queue:
+        event = events_queue.pop()
+        if event.start == start:
+            same_start_events.append(event)
+        else:
+            yield from complete_events_relations(same_start_events)
+            same_start_events, start = [event], event.start
+        if event.is_left:
+            equal_segment_event = sweep_line.find_equal(event)
+            if equal_segment_event is None:
+                sweep_line.add(event)
+                below_event = sweep_line.below(event)
+                if below_event is not None:
+                    events_queue.detect_intersection(below_event, event,
+                                                     sweep_line)
+                above_event = sweep_line.above(event)
+                if above_event is not None:
+                    events_queue.detect_intersection(event, above_event,
+                                                     sweep_line)
+            else:
+                # found equal segments' fragments
+                equal_segment_event.merge_with(event)
+        else:
+            event = event.left
+            equal_segment_event = sweep_line.find_equal(event)
+            if equal_segment_event is not None:
+                above_event, below_event = (
+                    sweep_line.above(equal_segment_event),
+                    sweep_line.below(equal_segment_event))
+                sweep_line.remove(equal_segment_event)
+                if below_event is not None and above_event is not None:
+                    events_queue.detect_intersection(below_event, above_event,
+                                                     sweep_line)
+                if event is not equal_segment_event:
+                    equal_segment_event.merge_with(event)
+    yield from complete_events_relations(same_start_events)
+
+
+def complete_events_relations(same_start_events: Sequence[Event]
+                              ) -> Iterable[Event]:
+    for offset, first in enumerate(same_start_events,
+                                   start=1):
+        first_left = first if first.is_left else first.left
+        first_ids = first_left.segments_ids
+        for second_index in range(offset, len(same_start_events)):
+            second = same_start_events[second_index]
+            second_left = second if second.is_left else second.left
+            second_ids = second_left.segments_ids
+            segments_touch = (len(first_ids - second_ids)
+                              * len(second_ids - first_ids)) >= 1
+            if segments_touch:
+                intersection_point = first.start
+                relation = (Relation.TOUCH
+                            if (intersection_point == first.original_start
+                                or intersection_point == second.original_start)
+                            else Relation.CROSS)
+                first.register_tangent(second)
+                second.register_tangent(first)
+            else:
+                relation = classify_overlap(first_left.original_start,
+                                            first_left.original_end,
+                                            second_left.original_start,
+                                            second_left.original_end)
+            first_left.register_relation(relation)
+            second_left.register_relation(relation.complement)
+        if not first.is_left:
+            yield first_left
