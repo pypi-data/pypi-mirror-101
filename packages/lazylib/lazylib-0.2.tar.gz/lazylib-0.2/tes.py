763bedc0151f1f5e8c49623eeac851a69dd07d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:18 +0000

    Add package lazylib/0.2/lazylib-0.2.tar.gz

---
+++ packages/lazylib/lazylib-0.2.tar.gz/lazylib-0.2/tes.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(name='lazylib',
+                 version='0.2',
+                 author="Mikhail Pozdnyakov",
+                 scripts=['lazylib'],
+                 author_email="pozdnyakoffblog@gmail.com",
+                 description="util for build apps architecture.")
