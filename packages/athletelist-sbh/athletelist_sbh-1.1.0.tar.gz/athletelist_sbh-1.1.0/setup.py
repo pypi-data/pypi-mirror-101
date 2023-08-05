416ebf731c3bb82dc4c459d2943ed6bb8066e359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:54 +0000

    Add package athletelist-sbh/1.1.0/athletelist_sbh-1.1.0.tar.gz

---
+++ packages/athletelist-sbh/athletelist_sbh-1.1.0.tar.gz/athletelist_sbh-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+    name = 'athletelist_sbh',
+    version = '1.1.0',
+    py_modules = ['athletelist_sbh'],
+    author = 'sourav',
+    author_email = 'bhsourav17@gmail.com',
+    description = 'Athlete list class'
+    )
