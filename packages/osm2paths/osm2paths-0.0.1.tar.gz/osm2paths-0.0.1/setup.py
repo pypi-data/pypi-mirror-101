7f9791a2ed00755124c5d50d5ad050c1256ae841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:24 +0000

    Add package osm2paths/0.0.1/osm2paths-0.0.1.tar.gz

---
+++ packages/osm2paths/osm2paths-0.0.1.tar.gz/osm2paths-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 3 - Alpha',
+  'Intended Audience :: Developers',
+  'Operating System :: OS Independent',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='osm2paths',
+  version='0.0.1',
+  description='An automatic generator of JSON vector road network graph car tracks from OSM',
+  url='https://github.com/MikeNezumi/osm2tracks',
+  author='MikeFreeman',
+  author_email='michaelsvoboda42@gmail.com',
+  license='MIT',
+  classifiers=classifiers,
+  keywords='json, traffic, simulation, data graphs, osm',
+  package_dir={'':'src'},
+  py_modules=["osm2tracks"],
+  install_requires=['utm', 'json', 'pyglet', 'subprocess', 'pythematics']
+)
