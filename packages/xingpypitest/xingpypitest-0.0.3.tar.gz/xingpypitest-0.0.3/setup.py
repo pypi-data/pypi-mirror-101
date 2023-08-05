0ed4b631a8c848cb691484a02d5efd4abad39bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:21 +0000

    Add package xingpypitest/0.0.3/xingpypitest-0.0.3.tar.gz

---
+++ packages/xingpypitest/xingpypitest-0.0.3.tar.gz/xingpypitest-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open('xingpypitest/README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+print(setuptools.find_packages())
+
+
+setuptools.setup(
+    name='xingpypitest',
+    version='0.0.3',
+    author='XING-ZIWEN',
+    author_email='Rick.Xing@Nachmath.com',
+    description='The Short Description',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='http://www.baidu.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+    ],
+    python_requires='>=3.6',
+
+    packages=setuptools.find_packages(),
+
+)
