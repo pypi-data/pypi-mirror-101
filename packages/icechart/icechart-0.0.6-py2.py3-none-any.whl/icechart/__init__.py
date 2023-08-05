e681625ef5fdeccc16dd3b9ca39a8d0e76130594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:43 +0000

    Add package icechart/0.0.6/icechart-0.0.6-py2.py3-none-any.whl

---
+++ packages/icechart/icechart-0.0.6-py2.py3-none-any.whl/icechart/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for IceChart."""
+
+__author__ = """Naheem Adebisi"""
+__email__ = 'adebisi.naheem@gmail.com'
+__version__ = '0.0.6'
+
+from .icechart import *
+from .utils import *
