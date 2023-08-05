55760a8fecd5418c66f31e8a995091ddad529ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:48 +0000

    Add package romullo-distribution/1.0/romullo_distribution-1.0.tar.gz

---
+++ packages/romullo-distribution/romullo_distribution-1.0.tar.gz/romullo_distribution-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='romullo_distribution',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['romullo_distribution'],
+      author = 'Romullo Ferreira',
+      author_email = 'romulloferreira@gmail.com',
+      zip_safe= False)
