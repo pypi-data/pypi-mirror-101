9fa53351354d64f39d9ca6772dc8956629fe33dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:05 +0000

    Add package hydradb/2.4.2/hydradb-2.4.2.tar.gz

---
+++ packages/hydradb/hydradb-2.4.2.tar.gz/hydradb-2.4.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+with open("README.txt", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='hydradb',
+    version='2.4.2',
+    packages=['hydra'],
+    url='https://github.com/rhmtazad/hydraDB',
+    license='MIT License',
+    classifiers=classifiers,
+    author='Rahmat Azad',
+    author_email='rhmtazad@gmail.com',
+    description='An API for faster and easier SQLite/Python operations',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['']
+)
