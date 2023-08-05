93d2ab5c189a344a28ad7ef95561851cd552b5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:39 +0000

    Add package PriceHero-mehtaarn000/0.5.2/PriceHero_mehtaarn000-0.5.2-py3-none-any.whl

---
+++ packages/PriceHero-mehtaarn000/PriceHero_mehtaarn000-0.5.2-py3-none-any.whl/PriceHero/_amazon.py
@@ -0,0 +1,31 @@
+from requests import get
+from selectorlib import Extractor
+
+def _amazon(amazon_url):
+    extractor = Extractor.from_yaml_string("""
+    name:
+        css: 'h1.a-size-large span.a-size-large'
+        xpath: null
+        type: Text
+    price:
+        css: 'div.a-section.a-spacing-micro span.a-size-medium'
+        xpath: null
+        type: Text
+    """)   
+    headers = {
+        'authority': 'www.amazon.com',
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'none',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-dest': 'document',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8'}
+
+    website = get(amazon_url, headers=headers)
+
+    productdata = extractor.extract(website.text)
+    return productdata
