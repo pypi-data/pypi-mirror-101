2f1bdd2c713de0430930be3b7e799d768efbb01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:55 +0000

    Add package sas2nb/0.1.0/sas2nb-0.1.0.tar.gz

---
+++ packages/sas2nb/sas2nb-0.1.0.tar.gz/sas2nb-0.1.0/src/iconImport.ts
@@ -0,0 +1,8 @@
+import { LabIcon } from '@jupyterlab/ui-components';
+
+import sasSVGstr from '../style/icons/sas.svg';
+
+export const sasIcon = new LabIcon({
+    name: 'sas2nb:sas',
+    svgstr: sasSVGstr
+  });
