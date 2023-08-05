24fa974da52cc114acf7fe316a357886656c0f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:38 +0000

    Add package simple-ddl-parser/0.10.1/simple_ddl_parser-0.10.1-py3-none-any.whl

---
+++ packages/simple-ddl-parser/simple_ddl_parser-0.10.1-py3-none-any.whl/simple_ddl_parser/test.py
@@ -0,0 +1,19 @@
+from simple_ddl_parser import DDLParser
+
+
+ddl = """
+
+create table ChildTableName(
+                parentTable varchar
+                );
+        ALTER TABLE ChildTableName
+        ADD CONSTRAINT "fk_t1_t2_tt"
+        FOREIGN KEY ("parentTable")
+        REFERENCES parentTable ("columnName")
+        ON DELETE CASCADE
+        ON UPDATE CASCADE;
+"""
+
+result = DDLParser(ddl).run()
+import pprint
+pprint.pprint(result)
