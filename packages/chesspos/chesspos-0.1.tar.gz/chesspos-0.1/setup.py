dff9880c838798080c8950f75892bc132c0046c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:57 +0000

    Add package chesspos/0.1/chesspos-0.1.tar.gz

---
+++ packages/chesspos/chesspos-0.1.tar.gz/chesspos-0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='chesspos',
+	version='0.1',
+	description='A library for manipulating, learning and searching chess positions',
+	url='https://github.com/patrickfrank1/chess-pos/',
+	author='Patrick Frank',
+	author_email='patrickfrank1@outlook.com',
+	license='GPLv3',
+	packages=find_packages(),
+	classifiers=[
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3',
+		'Topic :: Scientific/Engineering :: Artificial Intelligence',
+		'Topic :: Software Development :: Pre-processors',
+		'Topic :: Games/Entertainment :: Board Games'
+	],
+	zip_safe=False
+	# TODO: specify install_requires and python_requires
+)
