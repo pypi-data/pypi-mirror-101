ade928767fa5fb53a4d4d366d53788881c384f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:35 +0000

    Add package flashday/0.1.1.post22/flashday-0.1.1.post22.tar.gz

---
+++ packages/flashday/flashday-0.1.1.post22.tar.gz/flashday-0.1.1.post22/src/pripessoaink/flashday/admin.py
@@ -0,0 +1,66 @@
+from django.contrib import admin
+
+from .models import Artist, Collection, Event, ExtraPicture, Product, ProductOption
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ['name', 'instagram', 'telephone']
+
+    class CollectionInline(admin.TabularInline):
+        model = Collection
+        extra = 1
+
+    inlines = [CollectionInline]
+
+
+# @admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['author']
+
+    def author(self, obj):
+        return obj.artist.name
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'artist']
+
+    def artist(self, obj):
+        return obj.collection.artist.name
+
+    class ExtraPictureInline(admin.TabularInline):
+        model = ExtraPicture
+        fk_name = 'Extra Picture'
+        extra = 1
+
+    class ProductOptionInline(admin.TabularInline):
+        model = ProductOption
+        extra = 1
+
+    inlines = [
+        ProductOptionInline,
+        ExtraPictureInline,
+    ]
+
+
+# @admin.register(ProductOption)
+class ProductOptionAdmin(admin.ModelAdmin):
+    list_display = ['key', 'value', 'product']
+
+    def product(self, obj):
+        return obj.reference.title
+
+
+# @admin.register(ExtraPicture)
+class ExtraPictureAdmin(admin.ModelAdmin):
+    list_display = ['product']
+
+    def product(self, obj):
+        return obj.reference.title
+
