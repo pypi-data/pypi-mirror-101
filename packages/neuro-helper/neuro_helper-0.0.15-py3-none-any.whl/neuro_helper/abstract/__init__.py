fb40970939f401200c14e0adc99e478ed7224bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:39 +0000

    Add package neuro-helper/0.0.15/neuro_helper-0.0.15-py3-none-any.whl

---
+++ packages/neuro-helper/neuro_helper-0.0.15-py3-none-any.whl/neuro_helper/abstract/__init__.py
@@ -0,0 +1,4 @@
+from .map import *
+
+__all__ = ["Space", "TopoName", "TemplateName", "HierarchyName", "TemplateData",
+           "AbstractMap", "TemplateMap", "TopoMap", "file_names"]
---
+++ packages/neuro-helper/neuro_helper-0.0.15-py3-none-any.whl/neuro_helper/hcp/meg/generic.py
@@ -0,0 +1,9 @@
+__all__ = ["task_order"]
+
+
+def task_order(with_rest=True):
+    if with_rest:
+        out = ["Rest"]
+    else:
+        out = []
+    return out + ["StoryM", "Motort", "Wrkmem"]
