1b0efb3292817d9aba906395692134b010a91c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:18 +0000

    Add package pnplabs/1.0.3/pnplabs-1.0.3.tar.gz

---
+++ packages/pnplabs/pnplabs-1.0.3.tar.gz/pnplabs-1.0.3/pnplabs/utils/local_config.py
@@ -0,0 +1,23 @@
+from appdirs import user_data_dir
+import configparser
+from os import makedirs
+import os.path
+
+APP_NAME = "pnplabs"
+AUTHOR_NAME = "pnplabs"
+
+USER_CONFIG_DIR_PATH = user_data_dir(APP_NAME, AUTHOR_NAME)
+USER_CONFIG_PATH = os.path.join(USER_CONFIG_DIR_PATH, "config.ini")
+
+
+def get_config():
+    config = configparser.ConfigParser()
+    if os.path.exists(USER_CONFIG_PATH):
+        config.read(USER_CONFIG_PATH)
+    return config
+
+
+def dump_config(config):
+    os.makedirs(USER_CONFIG_DIR_PATH, exist_ok=True)
+    with open(USER_CONFIG_PATH, 'w') as configfile:
+        config.write(configfile)
