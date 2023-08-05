d06b84f4f512d9443a0722395bf85d823912cd21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:07 +0000

    Add package atlcli/0.2.1/atlcli-0.2.1-py3-none-any.whl

---
+++ packages/atlcli/atlcli-0.2.1-py3-none-any.whl/cli/services/__init__.py
@@ -0,0 +1,6 @@
+from .bitbucket_service import BitbucketService
+from .confluence_service import ConfluenceService
+from .jira_service import JiraService
+from .sonarqube_service import SonarQubeService
+from .stats_service import StatsService
+from .powerbi_service import PowerBIService
