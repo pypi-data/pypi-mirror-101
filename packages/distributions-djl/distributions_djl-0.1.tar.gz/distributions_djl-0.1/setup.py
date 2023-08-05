b5d81a5762ee8e883ba484e1be3a4b22159708f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:49 +0000

    Add package distributions-djl/0.1/distributions_djl-0.1.tar.gz

---
+++ packages/distributions-djl/distributions_djl-0.1.tar.gz/distributions_djl-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_djl',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_djl'],
+      author='Derrick Lewis',
+      zip_safe=False)
