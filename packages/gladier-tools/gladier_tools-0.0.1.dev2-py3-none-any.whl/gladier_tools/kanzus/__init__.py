23b68e56373568b0cba8ecdf2568f1b1643fa6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:34 +0000

    Add package gladier-tools/0.0.1.dev2/gladier_tools-0.0.1.dev2-py3-none-any.whl

---
+++ packages/gladier-tools/gladier_tools-0.0.1.dev2-py3-none-any.whl/gladier_tools/kanzus/__init__.py
@@ -0,0 +1,17 @@
+from gladier import GladierBaseTool
+
+
+#__[all]__ = [CreatePhil,DialsStills,DialsVersion,Pilot,Prime,Primalisys]
+
+
+CreatePhil = GladierBaseTool()
+
+DialsStills = GladierBaseTool()
+
+DialsVersion = GladierBaseTool()
+
+Pilot = GladierBaseTool()
+
+Prime = GladierBaseTool()
+
+Primalisys = GladierBaseTool()
