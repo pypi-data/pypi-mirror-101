c9a33b48f22785f9114aed562a26224b46358168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:23 +0000

    Add package blizzard-api/0.3.0/blizzard-api-0.3.0.tar.gz

---
+++ packages/blizzard-api/blizzard-api-0.3.0.tar.gz/blizzard-api-0.3.0/vbinds/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=1.6.7
+# hash=8014025c88e39772f199eecaf4397d22
+# =====================================
+
+"""
+vbinds - Useful defaults and other package metadata.
+"""
+
+PKG_NAME = "blizzard-api"
+VERSION = "0.3.0"
+DESCRIPTION = "Interact with Blizard's game-data API."
