9e9651581df88b3e2c8de2e3457f0aba2ea9ff99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:45 +0000

    Add package ilim/1.2/ilim-1.2.tar.gz

---
+++ packages/ilim/ilim-1.2.tar.gz/ilim-1.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'ilim',         
+  packages = ['ilim'],   
+  version = '1.2',      
+  license = 'MIT',       
+  description = 'Grammar formater for kyrgyz language',   
+  author = 'Daniyar Biybosunov',                   
+  author_email = 'daniiar.commerce@gmail.com',    
+  url = 'https://github.com/daniiarkg/ilim',  
+  download_url = 'https://github.com/daniiarkg/ilim/archive/refs/tags/v_1.2.tar.gz',    
+  keywords = ['Grammar', 'Formatter', 'Grammar formatter', 'Library'],   
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
