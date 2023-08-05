17dfdb36eb5040880ecedf20a014cec6e963d158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:13 +0000

    Add package crcengine/0.3.0b3/crcengine-0.3.0b3-py3-none-any.whl

---
+++ packages/crcengine/crcengine-0.3.0b3-py3-none-any.whl/crcengine/__init__.py
@@ -0,0 +1,38 @@
+"""
+This file is part of crcengine, a python library for CRC calculation
+
+Copyright 2021 Garden Tools software
+
+crcengine is free software: you can redistribute it an d /or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+crcengine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with crcengine.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from .version import __version__
+from .calc import (
+    new,
+    create,
+    create_generic,
+    create_msb_table,
+    create_lsb_table,
+    bit_reverse_byte,
+    bit_reverse_n,
+    get_bits_max_value,
+)
+from .algorithms import (
+    get_algorithm_params,
+    algorithms_available,
+    register_algorithm,
+    unregister_algorithm,
+    AlgorithmNotFoundError,
+)
+from .codegen import generate_code, generate_test
