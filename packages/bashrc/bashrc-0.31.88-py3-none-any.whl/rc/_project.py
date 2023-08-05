bf84d5a2c83d3eb492bff2a9038e3a20c50af2ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:38 +0000

    Add package bashrc/0.31.88/bashrc-0.31.88-py3-none-any.whl

---
+++ packages/bashrc/bashrc-0.31.88-py3-none-any.whl/rc/_project.py
@@ -0,0 +1,32 @@
+__all__ = (
+    'APPCONTEXT',
+    'CLI',
+    'TESTS',
+    'Project',
+)
+
+from dataclasses import dataclass
+from dataclasses import field
+from dataclasses import Field
+from typing import Union
+
+from typer import Typer
+
+# from ._info import package
+# from ._info import _main
+# from ._info import info
+from .utils import *
+
+APPCONTEXT = dict(help_option_names=['-h', '--help'], color=True)
+CLI = varname(1)
+TESTS = varname(1)
+ic(TESTS)
+#
+# @dataclass
+# class Project(_base):
+#     init: Union[Field, info] = field(default=package, init=False)
+#     main: info = field(default=_main, init=False)
+#     cli: Typer = Typer(name=init.default.package, context_settings=APPCONTEXT)
+#
+#
+# project = Project()
