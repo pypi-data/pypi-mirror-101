850d8185c5cc99ef12eee40a28e1db31891f0838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:08 +0000

    Add package pymacro/0.0.1/pymacro-0.0.1.tar.gz

---
+++ packages/pymacro/pymacro-0.0.1.tar.gz/pymacro-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    _description = f.read()
+
+setuptools.setup(
+    name="pymacro",
+    version="0.0.1",
+    author="jay3332",
+    description="PyMacro can automate your tasks inside of Python.",
+    long_description=_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "pyautogui",
+        "pytweening"
+    ],
+    url="https://github.com/jay3332/PyMacro",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
