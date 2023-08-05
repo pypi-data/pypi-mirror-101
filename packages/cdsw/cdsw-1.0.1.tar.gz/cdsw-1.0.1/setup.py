e34561f4c03b6246257e6b9a002140af65f044b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:04 +0000

    Add package cdsw/1.0.1/cdsw-1.0.1.tar.gz

---
+++ packages/cdsw/cdsw-1.0.1.tar.gz/cdsw-1.0.1/setup.py
@@ -0,0 +1,28 @@
+"""Setup for the cdsw package."""
+
+import setuptools
+import os
+
+
+setuptools.setup(
+    author="Ajay Kulal",
+    author_email="hackerhero@bugcrowdninja.com",
+    name='cdsw',
+    license="MIT",
+    description='cdsw is a amazing package for data handling',
+    version='v1.0.1',
+    long_description="no long decsription given",
+    url='https://github.com/bountyrecon/',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
