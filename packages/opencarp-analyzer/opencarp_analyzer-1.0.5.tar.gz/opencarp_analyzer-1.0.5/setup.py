74d3694706b8b5832877952d53c37a3a62108365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:56 +0000

    Add package opencarp-analyzer/1.0.5/opencarp_analyzer-1.0.5.tar.gz

---
+++ packages/opencarp-analyzer/opencarp_analyzer-1.0.5.tar.gz/opencarp_analyzer-1.0.5/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import json
+import os
+
+with open("README.md","r") as f:
+    long_description = f.read()
+
+with open(os.path.join(os.path.dirname(__file__),"opencarp_analyzer","version.json"),"r") as f:
+    version = json.loads(f.read())
+
+setuptools.setup(
+    name='opencarp_analyzer',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    version= version["version"],
+    packages=setuptools.find_packages() ,
+    url='https://github.com/regmibijay/opencarp-analyzer',
+    license='MIT',
+    author='Bijay Regmi',
+    author_email='opencarp-analyzer@regdelivery.de',
+    description='OpenCARP Trace Analyzer',
+    install_requires = ['pandas','matplotlib'],
+    entry_points = {'console_scripts': [
+    'opencarp-analyzer=opencarp_analyzer.analyzer:main',
+    'opencarp-plotter=opencarp_analyzer.plotter:main',
+    ]},
+    python_requires = '>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
