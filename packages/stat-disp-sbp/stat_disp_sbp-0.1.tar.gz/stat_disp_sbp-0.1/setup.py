502a128fda7314dce431ec571471a0e0c2088daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:26 +0000

    Add package stat-disp-sbp/0.1/stat_disp_sbp-0.1.tar.gz

---
+++ packages/stat-disp-sbp/stat_disp_sbp-0.1.tar.gz/stat_disp_sbp-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='stat_disp_sbp',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['stat_dist_sbp'],
+      zip_safe=False)
