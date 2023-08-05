f6c19cb79b277245fced2930ba74fac05bdfae01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:27 +0000

    Add package dist-pkg/0.0/dist_pkg-0.0.tar.gz

---
+++ packages/dist-pkg/dist_pkg-0.0.tar.gz/dist_pkg-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dist_pkg',
+      version='0.0',
+      description='Distributions Test Package',
+      packages=['dist_pkg'],
+      author_email='safronov@phystech.edu',
+      zip_safe=False)
