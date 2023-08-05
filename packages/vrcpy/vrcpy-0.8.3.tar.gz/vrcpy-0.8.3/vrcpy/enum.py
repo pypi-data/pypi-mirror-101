7983298a099f75d4757831ac9481d1f6779be971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:16 +0000

    Add package vrcpy/0.8.3/vrcpy-0.8.3.tar.gz

---
+++ packages/vrcpy/vrcpy-0.8.3.tar.gz/vrcpy-0.8.3/vrcpy/enum.py
@@ -0,0 +1,21 @@
+class PlayerModerationType:
+    Block = "block"
+    ShowAvatar = "showAvatar"
+    HideAvatar = "hideAvatar"
+    Mute = "mute"
+    Unmute = "unmute"
+
+
+class FavoriteType:
+    World = "world"
+    Friend = "friend"
+    Avatar = "avatar"
+
+
+class NotificationType:
+    All = "all"
+    FriendRequest = "friendRequest"
+    Invite = "invite"
+    RequestInvite = "requestInvite"
+    RequestInviteResponse = "requestInviteResponse"
+    Hidden = "hidden"
