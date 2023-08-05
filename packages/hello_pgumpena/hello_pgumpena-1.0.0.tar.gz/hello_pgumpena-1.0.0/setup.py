16139bf5b712d99b63a6c5693331b80eb12ddff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:10 +0000

    Add package hello_pgumpena/1.0.0/hello_pgumpena-1.0.0.tar.gz

---
+++ packages/hello_pgumpena/hello_pgumpena-1.0.0.tar.gz/hello_pgumpena-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name = 'hello_pgumpena',
+    version = '1.0.0',
+    py_modules = ['hello'],
+    author = 'gumpena.prasanth',
+    author_email = 'gumpena.prasanth@gmail.com',
+    url    = 'http://www.gumpena_prasanth.com',
+    description = 'very first program'
+    )
+
