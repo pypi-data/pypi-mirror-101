533b166b5a2b4ea62a030906a8de57cdc54fdf63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:31 +0000

    Add package gifmak3r/0.1.1/gifmak3r-0.1.1.tar.gz

---
+++ packages/gifmak3r/gifmak3r-0.1.1.tar.gz/gifmak3r-0.1.1/setup.py
@@ -0,0 +1,27 @@
+# pylint: skip-file
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gifmak3r",
+    version="0.1.1",
+    author="Fahim Hussain",
+    author_email="fahimhussain21@gmail.com",
+    description="A gif generator in Python that creates gifs in user-specified formats",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/frykher/gif-mak3r",
+    project_urls={
+        "Bug Tracker": "https://github.com/frykher/gif-mak3r/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ], 
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=["Pillow"]
+)
