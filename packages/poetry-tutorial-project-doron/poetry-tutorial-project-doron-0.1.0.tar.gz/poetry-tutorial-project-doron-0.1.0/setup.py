89810b823efba930b76edf88fc56f8be6c329a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:19 +0000

    Add package poetry-tutorial-project-doron/0.1.0/poetry-tutorial-project-doron-0.1.0.tar.gz

---
+++ packages/poetry-tutorial-project-doron/poetry-tutorial-project-doron-0.1.0.tar.gz/poetry-tutorial-project-doron-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_tutorial_project_doron']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['icecream>=2.1.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'poetry-tutorial-project-doron',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'dorhar',
+    'author_email': 'doron.harnoy@tensorleap.ai',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
