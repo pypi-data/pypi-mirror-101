4d459439c5972a21792cc4231f38f3f598be3cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:40 +0000

    Add package atlas-engine-client/2.0.0a1/atlas_engine_client-2.0.0a1.tar.gz

---
+++ packages/atlas-engine-client/atlas_engine_client-2.0.0a1.tar.gz/atlas_engine_client-2.0.0a1/atlas_engine_client/core/api/helpers/process_instances.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json, LetterCase
+from typing import Callable, List
+
+from ..base_client import BaseClient
+
+
+class ProcessInstanceHandler(BaseClient):
+
+    def __init__(self, url: str, identity: Callable=None):
+        super(ProcessInstanceHandler, self).__init__(url, identity)
+
+    def terminate(self, process_instance_id: str, options: dict={}):
+        path = f"/atlas_engine/api/v1/process_instances/{process_instance_id}/terminate"
+
+        _ = self.do_put(path, {}, options)
+
+        return True
