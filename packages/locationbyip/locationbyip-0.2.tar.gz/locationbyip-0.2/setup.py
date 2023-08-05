15bfd7bf4977a70dae11adfa6f9da3058c8a3c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:21 +0000

    Add package locationbyip/0.2/locationbyip-0.2.tar.gz

---
+++ packages/locationbyip/locationbyip-0.2.tar.gz/locationbyip-0.2/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup
+setup(name="locationbyip",version="0.2",description="By this package you can find location, coordinates, isp details to use in your application.",author="Awais khan", packages=['LocationByIP'], install_requires=['requests'])
