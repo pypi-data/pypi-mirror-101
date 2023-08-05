6d2e04721f56084f7f27a290b222275b9438d27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:16 +0000

    Add package nonebot-plugin-bam/0.1.5/nonebot_plugin_bam-0.1.5-py3-none-any.whl

---
+++ packages/nonebot-plugin-bam/nonebot_plugin_bam-0.1.5-py3-none-any.whl/nonebot_plugin_bam/__init__.py
@@ -0,0 +1,15 @@
+from .activity import cmd_bilibili_activity_info
+from .follow import cmd_follower_add, cmd_follower_list, cmd_follower_remove
+from .group import cmd_group_add, cmd_group_list, cmd_group_remove
+from .tasks.activity_monitor import task_check_new_activity
+from .tasks.live_monitor import task_check_all_live_status
+from .user import cmd_user_fetch
+
+__version__ = "0.1.5"
+
+from nonebot import export
+
+from .bilibili.activity import Activity, OneActivity
+
+export().Activity = Activity
+export().OneActivity = OneActivity
