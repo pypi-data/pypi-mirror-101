42dc6990460440a5945e33b72fa69cae90f68d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:37 +0000

    Add package flaskmng/0.0.4/flaskmng-0.0.4.tar.gz

---
+++ packages/flaskmng/flaskmng-0.0.4.tar.gz/flaskmng-0.0.4/flaskmng/__main__.py
@@ -0,0 +1,20 @@
+import sys
+import click
+
+from .startproject import startproject
+
+commands = [
+    startproject
+]
+
+
+main = click.CommandCollection(sources=commands)
+
+if __name__ == '__main__':
+    args = sys.argv
+    if "--help" in args or len(args) == 1:
+        print("flaskmng")
+    try:
+        main()
+    except Exception as e:
+        print("❌ "+str(e))
---
+++ packages/flaskmng/flaskmng-0.0.4.tar.gz/flaskmng-0.0.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from python_script_manager.package import PSMReader
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+psm = PSMReader('psm.json')
+
+setuptools.setup(
+    name=psm.get_name(),
+    version=psm.get_version(),
+    author=psm.get_author(),
+    author_email=psm.get_author_email(),
+    description=psm.get_description(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=psm.get_url(),
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Natural Language :: English"
+    ],
+    python_requires='>=3.6',
+    entry_points='''
+        [console_scripts]
+        flaskmng=flaskmng.__main__:main
+    ''',
+)
