4ccda2d1603d12f8acbca268f244cbff21f4a26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:37 +0000

    Add package afif/0.2.1/afif-0.2.1.tar.gz

---
+++ packages/afif/afif-0.2.1.tar.gz/afif-0.2.1/afif/helloworld.py
@@ -0,0 +1,2 @@
+def say_hello():
+    print('congragulation guys.. this module has work !!! :)')
---
+++ packages/afif/afif-0.2.1.tar.gz/afif-0.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='afif',         # How you named your package folder (MyLib)
+    packages=['afif'],   # Chose the same as "name"
+    version='0.2.1',      # Start with a small number and increase it with every change you make
+    license='MIT',
+    description='this package is create to remembering me about create, install, use in local, and publishing in pipy',
+    author='Muhammad Afif Nur Azizi',
+    author_email='afifnurazizi@gmail.com',
+)
