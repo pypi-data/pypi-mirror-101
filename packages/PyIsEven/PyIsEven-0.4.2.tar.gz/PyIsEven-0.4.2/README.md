7c5f1a18fad70e77ba6a109f8deb1507e155743c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:35 +0000

    Add package PyIsEven/0.4.2/PyIsEven-0.4.2.tar.gz

---
+++ packages/PyIsEven/PyIsEven-0.4.2.tar.gz/PyIsEven-0.4.2/README.md
@@ -0,0 +1,26 @@
+# Is Even
+
+Check if interger is even using `isevenapi`.
+
+https://isevenapi.xyz/
+
+Main features:
+
+- cache memoization
+- api retry handler
+- hide ads
+
+## Install
+
+```bash
+pip install PyIsEven
+```
+
+## Usage
+
+```python
+from is_even import is_even as ie
+
+ie.is_even(10)
+> True
+```
---
+++ packages/PyIsEven/PyIsEven-0.4.2.tar.gz/PyIsEven-0.4.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='PyIsEven',
+    packages=['is_even'],
+    version='0.4.2',
+    license='MIT',
+    description='Check is a integer is even',
+    author='Rosiney Gomes Pereira',
+    url='https://github.com/rosineygp/PyIsEven',
+    keywords=['even'],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests>=2.14.0',
+        'retry>=0.9.2',
+    ],
+)
