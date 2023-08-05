fdb4a160ece30add8847a2e92cc44c57821b1e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:26 +0000

    Add package paraloop/0.0.1.dev0/paraloop-0.0.1.dev0.tar.gz

---
+++ packages/paraloop/paraloop-0.0.1.dev0.tar.gz/paraloop-0.0.1.dev0/paraloop/__init__.py
@@ -0,0 +1,5 @@
+import paraloop.aggregation_strategies as aggregation_strategies
+from paraloop.paraloop import ParaLoop
+from paraloop.variable import Variable
+
+__all__ = ["aggregation_strategies", "ParaLoop", "Variable"]
