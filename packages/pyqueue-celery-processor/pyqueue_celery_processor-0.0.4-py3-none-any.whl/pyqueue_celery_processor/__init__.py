3d577527a621b381ec1a6b0a520cadd36e9ef04a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:24 +0000

    Add package pyqueue-celery-processor/0.0.4/pyqueue_celery_processor-0.0.4-py3-none-any.whl

---
+++ packages/pyqueue-celery-processor/pyqueue_celery_processor-0.0.4-py3-none-any.whl/pyqueue_celery_processor/__init__.py
@@ -0,0 +1,21 @@
+"""
+Implementation referred from segment analytics-python
+https://github.com/segmentio/analytics-python/tree/master/analytics
+"""
+from .client import Client
+
+default_client = None
+
+"""SETTINGS"""
+debug = True
+send = True
+max_queue_size = 1_00_000
+__all__ = ["add_to_queue"]
+
+
+def add_to_queue(task_func, countdown=0, *args, **kwargs):
+    global default_client
+    if not default_client:
+        default_client = Client(max_queue_size, send, debug)
+
+    default_client.enqueue(task_func, countdown=countdown, args=args, kwargs=kwargs)
