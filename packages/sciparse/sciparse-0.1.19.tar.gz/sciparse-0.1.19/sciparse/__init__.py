7852155078ebcb89b5119308cf3a15d1082ae9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:51 +0000

    Add package sciparse/0.1.19/sciparse-0.1.19.tar.gz

---
+++ packages/sciparse/sciparse-0.1.19.tar.gz/sciparse-0.1.19/sciparse/__init__.py
@@ -0,0 +1,5 @@
+import pint
+ureg = pint.get_application_registry()
+from sciparse.source.unit_parsing import *
+from sciparse.source.parsers import parse_xrd, parse_default
+from sciparse.test.shorthand import *
