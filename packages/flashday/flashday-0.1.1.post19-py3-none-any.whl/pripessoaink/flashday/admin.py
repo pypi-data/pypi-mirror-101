0e635b318e9c57b0b26932f6d1392d487eb50120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:11 +0000

    Add package flashday/0.1.1.post19/flashday-0.1.1.post19-py3-none-any.whl

---
+++ packages/flashday/flashday-0.1.1.post19-py3-none-any.whl/pripessoaink/flashday/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+
+from .models import Artist, Collection, Event, ExtraPicture, Product, ProductOption
+
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    fields = ['name', 'instagram', 'telefone']
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['artist.name']
+
+
+@admin.register(Event)
+class Event(admin.ModelAdmin):
+    list_display = ['title']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'collection']
+
+    class ExtraPictureInline(admin.TabularInline):
+        model = ExtraPicture
+
+    class ProductOptionInline(admin.TabularInline):
+        model = ProductOption
+
+    inlines = [
+        ExtraPictureInline,
+        ProductOptionInline,
+    ]
+
+
+@admin.register(ProductOption)
+class ProductOptionAdmin(admin.ModelAdmin):
+    list_display = ['key', 'value']
+
+
+@admin.register(ExtraPicture)
+class ExtraPictureAdmin(admin.ModelAdmin):
+    list_display = ['reference.name']
+
