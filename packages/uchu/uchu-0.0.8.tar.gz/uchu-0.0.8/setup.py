96617de70598158d1e29f43971fbd9e3162e53f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:50 +0000

    Add package uchu/0.0.8/uchu-0.0.8.tar.gz

---
+++ packages/uchu/uchu-0.0.8.tar.gz/uchu-0.0.8/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+  name = 'uchu',
+  packages = ['uchu'],
+  version = '0.0.8',
+  description = 'Sane interface to cloud services.',
+  long_description = '',
+  author = '',
+  license = '',
+  package_data={},
+  url = 'https://github.com/alvations/uchu',
+  keywords = [],
+  classifiers = [],
+  install_requires = ['boto3', 'botocore'],
+
+)
