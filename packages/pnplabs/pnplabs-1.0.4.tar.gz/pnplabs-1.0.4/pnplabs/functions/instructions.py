a913b304ccbce44875bbffed152fd0d5d5d1a623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:19 +0000

    Add package pnplabs/1.0.4/pnplabs-1.0.4.tar.gz

---
+++ packages/pnplabs/pnplabs-1.0.4.tar.gz/pnplabs-1.0.4/pnplabs/functions/instructions.py
@@ -0,0 +1,4 @@
+def execute(*args, **kwargs):
+    print(f"Welcome to pnplabs!")
+    print(f"In order to use our command shortcuts, first connect with an API Key using the 'connect' command.\n"
+          f"Run: 'pnplabs connect <API Key>' using the API Key provided at http://pnplabs.io/getstarted")
