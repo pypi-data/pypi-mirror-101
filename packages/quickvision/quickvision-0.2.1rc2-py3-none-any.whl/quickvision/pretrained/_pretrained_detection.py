d61fc6d9188c9cd777594e2f1826f4fc17bcaa0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:25 +0000

    Add package quickvision/0.2.1rc2/quickvision-0.2.1rc2-py3-none-any.whl

---
+++ packages/quickvision/quickvision-0.2.1rc2-py3-none-any.whl/quickvision/pretrained/_pretrained_detection.py
@@ -0,0 +1,34 @@
+# flake8: noqa
+
+frcnn_weights_dict = {
+    "resnet50": {
+        "coco": "https://download.pytorch.org/models/fasterrcnn_resnet50_fpn_coco-258fb6c6.pth"
+    },
+    "mobilenet_v3_large_320": {
+        "coco": "https://download.pytorch.org/models/fasterrcnn_mobilenet_v3_large_320_fpn-907ea3f9.pth"
+    },
+    "mobilenet_v3_large": {
+        "coco": "https://download.pytorch.org/models/fasterrcnn_mobilenet_v3_large_fpn-fb6a3cc7.pth"
+    },
+}
+
+retina_weights_dict = {
+    "resnet50": {
+        "coco": "https://download.pytorch.org/models/retinanet_resnet50_fpn_coco-eeacb38b.pth"
+    },
+}
+
+detr_weights_dict = {
+    "resnet50": {
+        "coco": "https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth"
+    },
+    "resnet101": {
+        "coco": "https://dl.fbaipublicfiles.com/detr/detr-r101-2c7b67e5.pth"
+    },
+    "resnet50_dc5": {
+        "coco": "ttps://dl.fbaipublicfiles.com/detr/detr-r50-dc5-f0fb7ef5.pth"
+    },
+    "resnet101_dc5": {
+        "coco": "https://dl.fbaipublicfiles.com/detr/detr-r101-dc5-a2e86def.pth"
+    },
+}
