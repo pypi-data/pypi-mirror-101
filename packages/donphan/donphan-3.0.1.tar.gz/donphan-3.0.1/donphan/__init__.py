63f1ddf8942364464b7b44e28be16d6b5770f6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:03 +0000

    Add package donphan/3.0.1/donphan-3.0.1.tar.gz

---
+++ packages/donphan/donphan-3.0.1.tar.gz/donphan-3.0.1/donphan/__init__.py
@@ -0,0 +1,49 @@
+"""
+MIT License
+
+Copyright (c) 2019-present Josh B
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+__title__ = 'donphan'
+__author__ = 'Bijij'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 Bijij'
+__version__ = '3.0.1'
+
+from .column import Column as Column
+from .connection import (
+    create_pool as create_pool,
+    MaybeAcquire as MaybeAcquire
+)
+from .table import (
+    create_tables as create_tables,
+    Table as Table
+)
+from .types import (
+    create_types as create_types,
+    enum as enum,
+    Enum as Enum
+)
+from .sqltype import SQLType as SQLType
+from .view import (
+    create_views as create_views,
+    View as View
+)
---
+++ packages/donphan/donphan-3.0.1.tar.gz/donphan-3.0.1/donphan/connection.py
@@ -0,0 +1,76 @@
+"""
+MIT License
+
+Copyright (c) 2019-present Josh B
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import json
+
+import asyncpg  # type: ignore
+
+
+_pool: asyncpg.pool.Pool = None
+
+
+async def create_pool(dsn: str, **kwargs) -> asyncpg.pool.Pool:
+    """Creates the database connection pool."""
+    global _pool
+
+    def _encode_json(value):
+        return json.dumps(value)
+
+    def _decode_json(value):
+        return json.loads(value)
+
+    async def init(connection: asyncpg.Connection):
+        await connection.set_type_codec('json', schema='pg_catalog', encoder=_encode_json, decoder=_decode_json, format='text')
+        await connection.set_type_codec('jsonb', schema='pg_catalog', encoder=_encode_json, decoder=_decode_json, format='text')
+
+    _pool = p = await asyncpg.create_pool(dsn, init=init, **kwargs)
+    return p
+
+
+class MaybeAcquire:
+    """Async helper for acquiring a connection to the database.
+
+    Args:
+        connection (asyncpg.Connection, optional): A database connection to use
+                If none is supplied a connection will be acquired from the pool.
+    Kwargs:
+        pool (asyncpg.pool.Pool, optional): A connection pool to use.
+            If none is supplied the default pool will be used.
+    """
+
+    def __init__(self, connection: asyncpg.Connection = None, *, pool=None):
+        self.connection = connection
+        self.pool = pool or _pool
+        self._cleanup = False
+
+    async def __aenter__(self) -> asyncpg.Connection:
+        if self.connection is None:
+            self._cleanup = True
+            self._connection = c = await self.pool.acquire()
+            return c
+        return self.connection
+
+    async def __aexit__(self, *args):
+        if self._cleanup:
+            await self.pool.release(self._connection)
