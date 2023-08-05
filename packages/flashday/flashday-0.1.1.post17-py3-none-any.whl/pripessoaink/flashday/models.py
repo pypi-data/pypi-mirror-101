83ee8f38cdcbfae49fc8d3a179d6a66e02ccbbcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:10 +0000

    Add package flashday/0.1.1.post17/flashday-0.1.1.post17-py3-none-any.whl

---
+++ packages/flashday/flashday-0.1.1.post17-py3-none-any.whl/pripessoaink/flashday/models.py
@@ -0,0 +1,44 @@
+from django.db.models import (
+    CASCADE,
+    CharField,
+    ForeignKey,
+    ImageField,
+    Model,
+    TextField,
+)
+
+
+class Event(Model):
+    title = CharField(max_length=50)
+    description = TextField()
+    image = ImageField(upload_to='event')
+
+
+class Artist(Model):
+    name = CharField(max_length=50)
+    description = TextField()
+    instagram = CharField(max_length=50)
+    telephone = CharField(max_length=13)
+    picture = ImageField(upload_to='artist')
+
+
+class Collection(Model):
+    artist = ForeignKey(Artist, on_delete=CASCADE)
+
+
+class Product(Model):
+    collection = ForeignKey(Collection, on_delete=CASCADE)
+    title = CharField(max_length=50)
+    description = TextField()
+    picture = ImageField(upload_to='product')
+
+
+class ProductOption(Model):
+    reference = ForeignKey(Product, on_delete=CASCADE)
+    key = CharField(max_length=50)
+    value = CharField(max_length=50)
+
+
+class ExtraPicture(Model):
+    reference = ForeignKey(Product, on_delete=CASCADE)
+    image = ImageField(upload_to='product')
