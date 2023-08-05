8ae2a62c460b3075f82128ae3573d8ec7aa584f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:50 +0000

    Add package afif/0.1.1/afif-0.1.1.tar.gz

---
+++ packages/afif/afif-0.1.1.tar.gz/afif-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='afif',         # How you named your package folder (MyLib)
+    packages=['src'],   # Chose the same as "name"
+    version='0.1.1',      # Start with a small number and increase it with every change you make
+    license='MIT',
+    description='this package is create to remembering me about create, install, use in local, and publishing in pipy',
+    author='Muhammad Afif Nur Azizi',
+    author_email='afifnurazizi@gmail.com',
+)
