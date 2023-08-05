94cc43a5d0f2ebde446cde9973dff42e6fd7f358
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:15 +0000

    Add package proxygrab/0.3.1/ProxyGrab-0.3.1-py3-none-any.whl

---
+++ packages/proxygrab/ProxyGrab-0.3.1-py3-none-any.whl/proxygrab/__init__.py
@@ -0,0 +1,34 @@
+__version__ = "0.3.1"
+__copyright__ = "Copyright (C) 2020-2021 Divkix <https://github.com/Divkix>"
+
+
+# Free Methods to get proxies!
+from proxygrab.package.main import (
+    get_all_proxies,
+    get_http,
+    get_https,
+    get_proxy,
+    get_socks4,
+    get_socks5,
+    save_all_proxies,
+    save_http,
+    save_https,
+    save_proxy,
+    save_socks4,
+    save_socks5,
+)
+
+
+async def __useless_func():
+    get_all_proxies()
+    get_http()
+    get_https()
+    get_proxy()
+    get_socks4()
+    get_socks5()
+    save_all_proxies()
+    save_http()
+    save_https()
+    save_proxy()
+    save_socks4()
+    save_socks5()
