1831c84fb079818e64718f2404068c71976e0141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:01 +0000

    Add package allianceauth-afat/2.0.0a3/allianceauth-afat-2.0.0a3.tar.gz

---
+++ packages/allianceauth-afat/allianceauth-afat-2.0.0a3.tar.gz/allianceauth-afat-2.0.0a3/afat/providers.py
@@ -0,0 +1,14 @@
+"""
+providers
+"""
+
+from allianceauth.services.hooks import get_extension_logger
+from esi.clients import EsiClientProvider
+
+from afat import __title__
+from afat.constants import USER_AGENT
+from afat.utils import LoggerAddTag, get_swagger_spec_path
+
+logger = LoggerAddTag(get_extension_logger(__name__), __title__)
+
+esi = EsiClientProvider(spec_file=get_swagger_spec_path(), app_info_text=USER_AGENT)
