07756f6fd6fa1002aff6dfb7534dadb1a393c05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:34 +0000

    Add package git-dumper/1.0.2/git-dumper-1.0.2.tar.gz

---
+++ packages/git-dumper/git-dumper-1.0.2.tar.gz/git-dumper-1.0.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name='git-dumper',
+    version='1.0.2',
+    py_modules=['git_dumper'],
+    entry_points={
+        'console_scripts': [
+            'git-dumper = git_dumper:main',
+        ]
+    },
+    author='Maxime Arthaud',
+    author_email='maxime@arthaud.me',
+    description='A tool to dump a git repository from a website',
+    license='MIT',
+    keywords='dump git repository security vulnerability ctf',
+    url='https://github.com/arthaud/git-dumper',
+    install_requires=[
+        'PySocks',
+        'requests',
+        'beautifulsoup4',
+        'dulwich',
+        'wheel', # prevent some pip errors regarding 'bdist_wheel'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Security',
+    ],
+)
