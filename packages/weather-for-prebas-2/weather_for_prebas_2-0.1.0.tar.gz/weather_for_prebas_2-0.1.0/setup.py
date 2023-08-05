6c3432ac8df2a9d7b1bc208f4fccf02396ef929e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:43 +0000

    Add package weather-for-prebas-2/0.1.0/weather_for_prebas_2-0.1.0.tar.gz

---
+++ packages/weather-for-prebas-2/weather_for_prebas_2-0.1.0.tar.gz/weather_for_prebas_2-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['weather_for_prebas_2']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['geopandas>=0.9.0,<0.10.0', 'requests>=2.25.1,<3.0.0', 'utm>=0.7.0,<0.8.0']
+
+setup_kwargs = {
+    'name': 'weather-for-prebas-2',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Jan Pisl',
+    'author_email': 'jan.pisl@simosol.fi',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
