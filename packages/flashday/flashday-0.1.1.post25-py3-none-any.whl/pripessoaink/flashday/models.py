a522c698c6b759bd03e3c1457073d69571029eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:07 +0000

    Add package flashday/0.1.1.post25/flashday-0.1.1.post25-py3-none-any.whl

---
+++ packages/flashday/flashday-0.1.1.post25-py3-none-any.whl/pripessoaink/flashday/models.py
@@ -0,0 +1,72 @@
+from django.db.models import (
+    CASCADE,
+    BooleanField,
+    CharField,
+    ForeignKey,
+    ImageField,
+    Model,
+    TextField,
+)
+
+
+class Event(Model):
+    title = CharField(max_length=50)
+    description = TextField()
+    image = ImageField(upload_to='event')
+
+    def __str__(self):
+        return self.title
+
+
+class Artist(Model):
+    name = CharField(max_length=50)
+    description = TextField()
+    instagram = CharField(max_length=50)
+    telephone = CharField(max_length=13)
+    picture = ImageField(upload_to='artist')
+
+    def __str__(self):
+        return self.name
+
+
+class Collection(Model):
+    artist = ForeignKey(Artist, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.artist.name
+
+
+class Product(Model):
+    collection = ForeignKey(Collection, on_delete=CASCADE)
+    title = CharField(max_length=50)
+    description = TextField()
+    availability = BooleanField(default=True)
+    picture = ImageField(upload_to='product')
+
+    actions = ['change_availability']
+
+    def change_availability(self, request, queryset):
+        for model in queryset:
+            queryset.filter(model.id).update(
+                availability=not model.availability
+            )
+
+    def __str__(self):
+        return self.title
+
+
+class ProductOption(Model):
+    reference = ForeignKey(Product, on_delete=CASCADE)
+    key = CharField(max_length=50)
+    value = CharField(max_length=50)
+
+    def __str__(self):
+        return self.key
+
+
+class ExtraPicture(Model):
+    reference = ForeignKey(Product, on_delete=CASCADE)
+    image = ImageField(upload_to='product')
+
+    def __str__(self):
+        return self.image
