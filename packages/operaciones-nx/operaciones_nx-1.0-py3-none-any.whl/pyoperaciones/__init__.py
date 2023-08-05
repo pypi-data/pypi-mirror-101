1aa2ed5fb7d12b36eedcd5d6a9dc1bf2ba7cc22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:49 +0000

    Add package operaciones-nx/1.0/operaciones_nx-1.0-py3-none-any.whl

---
+++ packages/operaciones-nx/operaciones_nx-1.0-py3-none-any.whl/pyoperaciones/__init__.py
@@ -0,0 +1,14 @@
+def sumaEnteros (a, b):
+        return(a+b)
+
+def restaEnteros(a,b):
+    return(a-b)
+
+def multiplicaEnteros(a,b):
+    return(a*b)
+
+def divEnteros(a,b):
+    if (b>0):
+        return(a/b)    
+    else:
+         return 0
