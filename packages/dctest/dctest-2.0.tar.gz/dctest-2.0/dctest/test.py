facd13e4420c3aed6c1ae35c9cb85999c57ba36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:01 +0000

    Add package dctest/2.0/dctest-2.0.tar.gz

---
+++ packages/dctest/dctest-2.0.tar.gz/dctest-2.0/dctest/test.py
@@ -0,0 +1,2 @@
+def my_address():
+   print("Hi! I belong to the external repository")
---
+++ packages/dctest/dctest-2.0.tar.gz/dctest-2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="dctest",
+    version="2.0",
+    long_description="DC-Test",
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=["dctest"],
+)
