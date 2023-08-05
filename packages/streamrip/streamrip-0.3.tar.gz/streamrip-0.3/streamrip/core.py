312c13d67f8f1ba053f00d3841bea00691e05b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:59 +0000

    Add package streamrip/0.3/streamrip-0.3.tar.gz

---
+++ packages/streamrip/streamrip-0.3.tar.gz/streamrip-0.3/streamrip/core.py
@@ -0,0 +1,382 @@
+import logging
+import os
+import re
+import sys
+from getpass import getpass
+from hashlib import md5
+from string import Formatter
+from typing import Generator, Optional, Tuple, Union
+
+import click
+
+from .clients import DeezerClient, QobuzClient, SoundCloudClient, TidalClient
+from .config import Config
+from .constants import (
+    CONFIG_PATH,
+    DB_PATH,
+    MEDIA_TYPES,
+    SOUNDCLOUD_URL_REGEX,
+    URL_REGEX,
+)
+from .db import MusicDB
+from .downloader import Album, Artist, Label, Playlist, Track
+from .exceptions import AuthenticationError, ParsingError
+from .utils import capitalize
+
+logger = logging.getLogger(__name__)
+
+
+MEDIA_CLASS = {
+    "album": Album,
+    "playlist": Playlist,
+    "artist": Artist,
+    "track": Track,
+    "label": Label,
+}
+Media = Union[Album, Playlist, Artist, Track]
+
+
+class MusicDL(list):
+    def __init__(
+        self,
+        config: Optional[Config] = None,
+    ):
+
+        self.url_parse = re.compile(URL_REGEX)
+        self.soundcloud_url_parse = re.compile(SOUNDCLOUD_URL_REGEX)
+        self.config = config
+        if self.config is None:
+            self.config = Config(CONFIG_PATH)
+
+        self.clients = {
+            "qobuz": QobuzClient(),
+            "tidal": TidalClient(),
+            "deezer": DeezerClient(),
+            "soundcloud": SoundCloudClient(),
+        }
+
+        if config.session["database"]["enabled"]:
+            if config.session["database"]["path"] is not None:
+                self.db = MusicDB(config.session["database"]["path"])
+            else:
+                self.db = MusicDB(DB_PATH)
+                config.file["database"]["path"] = DB_PATH
+                config.save()
+        else:
+            self.db = []
+
+    def prompt_creds(self, source: str):
+        """Prompt the user for credentials.
+
+        :param source:
+        :type source: str
+        """
+        if source == "qobuz":
+            click.secho(f"Enter {capitalize(source)} email:", fg="green")
+            self.config.file[source]["email"] = input()
+            click.secho(
+                f"Enter {capitalize(source)} password (will not show on screen):",
+                fg="green",
+            )
+            self.config.file[source]["password"] = md5(
+                getpass(prompt="").encode("utf-8")
+            ).hexdigest()
+
+            self.config.save()
+            click.secho(f'Credentials saved to config file at "{self.config._path}"')
+        else:
+            raise Exception
+
+    def assert_creds(self, source: str):
+        assert source in (
+            "qobuz",
+            "tidal",
+            "deezer",
+            "soundcloud",
+        ), f"Invalid source {source}"
+        if source == "deezer":
+            # no login for deezer
+            return
+
+        if source == "soundcloud":
+            return
+
+        if source == "qobuz" and (
+            self.config.file[source]["email"] is None
+            or self.config.file[source]["password"] is None
+        ):
+            self.prompt_creds(source)
+
+    def handle_urls(self, url: str):
+        """Download an url
+
+        :param url:
+        :type url: str
+        :raises InvalidSourceError
+        :raises ParsingError
+        """
+        for source, url_type, item_id in self.parse_urls(url):
+            if item_id in self.db:
+                logger.info(
+                    f"ID {item_id} already downloaded, use --no-db to override."
+                )
+                click.secho(
+                    f"ID {item_id} already downloaded, use --no-db to override.",
+                    fg="magenta",
+                )
+                continue
+
+            self.handle_item(source, url_type, item_id)
+
+    def handle_item(self, source: str, media_type: str, item_id: str):
+        self.assert_creds(source)
+
+        client = self.get_client(source)
+
+        if media_type not in MEDIA_TYPES:
+            if "playlist" in media_type:  # for SoundCloud
+                media_type = "playlist"
+
+        assert media_type in MEDIA_TYPES, media_type
+        item = MEDIA_CLASS[media_type](client=client, id=item_id)
+        self.append(item)
+
+    def download(self):
+        arguments = {
+            "database": self.db,
+            "parent_folder": self.config.session["downloads"]["folder"],
+            "keep_cover": self.config.session["keep_cover"],
+            "large_cover": self.config.session["metadata"]["large_cover"],
+            "folder_format": self.config.session["path_format"]["folder"],
+            "track_format": self.config.session["path_format"]["track"],
+            "embed_cover": self.config.session["metadata"]["embed_cover"],
+        }
+        logger.debug("Arguments from config: %s", arguments)
+        for item in self:
+            arguments["quality"] = self.config.session[item.client.source]["quality"]
+            if isinstance(item, Artist):
+                filters_ = tuple(
+                    k for k, v in self.config.session["filters"].items() if v
+                )
+                arguments["filters"] = filters_
+                logger.debug("Added filter argument for artist/label: %s", filters_)
+
+            item.load_meta()
+
+            if isinstance(item, Track):
+                # track.download doesn't automatically tag
+                item.download(**arguments, tag=True)
+            else:
+                item.download(**arguments)
+
+            if self.db != []:
+                self.db.add(item.id)
+
+            if self.config.session["conversion"]["enabled"]:
+                item.convert(**self.config.session["conversion"])
+
+    def get_client(self, source: str):
+        client = self.clients[source]
+        if not client.logged_in:
+            self.assert_creds(source)
+            self.login(client)
+        return client
+
+    def login(self, client):
+        creds = self.config.creds(client.source)
+        if not client.logged_in:
+            while True:
+                try:
+                    client.login(**creds)
+                    break
+                except AuthenticationError:
+                    click.secho("Invalid credentials, try again.")
+                    self.prompt_creds(client.source)
+            if (
+                client.source == "qobuz"
+                and not creds.get("secrets")
+                and not creds.get("app_id")
+            ):
+                (
+                    self.config.file["qobuz"]["app_id"],
+                    self.config.file["qobuz"]["secrets"],
+                ) = client.get_tokens()
+                self.config.save()
+            elif client.source == "tidal":
+                self.config.file["tidal"].update(client.get_tokens())
+                self.config.save()
+
+    def parse_urls(self, url: str) -> Tuple[str, str]:
+        """Returns the type of the url and the id.
+
+        Compatible with urls of the form:
+            https://www.qobuz.com/us-en/{type}/{name}/{id}
+            https://open.qobuz.com/{type}/{id}
+            https://play.qobuz.com/{type}/{id}
+            /us-en/{type}/-/{id}
+
+            https://www.deezer.com/us/{type}/{id}
+            https://tidal.com/browse/{type}/{id}
+
+        :raises exceptions.ParsingError
+        """
+        parsed = self.url_parse.findall(url)  # Qobuz, Tidal, Dezer
+        soundcloud_urls = self.soundcloud_url_parse.findall(url)
+        soundcloud_items = [self.clients["soundcloud"].get(u) for u in soundcloud_urls]
+
+        parsed.extend(
+            ("soundcloud", item["kind"], url)
+            for item, url in zip(soundcloud_items, soundcloud_urls)
+        )
+
+        logger.debug(f"Parsed urls: {parsed}")
+
+        if parsed != []:
+            return parsed
+
+        raise ParsingError(f"Error parsing URL: `{url}`")
+
+    def handle_txt(self, filepath: Union[str, os.PathLike]):
+        """
+        Handle a text file containing URLs. Lines starting with `#` are ignored.
+
+        :param filepath:
+        :type filepath: Union[str, os.PathLike]
+        :raises OSError
+        :raises exceptions.ParsingError
+        """
+        with open(filepath) as txt:
+            self.handle_urls(txt.read())
+
+    def search(
+        self, source: str, query: str, media_type: str = "album", limit: int = 200
+    ) -> Generator:
+        client = self.get_client(source)
+        results = client.search(query, media_type)
+
+        i = 0
+        if isinstance(results, Generator):  # QobuzClient
+            for page in results:
+                tracklist = (
+                    page[f"{media_type}s"]["items"]
+                    if media_type != "featured"
+                    else page["albums"]["items"]
+                )
+                for item in tracklist:
+                    yield MEDIA_CLASS[
+                        media_type if media_type != "featured" else "album"
+                    ].from_api(item, client)
+                    i += 1
+                    if i > limit:
+                        return
+        else:
+            for item in (
+                results.get("data") or results.get("items") or results.get("collection")
+            ):
+                yield MEDIA_CLASS[media_type].from_api(item, client)
+                i += 1
+                if i > limit:
+                    return
+
+    def preview_media(self, media):
+        if isinstance(media, Album):
+            fmt = (
+                "{albumartist} - {title}\n"
+                "Released on {year}\n{tracktotal} tracks\n"
+                "{bit_depth} bit / {sampling_rate} Hz\n"
+                "Version: {version}"
+            )
+        elif isinstance(media, Artist):
+            fmt = "{name}"
+        elif isinstance(media, Track):
+            fmt = "{artist} - {title}\nReleased on {year}"
+        elif isinstance(media, Playlist):
+            fmt = (
+                "{title}\n"
+                "{tracktotal} tracks\n"
+                "{popularity}\n"
+                "Description: {description}"
+            )
+        else:
+            raise NotImplementedError
+
+        fields = (fname for _, fname, _, _ in Formatter().parse(fmt) if fname)
+        ret = fmt.format(**{k: media.get(k, default="Unknown") for k in fields})
+        return ret
+
+    def interactive_search(
+        self, query: str, source: str = "qobuz", media_type: str = "album"
+    ):
+        results = tuple(self.search(source, query, media_type, limit=50))
+
+        def title(res):
+            return f"{res[0]+1}. {res[1].title}"
+
+        def from_title(s):
+            num = []
+            for char in s:
+                if char != ".":
+                    num.append(char)
+                else:
+                    break
+            return self.preview_media(results[int("".join(num)) - 1])
+
+        if os.name == "nt":
+            try:
+                from pick import pick
+            except (ImportError, ModuleNotFoundError):
+                click.secho(
+                    "Run `pip3 install windows-curses` to use interactive mode.",
+                    fg="red",
+                )
+                sys.exit()
+
+            choice = pick(
+                tuple(enumerate(results)),
+                title=(
+                    f"{capitalize(source)} {media_type} search.\n"
+                    "Press SPACE to select, RETURN to download, ctrl-C to exit."
+                ),
+                options_map_func=title,
+                multiselect=True,
+            )
+
+            if isinstance(choice, list):
+                for item in choice:
+                    self.append(item[0][1])
+            elif isinstance(choice, tuple):
+                self.append(choice[0][1])
+
+            return True
+        else:
+            try:
+                from simple_term_menu import TerminalMenu
+            except (ImportError, ModuleNotFoundError):
+                click.secho(
+                    "Run `pip3 install simple-term-menu` to use interactive mode.",
+                    fg="red",
+                )
+                sys.exit()
+
+            menu = TerminalMenu(
+                map(title, enumerate(results)),
+                preview_command=from_title,
+                preview_size=0.5,
+                title=(
+                    f"{capitalize(source)} {media_type} search.\n"
+                    "SPACE - multiselection, ENTER - download, ESC - exit"
+                ),
+                cycle_cursor=True,
+                clear_screen=True,
+                multi_select=True,
+            )
+            choice = menu.show()
+            if choice is None:
+                return False
+            else:
+                if isinstance(choice, int):
+                    self.append(results[choice])
+                elif isinstance(choice, tuple):
+                    for i in choice:
+                        self.append(results[i])
+                return True
---
+++ packages/streamrip/streamrip-0.3.tar.gz/streamrip-0.3/streamrip/utils.py
@@ -0,0 +1,232 @@
+import base64
+import logging
+import logging.handlers as handlers
+import os
+from string import Formatter
+from typing import Optional, Union
+
+import requests
+from Crypto.Cipher import AES
+from Crypto.Util import Counter
+from pathvalidate import sanitize_filename
+from tqdm import tqdm
+
+from .constants import LOG_DIR, TIDAL_COVER_URL
+from .exceptions import InvalidSourceError, NonStreamable
+
+logger = logging.getLogger(__name__)
+
+
+def safe_get(d: dict, *keys, default=None):
+    """A replacement for chained `get()` statements on dicts:
+    >>> d = {'foo': {'bar': 'baz'}}
+    >>> _safe_get(d, 'baz')
+    None
+    >>> _safe_get(d, 'foo', 'bar')
+    'baz'
+    """
+    curr = d
+    res = default
+    for key in keys:
+        res = curr.get(key, default)
+        if res == default or not hasattr(res, "__getitem__"):
+            return res
+        else:
+            curr = res
+    return res
+
+
+def get_quality(quality_id: int, source: str) -> Union[str, int]:
+    """Given the quality id in (0, 1, 2, 3, 4), return the streaming quality
+    value to send to the api for a given source.
+
+    :param quality_id: the quality id
+    :type quality_id: int
+    :param source: qobuz, tidal, or deezer
+    :type source: str
+    :rtype: Union[str, int]
+    """
+    if source == "qobuz":
+        q_map = {
+            1: 5,
+            2: 6,
+            3: 7,
+            4: 27,
+        }
+    elif source == "tidal":
+        q_map = {
+            0: "LOW",  # AAC
+            1: "HIGH",  # AAC
+            2: "LOSSLESS",  # CD Quality
+            3: "HI_RES",  # MQA
+        }
+    elif source == "deezer":
+        q_map = {
+            0: 128,
+            1: 320,
+            2: 1411,
+        }
+    else:
+        raise InvalidSourceError(source)
+
+    possible_keys = set(q_map.keys())
+    assert quality_id in possible_keys, f"{quality_id} must be in {possible_keys}"
+    return q_map[quality_id]
+
+
+def get_quality_id(bit_depth: Optional[int], sampling_rate: Optional[int]):
+    """Return a quality id in (5, 6, 7, 27) from bit depth and
+    sampling rate. If None is provided, mp3/lossy is assumed.
+
+    :param bit_depth:
+    :type bit_depth: Optional[int]
+    :param sampling_rate:
+    :type sampling_rate: Optional[int]
+    """
+    if not (bit_depth or sampling_rate):  # is lossy
+        return 1
+
+    if bit_depth == 16:
+        return 2
+
+    if bit_depth == 24:
+        if sampling_rate <= 96:
+            return 3
+
+        return 4
+
+
+def tqdm_download(url: str, filepath: str, params: dict = None):
+    """Downloads a file with a progress bar.
+
+    :param url: url to direct download
+    :param filepath: file to write
+    :type url: str
+    :type filepath: str
+    """
+    logger.debug(f"Downloading {url} to {filepath} with params {params}")
+    if params is None:
+        params = {}
+
+    r = requests.get(url, allow_redirects=True, stream=True, params=params)
+    total = int(r.headers.get("content-length", 0))
+    logger.debug(f"File size = {total}")
+    if total < 1000 and not url.endswith("jpg"):
+        raise NonStreamable
+
+    try:
+        with open(filepath, "wb") as file, tqdm(
+            total=total, unit="iB", unit_scale=True, unit_divisor=1024
+        ) as bar:
+            for data in r.iter_content(chunk_size=1024):
+                size = file.write(data)
+                bar.update(size)
+    except Exception:
+        try:
+            os.remove(filepath)
+        except OSError:
+            pass
+        raise
+
+
+def clean_format(formatter: str, format_info):
+    """Formats track or folder names sanitizing every formatter key.
+
+    :param formatter:
+    :type formatter: str
+    :param kwargs:
+    """
+    fmt_keys = [i[1] for i in Formatter().parse(formatter) if i[1] is not None]
+
+    logger.debug("Formatter keys: %s", fmt_keys)
+
+    clean_dict = dict()
+    for key in fmt_keys:
+        if isinstance(format_info.get(key), (str, int, float)):  # int for track numbers
+            clean_dict[key] = sanitize_filename(str(format_info[key]))
+        else:
+            clean_dict[key] = "Unknown"
+
+    return formatter.format(**clean_dict)
+
+
+def tidal_cover_url(uuid, size):
+    possibles = (80, 160, 320, 640, 1280)
+    assert size in possibles, f"size must be in {possibles}"
+
+    return TIDAL_COVER_URL.format(uuid=uuid.replace("-", "/"), height=size, width=size)
+
+
+def init_log(
+    path: Optional[str] = None, level: str = "DEBUG", rotate: str = "midnight"
+):
+    """
+    Initialize a log instance with a stream handler and a rotating file handler.
+    If a path is not set, fallback to the default app log directory.
+
+    :param path:
+    :type path: Optional[str]
+    :param level:
+    :type level: str
+    :param rotate:
+    :type rotate: str
+    """
+    if not path:
+        os.makedirs(LOG_DIR, exist_ok=True)
+        path = os.path.join(LOG_DIR, "qobuz_dl.log")
+
+    logger = logging.getLogger()
+    level = logging.getLevelName(level)
+    logger.setLevel(level)
+
+    formatter = logging.Formatter(
+        fmt="%(asctime)s - %(module)s.%(funcName)s.%(levelname)s: %(message)s",
+        datefmt="%H:%M:%S",
+    )
+
+    rotable = handlers.TimedRotatingFileHandler(path, when=rotate)
+    printable = logging.StreamHandler()
+
+    rotable.setFormatter(formatter)
+    printable.setFormatter(formatter)
+
+    logger.addHandler(printable)
+    logger.addHandler(rotable)
+
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+    logging.getLogger("tidal_api").setLevel(logging.WARNING)
+
+
+def capitalize(s: str) -> str:
+    return s[0].upper() + s[1:]
+
+
+def decrypt_mqa_file(in_path, out_path, encryption_key):
+    # Do not change this
+    master_key = "UIlTTEMmmLfGowo/UC60x2H45W6MdGgTRfo/umg4754="
+
+    # Decode the base64 strings to ascii strings
+    master_key = base64.b64decode(master_key)
+    security_token = base64.b64decode(encryption_key)
+
+    # Get the IV from the first 16 bytes of the securityToken
+    iv = security_token[:16]
+    encrypted_st = security_token[16:]
+
+    # Initialize decryptor
+    decryptor = AES.new(master_key, AES.MODE_CBC, iv)
+
+    # Decrypt the security token
+    decrypted_st = decryptor.decrypt(encrypted_st)
+
+    # Get the audio stream decryption key and nonce from the decrypted security token
+    key = decrypted_st[:16]
+    nonce = decrypted_st[16:24]
+
+    counter = Counter.new(64, prefix=nonce, initial_value=0)
+    decryptor = AES.new(key, AES.MODE_CTR, counter=counter)
+
+    with open(in_path, "rb") as enc_file:
+        dec_bytes = decryptor.decrypt(enc_file.read())
+        with open(out_path, "wb") as dec_file:
+            dec_file.write(dec_bytes)
