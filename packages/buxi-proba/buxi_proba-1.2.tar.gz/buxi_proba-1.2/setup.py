f4dd0b47868cd7cb8c880344ae35a71f7fb6c45c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:31 +0000

    Add package buxi-proba/1.2/buxi_proba-1.2.tar.gz

---
+++ packages/buxi-proba/buxi_proba-1.2.tar.gz/buxi_proba-1.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='buxi_proba',
+      version='1.2',
+      description='Gaussian and binomial distributions',
+      packages=['buxi_proba'],
+      zip_safe=False)
