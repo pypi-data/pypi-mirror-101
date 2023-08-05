de0eaab15d26604835f98e8c96bb5b9429b346c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:57 +0000

    Add package datapipelinev1/1.0.1/datapipelinev1-1.0.1-py3-none-any.whl

---
+++ packages/datapipelinev1/datapipelinev1-1.0.1-py3-none-any.whl/pipeline/persist.py
@@ -0,0 +1,16 @@
+import pyspark
+from pyspark.sql import SparkSession
+import logging
+import logging.config
+
+class Persist:
+    logging.config.fileConfig("pipeline/resources/configs/logging.conf")
+
+    def __init__(self,spark):
+        self.spark=spark
+
+    def persist_data(self,df):
+        logger = logging.getLogger("Persist")
+        logger.info('Persisting')
+        logger.error("dummy error in Persisting")
+        df.coalesce(1).write.option("header", "true").csv("transformed_retailstore")
