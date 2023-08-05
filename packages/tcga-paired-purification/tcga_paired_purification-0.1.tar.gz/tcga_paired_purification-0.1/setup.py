c22dda7a20d2a75ec635993aee70f43602804757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:07 +0000

    Add package tcga-paired-purification/0.1/tcga_paired_purification-0.1.tar.gz

---
+++ packages/tcga-paired-purification/tcga_paired_purification-0.1.tar.gz/tcga_paired_purification-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='tcga_paired_purification',
+    version='0.1',
+    author='Jeff Liu',
+    author_email='jeffliu6068@gmail.com',
+    packages=['tcga_paired_purification'],
+    url='https://github.com/jeffliu6068/TCGA_paired_purification',
+    license='LICENSE.txt',
+    description='Implementation of the TCGA purification protocol',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'pandas',
+        'scipy',
+        'numpy',
+        'matplotlib',
+        'seaborn',
+        'sklearn',
+        'tqdm'
+    ],
+)
