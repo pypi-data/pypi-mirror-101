682bc37d45f1f226d1349327edc5db8a88af0100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:19 +0000

    Add package py-Ultroid/15.9b0/py_Ultroid-15.9b0-py3-none-any.whl

---
+++ packages/py-Ultroid/py_Ultroid-15.9b0-py3-none-any.whl/pyUltroid/functions/welcome_db.py
@@ -0,0 +1,21 @@
+from .. import udB
+import json
+
+
+def add_welcome(chat, msg, media):
+    x = {"welcome": msg, "media": media}
+    return udB.set(f"{chat}_100", str(x))
+
+
+def get_welcome(chat):
+    wl = udB.get(f"{chat}_100")
+    if wl:
+        y = json.dumps(wl)
+        x = json.loads(y)
+        return x
+    else:
+        return
+
+
+def delete_welcome(chat):
+    return udB.delete(f"{chat}_100")
