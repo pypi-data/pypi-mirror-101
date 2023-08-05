4fff2ee03141ca8c7193945209cca9d65d92f84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:03 +0000

    Add package hks-pynetwork/0.0.1/hks_pynetwork-0.0.1.tar.gz

---
+++ packages/hks-pynetwork/hks_pynetwork-0.0.1.tar.gz/hks_pynetwork-0.0.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hks_pynetwork",
+    version="0.0.1",
+    author="huykingsofm",
+    author_email="huykingsofm@gmail.com",
+    description="A Python library is built to communicate between objects in internal or external programs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/huykingsofm/hks_pynetwork",
+    project_urls={
+        "Bug Tracker": "https://github.com/huykingsofm/hks_pynetwork/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3",
+    install_requires=["hks-pylib"],
+    setup_requires=["pytest-runner==4.4"],
+    tests_require=["pytest==4.4.1"],
+    test_suite="tests",
+)
