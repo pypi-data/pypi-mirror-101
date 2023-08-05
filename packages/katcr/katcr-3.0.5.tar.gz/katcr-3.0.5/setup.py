7577ed62f9a51af42c652917cd0085e445553f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:59 +0000

    Add package katcr/3.0.5/katcr-3.0.5.tar.gz

---
+++ packages/katcr/katcr-3.0.5.tar.gz/katcr-3.0.5/setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['katcr', 'katcr.engines']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.7.2,<4.0.0',
+ 'cleo',
+ 'cutie>=0.2.2,<0.3.0',
+ 'feedparser>=6.0.2,<7.0.0',
+ 'pytest-asyncio>=0.14.0,<0.15.0',
+ 'torrentmirror>=2.0,<3.0',
+ 'xdg>=5.0.1,<6.0.0']
+
+extras_require = \
+{':extra == "bot"': ['aiogram>=2.11.2,<3.0.0']}
+
+entry_points = \
+{'console_scripts': ['bot = katcr:bot', 'katcr = katcr:main']}
+
+setup_kwargs = {
+    'name': 'katcr',
+    'version': '3.0.5',
+    'description': 'KickassTorrents CLI and Telegram bot',
+    'long_description': None,
+    'author': 'David Francos',
+    'author_email': 'opensource@davidfrancos.net',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
