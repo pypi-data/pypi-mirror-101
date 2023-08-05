b8f989c246fdc6e4370b4cb6f2715beea9971760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:48 +0000

    Add package ts-machine/2021.4.4/ts_machine-2021.4.4-py3-none-any.whl

---
+++ packages/ts-machine/ts_machine-2021.4.4-py3-none-any.whl/tsm/main.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def main():
+    sys.exit("error: ts_machine is no longer available " +
+             "due to the termination of the niconico Contents Search API")
+
+
+if __name__ == '__main__':
+    main()
