1f43d81c31568d9325eb8d95022360d45a877135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:39 +0000

    Add package mtg-parser/0.0.1a3/mtg_parser-0.0.1a3-py3-none-any.whl

---
+++ packages/mtg-parser/mtg_parser-0.0.1a3-py3-none-any.whl/mtg_parser/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .gramar import * # noqa
+from .parser import * # noqa
+from .options import * # noqa
+from .diff import * # noqa
+from .moxfield import * # noqa
+from .archidekt import * # noqa
+
+
+__version__ = '0.0.1-alpha.3'
