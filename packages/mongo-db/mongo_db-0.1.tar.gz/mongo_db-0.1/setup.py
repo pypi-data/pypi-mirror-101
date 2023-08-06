02e899cfa720a0d6d71753c4e49c8cbd2a3fb31e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:23 +0000

    Add package mongo-db/0.1/mongo_db-0.1.tar.gz

---
+++ packages/mongo-db/mongo_db-0.1.tar.gz/mongo_db-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mongo_db',
+    version='0.1',
+    packages=['mongo_db'],
+    url='',
+    license='MIT',
+    author='LDmitriy7',
+    author_email='ldm.work2019@gmail.com',
+    description='TODO'
+)
