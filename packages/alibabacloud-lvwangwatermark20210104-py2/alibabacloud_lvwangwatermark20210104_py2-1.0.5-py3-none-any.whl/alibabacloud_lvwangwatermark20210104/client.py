2dd798755d9a7f41ce4c7da9f2dda971a03bfb93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:44 +0000

    Add package alibabacloud-lvwangwatermark20210104-py2/1.0.5/alibabacloud_lvwangwatermark20210104_py2-1.0.5-py3-none-any.whl

---
+++ packages/alibabacloud-lvwangwatermark20210104-py2/alibabacloud_lvwangwatermark20210104_py2-1.0.5-py3-none-any.whl/alibabacloud_lvwangwatermark20210104/client.py
@@ -0,0 +1,296 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from __future__ import unicode_literals
+
+from Tea.core import TeaCore
+
+from alibabacloud_tea_openapi.client import Client as OpenApiClient
+from alibabacloud_tea_openapi import models as open_api_models
+from alibabacloud_tea_util.client import Client as UtilClient
+from alibabacloud_endpoint_util.client import Client as EndpointUtilClient
+from alibabacloud_lvwangwatermark20210104 import models as lvwang_watermark_20210104_models
+from alibabacloud_tea_util import models as util_models
+
+
+class Client(OpenApiClient):
+    """
+    *\
+    """
+    def __init__(self, config):
+        super(Client, self).__init__(config)
+        self._endpoint_rule = ''
+        self.check_config(config)
+        self._endpoint = self.get_endpoint('lvwangwatermark', self._region_id, self._endpoint_rule, self._network, self._suffix, self._endpoint_map, self._endpoint)
+
+    def get_endpoint(self, product_id, region_id, endpoint_rule, network, suffix, endpoint_map, endpoint):
+        if not UtilClient.empty(endpoint):
+            return endpoint
+        if not UtilClient.is_unset(endpoint_map) and not UtilClient.empty(endpoint_map.get(region_id)):
+            return endpoint_map.get(region_id)
+        return EndpointUtilClient.get_endpoint_rules(product_id, region_id, endpoint_rule, network, suffix)
+
+    def add_audio_async_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.AddAudioAsyncResponse(),
+            self.do_rpcrequest('AddAudioAsync', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def add_audio_async(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.add_audio_async_with_options(request, runtime)
+
+    def add_doc_async_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.AddDocAsyncResponse(),
+            self.do_rpcrequest('AddDocAsync', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def add_doc_async(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.add_doc_async_with_options(request, runtime)
+
+    def add_image_async_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.AddImageAsyncResponse(),
+            self.do_rpcrequest('AddImageAsync', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def add_image_async(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.add_image_async_with_options(request, runtime)
+
+    def add_image_sync_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.AddImageSyncResponse(),
+            self.do_rpcrequest('AddImageSync', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def add_image_sync(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.add_image_sync_with_options(request, runtime)
+
+    def add_video_async_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.AddVideoAsyncResponse(),
+            self.do_rpcrequest('AddVideoAsync', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def add_video_async(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.add_video_async_with_options(request, runtime)
+
+    def get_audio_add_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetAudioAddResponse(),
+            self.do_rpcrequest('GetAudioAdd', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_audio_add(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_audio_add_with_options(request, runtime)
+
+    def get_audio_async_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetAudioAsyncResponse(),
+            self.do_rpcrequest('GetAudioAsync', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_audio_async(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_audio_async_with_options(request, runtime)
+
+    def get_audio_extract_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetAudioExtractResponse(),
+            self.do_rpcrequest('GetAudioExtract', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_audio_extract(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_audio_extract_with_options(request, runtime)
+
+    def get_audio_trace_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetAudioTraceResponse(),
+            self.do_rpcrequest('GetAudioTrace', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_audio_trace(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_audio_trace_with_options(request, runtime)
+
+    def get_doc_add_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetDocAddResponse(),
+            self.do_rpcrequest('GetDocAdd', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_doc_add(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_doc_add_with_options(request, runtime)
+
+    def get_doc_async_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetDocAsyncResponse(),
+            self.do_rpcrequest('GetDocAsync', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_doc_async(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_doc_async_with_options(request, runtime)
+
+    def get_doc_extract_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetDocExtractResponse(),
+            self.do_rpcrequest('GetDocExtract', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_doc_extract(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_doc_extract_with_options(request, runtime)
+
+    def get_image_async_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetImageAsyncResponse(),
+            self.do_rpcrequest('GetImageAsync', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_image_async(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_image_async_with_options(request, runtime)
+
+    def get_image_sync_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetImageSyncResponse(),
+            self.do_rpcrequest('GetImageSync', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_image_sync(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_image_sync_with_options(request, runtime)
+
+    def get_query_trace_file_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetQueryTraceFileResponse(),
+            self.do_rpcrequest('GetQueryTraceFile', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_query_trace_file(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_query_trace_file_with_options(request, runtime)
+
+    def get_video_add_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetVideoAddResponse(),
+            self.do_rpcrequest('GetVideoAdd', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_video_add(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_video_add_with_options(request, runtime)
+
+    def get_video_async_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetVideoAsyncResponse(),
+            self.do_rpcrequest('GetVideoAsync', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_video_async(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_video_async_with_options(request, runtime)
+
+    def get_video_extract_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetVideoExtractResponse(),
+            self.do_rpcrequest('GetVideoExtract', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_video_extract(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_video_extract_with_options(request, runtime)
+
+    def get_video_trace_with_options(self, request, runtime):
+        UtilClient.validate_model(request)
+        req = open_api_models.OpenApiRequest(
+            body=UtilClient.to_map(request)
+        )
+        return TeaCore.from_map(
+            lvwang_watermark_20210104_models.GetVideoTraceResponse(),
+            self.do_rpcrequest('GetVideoTrace', '2021-01-04', 'HTTPS', 'POST', 'AK', 'json', req, runtime)
+        )
+
+    def get_video_trace(self, request):
+        runtime = util_models.RuntimeOptions()
+        return self.get_video_trace_with_options(request, runtime)
