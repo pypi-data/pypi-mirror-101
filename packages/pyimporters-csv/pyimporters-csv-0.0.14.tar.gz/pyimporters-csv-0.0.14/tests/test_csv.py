bd42bc77fe922c66c2e9889d715206d32acd813e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:38 +0000

    Add package pyimporters-csv/0.0.14/pyimporters-csv-0.0.14.tar.gz

---
+++ packages/pyimporters-csv/pyimporters-csv-0.0.14.tar.gz/pyimporters-csv-0.0.14/tests/test_csv.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from progress.bar import Bar
+
+from pyimporters_plugins.base import Term
+from pyimporters_csv.csv import CSVKnowledgeParser, CSVOptions, CSVOptionsModel
+
+
+def test_csv():
+    source = Path('data/Digestion.csv')
+    parser = CSVKnowledgeParser()
+    options = CSVOptionsModel(encoding="utf-8", identifier_col="ID", prefLabel_col="prefLabel_en", altLabel_cols="altLabel_en", multivalue_separator="|")
+    concepts = list(parser.parse(source, options.dict(), Bar('Processing')))
+    assert len(concepts) == 92
+    c7 : Term = concepts[7]
+    assert c7.identifier == 'https://opendata.inra.fr/EMTD/8'
+    assert c7.preferredForm == 'specific pathogen-free animal'
+    assert len(c7.properties['altForms']) == 2
+    assert set(c7.properties['altForms']) == set(['SPF animal', 'specific pathogen free animal'])
