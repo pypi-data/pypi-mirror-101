f5d54cb1faf71cb903e53920f56b9d7184515c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:09 +0000

    Add package stocker-d/0.1a0/stocker_d-0.1a0.tar.gz

---
+++ packages/stocker-d/stocker_d-0.1a0.tar.gz/stocker_d-0.1a0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    readme = f.read()
+
+requires = [
+    "Flask==1.1.2",
+    "yfinance==0.1.59",
+    "pandas==1.2.3",
+    "numpy==1.20.2"
+]
+
+setup(
+    name='stocker_d',
+    version='0.1a',
+    scripts=['stocker'],
+    description="Stocker D",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author_email='david.woodruff@coxautoinc.com',
+    install_requires=requires,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keyworks='stocker'
+)
