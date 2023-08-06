a4d3aceed20f0b7f8b18f46e66b2499e459549ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:14 +0000

    Add package airflow-provider-great-expectations/0.0.6/airflow-provider-great-expectations-0.0.6.tar.gz

---
+++ packages/airflow-provider-great-expectations/airflow-provider-great-expectations-0.0.6.tar.gz/airflow-provider-great-expectations-0.0.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="airflow-provider-great-expectations",
+    version="0.0.6",
+    author="Great Expectations",
+    description="An Apache Airflow provider for Great Expectations",
+    entry_points='''
+        [apache_airflow_provider]
+        provider_info=great_expectations_provider.__init__:get_provider_info
+    ''',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/great-expectations/airflow-provider-great-expectations",
+    install_requires=['apache-airflow>=1.10',
+                      'great-expectations>=0.13.14',
+                      'pybigquery>0.3.0',
+                      'SQLAlchemy>=1.3.18,<1.4'],
+    packages=['great_expectations_provider',
+              'great_expectations_provider.operators'],
+    python_requires='>=3.6',
+    include_package_data=True
+)
