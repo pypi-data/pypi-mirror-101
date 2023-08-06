1ee2b0750e1498008bb08fc4a1064abf2205ca12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:27 +0000

    Add package kdmt/0.9.13/kdmt-0.9.13-py2.py3-none-any.whl

---
+++ packages/kdmt/kdmt-0.9.13-py2.py3-none-any.whl/kdmt/dict.py
@@ -0,0 +1,27 @@
+
+def nested_dict_get_values(key, dictionary):
+    if hasattr(dictionary, 'items'):
+        for k, v in dictionary.items():
+            if k == key:
+                yield v
+            if isinstance(v, dict):
+                for result in nested_dict_get_values(key, v):
+                    yield result
+            elif isinstance(v, list):
+                for d in v:
+                    for result in nested_dict_get_values(key, d):
+                        yield result
+
+
+def nested_dict_has_key(key, dictionary):
+
+    if hasattr(dictionary, 'items'):
+        for k, v in dictionary.items():
+            if k == key:
+                return True
+            if isinstance(v, dict):
+                return nested_dict_has_key(key, v)
+            elif isinstance(v, list):
+                for d in v:
+                    if nested_dict_has_key(key, d):
+                        return True
