f36da126ba25f88453f4ecfa43e4c4f6d6be3a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:05 +0000

    Add package SvgTestApp/0.0.0.1/SvgTestApp-0.0.0.1.tar.gz

---
+++ packages/SvgTestApp/SvgTestApp-0.0.0.1.tar.gz/SvgTestApp-0.0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='SvgTestApp',
+    version='0.0.0.1',
+    description="This is just a test",
+    packages=['script'],
+    install_requires=['svgwrite'],
+    python_requires='>=3',
+)
