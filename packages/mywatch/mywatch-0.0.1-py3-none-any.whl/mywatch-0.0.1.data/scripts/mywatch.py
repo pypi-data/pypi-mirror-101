f6d7e5723e7c3eb91bbc8c6c08654af02035c70c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:39 +0000

    Add package mywatch/0.0.1/mywatch-0.0.1-py3-none-any.whl

---
+++ packages/mywatch/mywatch-0.0.1-py3-none-any.whl/mywatch-0.0.1.data/scripts/mywatch.py
@@ -0,0 +1,23 @@
+import sys
+import subprocess
+import checksumdir
+from time import sleep
+
+
+_dir = sys.argv[1]
+command = sys.argv[2]
+
+
+subprocess.run(command, shell=True)
+
+dirhash = checksumdir.dirhash(_dir)
+
+
+while True:
+    sleep(1)
+    new_hash = checksumdir.dirhash(_dir)
+    if new_hash != dirhash:
+        print(f"Change detected, new hash: {new_hash}")
+        dirhash = new_hash
+        subprocess.run(command, shell=True)
+
