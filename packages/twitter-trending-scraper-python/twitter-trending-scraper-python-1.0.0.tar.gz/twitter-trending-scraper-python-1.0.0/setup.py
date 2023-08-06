b94334a17dd9f101150f896edf7503ca597d25c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:04 +0000

    Add package twitter-trending-scraper-python/1.0.0/twitter-trending-scraper-python-1.0.0.tar.gz

---
+++ packages/twitter-trending-scraper-python/twitter-trending-scraper-python-1.0.0.tar.gz/twitter-trending-scraper-python-1.0.0/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+VERSION = '1.0.0'
+PACKAGE_NAME = 'twitter-trending-scraper-python'
+AUTHOR = 'DataKund'
+AUTHOR_EMAIL = 'datakund@gmail.com'
+URL = 'https://twitter-api.datakund.com/en/latest/'
+KEYWORDS='twitter python bot_studio scraper login trending hashtags data web-scraping'
+LICENSE = 'Apache License 2.0'
+DESCRIPTION = 'A python library to scrape trending keywords from twitter automatically.'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = [
+      'requests','bot-studio'
+]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESC_TYPE,
+      author=AUTHOR,
+      license=LICENSE,
+      author_email=AUTHOR_EMAIL,
+      install_requires=INSTALL_REQUIRES,
+      packages=find_packages(),
+      url=URL,
+      keywords = KEYWORDS
+      )
