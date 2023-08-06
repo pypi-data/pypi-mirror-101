80bdb5c9f49b15f0c130b6c611e0b64f8cccbaf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:19 +0000

    Add package dist-bingau/0.1/dist_bingau-0.1.tar.gz

---
+++ packages/dist-bingau/dist_bingau-0.1.tar.gz/dist_bingau-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dist_bingau',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_bingau'],
+      zip_safe=False)
