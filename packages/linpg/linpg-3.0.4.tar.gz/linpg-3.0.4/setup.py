cc2564230c5bb405b99c6e4fe0a9dfae56250277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:13 +0000

    Add package linpg/3.0.4/linpg-3.0.4.tar.gz

---
+++ packages/linpg/linpg-3.0.4.tar.gz/linpg-3.0.4/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import linpg
+
+#读取readme
+with open("README.md", "r", encoding="utf-8") as fh: long_description = fh.read()
+
+#生成.whl文件
+setuptools.setup(
+    name = "linpg",
+    version = linpg.get_current_version(),
+    author = "Tigeia-Workshop",
+    author_email = linpg.get_author_email(),
+    description = linpg.get_short_description(),
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = linpg.get_repository_url(),
+    license='LICENSE',
+    project_urls={
+        "Bug Tracker": "https://github.com/Tigeia-Workshop/linpg/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+    ],
+    package_dir={"": "src"},
+    include_package_data=True,
+    python_requires = '>=3.6',
+    install_requires = [
+        "pygame",
+        "pyyaml",
+        "av",
+        "numpy",
+    ]
+)
