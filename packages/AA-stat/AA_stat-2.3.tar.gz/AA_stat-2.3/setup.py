f2e62dab9b2389395a17257acbe61961f5ef0c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:45 +0000

    Add package AA-stat/2.3/AA_stat-2.3.tar.gz

---
+++ packages/AA-stat/AA_stat-2.3.tar.gz/AA_stat-2.3/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name                 = 'AA_stat',
+    version              = '2.3',
+    description          = '''A utility for validation of peptide identification results in proteomics using amino acid counting.''',
+    long_description     = (''.join(open('README.MD').readlines())),
+    long_description_content_type="text/markdown",
+    author               = 'Julia Bubis & Lev Levitsky',
+    author_email         = 'julia.bubis@gmail.com',
+    install_requires     = ['pyteomics>4.4.1', 'pandas', 'seaborn', 'scipy', 'numpy', 'lxml', 'jinja2', 'scikit-learn'],
+    classifiers          = ['Intended Audience :: Science/Research',
+                            'Programming Language :: Python :: 3',
+                            'Topic :: Scientific/Engineering :: Bio-Informatics',
+                            'Topic :: Scientific/Engineering :: Chemistry',
+                            'Topic :: Scientific/Engineering :: Physics'],
+    license              = 'License :: OSI Approved :: Apache Software License',
+    packages             = find_packages(),
+    package_data         = {'AA_stat': ['report.template', 'open_search.params', 'default.cfg', 'unimod.xml']},
+    entry_points         = {'console_scripts': ['AA_stat=AA_stat.main:main', 'AA_search=AA_stat.aa_search:main']}
+    )
