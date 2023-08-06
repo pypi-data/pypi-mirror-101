44b694d149c6b4daccf3c421d8d336f7d0716600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:07 +0000

    Add package janehag/0.1.1/janehag-0.1.1-py3-none-any.whl

---
+++ packages/janehag/janehag-0.1.1-py3-none-any.whl/janehag/__init__.py
@@ -0,0 +1,10 @@
+"""Imports all modules in the janehag package."""
+
+__author__ = "Kasper Janehag"
+__version__ = "0.1.1"
+
+import janehag.aws.s3
+import janehag.aws.rds
+import janehag.plot
+import janehag.logging
+import janehag.snowflake
