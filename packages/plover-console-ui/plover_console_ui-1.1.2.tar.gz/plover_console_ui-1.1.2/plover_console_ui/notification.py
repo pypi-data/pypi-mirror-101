9b93df9ccb708d0980b4492ef857af3b1741679d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:48 +0000

    Add package plover-console-ui/1.1.2/plover_console_ui-1.1.2.tar.gz

---
+++ packages/plover-console-ui/plover_console_ui-1.1.2.tar.gz/plover_console_ui-1.1.2/plover_console_ui/notification.py
@@ -0,0 +1,25 @@
+from plover import log
+from plover.log import logging
+
+
+class ConsoleNotificationHandler(logging.Handler):
+    """ Handler using console to show messages. """
+
+    def __init__(self):
+        super().__init__()
+        self.output = None
+        self.setFormatter(logging.Formatter(format))
+
+    def set_output(self, output):
+        self.output = output
+
+    def emit(self, record):
+        if not self.output:
+            return
+        message = self.format(record)
+        if message.endswith("\n"):
+            message = message[:-1]
+        self.output(message)
+
+
+notification_handler = ConsoleNotificationHandler()
