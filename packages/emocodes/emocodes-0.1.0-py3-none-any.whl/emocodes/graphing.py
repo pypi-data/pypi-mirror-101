b3d745e3db1866d44343d2e72451b4c82570799b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:33 +0000

    Add package emocodes/0.1.0/emocodes-0.1.0-py3-none-any.whl

---
+++ packages/emocodes/emocodes-0.1.0-py3-none-any.whl/emocodes/graphing.py
@@ -0,0 +1,104 @@
+import matplotlib.pyplot as plt
+
+# TODO: make function to graph values
+# TODO: add documentation
+
+
+class GraphTimeSeries:
+    
+    def __init__(self, code_series_df):
+        self.codes_series_df = code_series_df
+        self.numeric_cols = None
+        self.categorical_cols = None
+
+    def graph_all(self, num_outfile='numerical_plots', cat_outfile='categorical_plots', file_type='png'):
+        """Fully plot all variables in the dataframe"""
+        self.sort_column_types()
+        self.plot_numeric(num_outfile, file_type)
+        self.plot_categorical(cat_outfile, file_type)
+        return self
+
+    def sort_column_types(self):
+        """Sort columns by their data type for graphing"""
+        df = self.codes_series_df
+        num_vars = []
+        cat_vars = []
+        for x in df.columns:
+            if df[x].dtype == float or df[x].dtype == int:
+                num_vars.append(x)
+            else:
+                cat_vars.append(x)
+        self.numeric_cols = num_vars
+        self.categorical_cols = cat_vars
+        return self
+
+    def plot_numeric(self, outfile_name='numerical_plots', file_type='png'):
+        """Save time plots of all numeric series."""
+        make_num_plot(self.numeric_cols, self.codes_series_df, outfile_name, file_type)
+
+    def plot_categorical(self, outfile_name='categorical_plots', file_type='png'):
+        """Save pie plots of all categorical series."""
+        make_cat_plot(self.categorical_cols, self.codes_series_df, outfile_name, file_type)
+
+
+def make_cat_plot(cat_col_names, codes_df, outfile_name='categorical_plots', file_type='png'):
+    """
+    This function takes the categorical data within a data frame and plots separate pie graphs for each.
+
+    Parameters
+    ----------
+    :param outfile_name: str
+        The name of the out file name for the plots
+
+    :param cat_col_names: list
+        The list of columns for categorical data to be graphed
+
+    :param codes_df: DataFrame
+        The dataframe with the code data
+
+    :param file_type: str
+        Default is 'png'. A string denoting the type of image to save the plot as. Must be 'svg','png', 'pdf', or 'eps'
+
+    :return: None
+    """
+    df = codes_df.loc[:, cat_col_names]
+    fig_height = 4 * len(cat_col_names)
+
+    if len(cat_col_names) >= 1:
+        fig, ax = plt.subplots(len(cat_col_names), figsize=(4, fig_height))
+        for i, x in enumerate(cat_col_names):
+            df[x].value_counts().plot(kind='pie', ax=ax[i], xlabel=' ', ylabel=' ').set_title(x)
+        plt.tight_layout()
+        plt.savefig('{0}.{1}'.format(outfile_name, file_type))
+    else:
+        print('Nothing to graph!')
+
+def make_num_plot(num_col_names, codes_df, outfile_name='numerical_plots', file_type='png'):
+    """
+    This function takes the numeric data within a data frame and plots separate line graphs for each.
+
+    Parameters
+    ---------
+    :param outfile_name: str
+        The name of the file to save the image plot as.
+
+    :param num_col_names: list
+        The list of columns for numeric data to be graphed
+
+    :param codes_df: DataFrame
+        The dataframe with the code data
+
+    :param file_type: str
+        Default is 'png'. A string denoting the type of image to save the plot as. Must be 'svg','png', 'pdf', or 'eps'
+
+    :returns: None
+    --------
+    """
+
+    df = codes_df.loc[:, num_col_names]
+    df.resample('1s')
+    fig_height = 1.5 * len(num_col_names)
+    plt.figure()
+    df.plot.line(figsize=(12, fig_height), title=num_col_names, subplots=True, legend=False, xlim=(0, df.index[-1]))
+    plt.tight_layout()
+    plt.savefig('{0}.{1}'.format(outfile_name, file_type))
---
+++ packages/emocodes/emocodes-0.1.0-py3-none-any.whl/emocodes/video.py
@@ -0,0 +1,76 @@
+# import needed libraries
+import pandas as pd
+import numpy as np
+
+# TODO: add function on edginess on image
+# TODO: make video processing class
+
+
+def get_video_length(video_file):
+    """
+    This function checks the length of a video file and returns that value in milliseconds.
+
+    Parameters
+    ----------
+    video_file : str
+        The path to the video file that was coded
+
+    Return
+    ------
+    file_duration : int
+        The duration of the file in milliseconds
+    """
+    from moviepy.editor import VideoFileClip
+    
+    clip = VideoFileClip(video_file)
+    file_duration = int(clip.duration*1000)
+    return file_duration
+
+
+def compute_luminance(video_file, sampling_rate, video_duration):
+    """
+    This function produces a Series of the luminance values for a video file.
+
+    Parameters
+    ----------
+
+    video_file : str
+        the video file to be processed
+
+    sampling_rate : int
+        desired sampling rate of outputs in Hz
+
+    video_duration : int
+        length of video in milliseconds
+
+    Return
+    ------
+    lum_df : DataFrame
+        The Pandas dataframe of the luminance measure, resampled according to the user input.
+    """
+
+    import cv2
+
+    video = cv2.VideoCapture(video_file)
+    frames_lum = []
+    rgb = []
+
+    end = False
+    while not end:
+        r, f = video.read()
+        if r == 1:
+            t = f.mean(axis=0).mean(axis=0)
+            lum = 0.299*t[0] + 0.587*t[1] + 0.114*t[2]
+            rgb.append(t)
+            frames_lum.append(lum)
+        else:
+            end = True
+
+    fps = (len(frames_lum)*1000)/video_duration
+    a = np.arange(0, (len(frames_lum)/fps)*1000, 1000/fps)
+    b = pd.DataFrame(frames_lum, index=pd.to_datetime(a, unit='ms'), columns=['luminance'])
+    b[['R','G','B']] = rgb
+    b.index.name = 'time'
+    lum_df = b.resample('{0}ms'.format(1000/sampling_rate)).mean()
+
+    return lum_df
