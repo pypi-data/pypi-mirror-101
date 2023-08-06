0c9301c13067fc7e6e565e32e3d731584da640ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:37 +0000

    Add package demographic-modeling/0.3.0/demographic-modeling-0.3.0.tar.gz

---
+++ packages/demographic-modeling/demographic-modeling-0.3.0.tar.gz/demographic-modeling-0.3.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setup(
+    name='demographic-modeling',
+    package_dir={"": "src"},
+    packages=find_packages('src',  include=['modeling']),
+    version='0.3.0',
+    description='Demographic Modeling',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Joel McCune',
+    license='Apache 2.0',
+)
