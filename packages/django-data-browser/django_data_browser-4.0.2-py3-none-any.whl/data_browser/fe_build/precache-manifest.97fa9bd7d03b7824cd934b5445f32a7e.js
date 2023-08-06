f20edf01d6501a999972454eae9322b378058dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:01 +0000

    Add package django-data-browser/4.0.2/django_data_browser-4.0.2-py3-none-any.whl

---
+++ packages/django-data-browser/django_data_browser-4.0.2-py3-none-any.whl/data_browser/fe_build/precache-manifest.97fa9bd7d03b7824cd934b5445f32a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179ce4d25dc58d523d0db5024ad0e2ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "991e68380a0f7b7dab54",
+    "url": "./static/css/main.642522e8.chunk.css"
+  },
+  {
+    "revision": "6b4e59d71c0d8ede848a",
+    "url": "./static/js/2.d314ae20.chunk.js"
+  },
+  {
+    "revision": "66486fd8359f40be208121c37c8c9200",
+    "url": "./static/js/2.d314ae20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "991e68380a0f7b7dab54",
+    "url": "./static/js/main.07949942.chunk.js"
+  },
+  {
+    "revision": "aba697584663a7cb794c",
+    "url": "./static/js/runtime-main.c3c9eabf.js"
+  }
+]);
