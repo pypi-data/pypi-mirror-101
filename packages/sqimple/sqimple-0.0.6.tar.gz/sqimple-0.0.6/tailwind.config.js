36be7bafc080348edec6326c6f87eac19bb52b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:45 +0000

    Add package sqimple/0.0.6/sqimple-0.0.6.tar.gz

---
+++ packages/sqimple/sqimple-0.0.6.tar.gz/sqimple-0.0.6/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['sqimple/templates/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
