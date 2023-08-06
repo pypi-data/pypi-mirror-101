9a2005a8bfd3091c0cdbb7bee69fa04b578b0746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:18 +0000

    Add package singular-server-updater/0.0.5/singular_server_updater-0.0.5.tar.gz

---
+++ packages/singular-server-updater/singular_server_updater-0.0.5.tar.gz/singular_server_updater-0.0.5/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name='singular_server_updater',
+    version='0.0.5',
+    author='Singular Sistemas',
+    author_email='ivan@singular.inf.br',
+    description='Server updater',
+    long_description='Server updater',
+    url='https://lucas@bitbucket.org/singular-dev/server_updater.git',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        'django',
+        'fabric',
+        'invoke',
+        'django-crontab',
+        'singular_dbbackup_sender'
+    ]
+)
