982136e756284935a3164ee906d636690a417423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:28 +0000

    Add package searchpods/0.1.1/searchpods-0.1.1-py3-none-any.whl

---
+++ packages/searchpods/searchpods-0.1.1-py3-none-any.whl/searchpods/__init__.py
@@ -0,0 +1,23 @@
+"""Let's find some podcasts!"""
+
+__version__ = "0.1.1"
+
+SEARCH_URL = "https://itunes.apple.com/search"
+
+@dataclass
+class Podcast:
+    """Podcast metadata."""
+
+    id: str
+    name: str
+    author: str
+    url: str
+    feed: Optional[str] = None
+    category: Optional[str] = None
+    image: Optional[str] = None
+
+def search(name: str, limit: int = 5):
+    """Search podcast by name."""
+    params = {"term": name, "limit": limit, "media": "podcast"}
+    response = _get(url=SEARCH_URL, params=params)
+    return _parse(response)
