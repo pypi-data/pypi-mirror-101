c45018c9ce73a499492a4026a66c85d64d53618f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:30 +0000

    Add package mldoe/0.0.1/mldoe-0.0.1-py3-none-any.whl

---
+++ packages/mldoe/mldoe-0.0.1-py3-none-any.whl/design.py
@@ -0,0 +1,14 @@
+# -*-coding: utf-8-*-
+"""
+File defining the design (meta)class and the two classes:
+- Two-level designs
+- Mixed-level designs (four- and two-levels)
+"""
+
+
+class design():
+    def __init__(self, N):
+        pass
+
+    def __repr__(self):
+        return 'Design object'
