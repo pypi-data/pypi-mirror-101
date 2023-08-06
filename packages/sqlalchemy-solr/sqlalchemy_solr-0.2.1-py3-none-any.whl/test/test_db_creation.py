4313a59593222afe3e3759b080b98f83ac203401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:33 +0000

    Add package sqlalchemy-solr/0.2.1/sqlalchemy_solr-0.2.1-py3-none-any.whl

---
+++ packages/sqlalchemy-solr/sqlalchemy_solr-0.2.1-py3-none-any.whl/test/test_db_creation.py
@@ -0,0 +1,13 @@
+import os
+import json
+from requests import Session
+from .steps import TestSteps
+
+class TestDBCreation:
+
+    def test_db_creation(self, settings):
+        test_steps = TestSteps(settings)
+        session = Session()
+        headers = test_steps.login(session)
+        db_creation_response = test_steps.create_database(session, headers)
+        assert db_creation_response.status_code == 201
