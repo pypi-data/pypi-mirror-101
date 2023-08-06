cf7d417b0146e560c2e34afedd09b32a301eae26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:27 +0000

    Add package py-ebooktools/0.1.0a2/py_ebooktools-0.1.0a2-py3-none-any.whl

---
+++ packages/py-ebooktools/py_ebooktools-0.1.0a2-py3-none-any.whl/py_ebooktools/lib.py
@@ -0,0 +1,12 @@
+"""Library that has useful functions for building other ebook management tools.
+
+This is a Python port of `lib.sh`_ from `ebook-tools`_ written in Shell by
+`na--`_.
+
+.. URLs
+
+.. external links
+.. _ebook-tools: https://github.com/na--/ebook-tools
+.. _lib.sh: https://github.com/na--/ebook-tools/blob/master/lib.sh
+.. _na--: https://github.com/na--
+"""
