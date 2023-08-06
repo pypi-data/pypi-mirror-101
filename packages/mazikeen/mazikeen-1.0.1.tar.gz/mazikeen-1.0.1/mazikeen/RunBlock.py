db9401c7f0f8c2f3ede612402c3f7075a2f5f7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:47 +0000

    Add package mazikeen/1.0.1/mazikeen-1.0.1.tar.gz

---
+++ packages/mazikeen/mazikeen-1.0.1.tar.gz/mazikeen-1.0.1/mazikeen/RunBlock.py
@@ -0,0 +1,36 @@
+import shlex
+import subprocess
+import os
+import pathlib
+
+from mazikeen.ConsolePrinter import Printer
+from mazikeen.Utils import replaceVariables, ensure_dir
+
+class RunBlock:
+    def __init__(self, cmd, outputfile = None, inputfile = None, exitcode = 0):
+        self.cmd = cmd
+        self.outputfile = outputfile
+        self.inputfile = inputfile
+        self.exitcode = exitcode
+
+    def run(self, workingDir = ".", variables = {}, printer = Printer()):
+        replCmd = replaceVariables(self.cmd, variables)
+        cmdNArgs = shlex.split(replCmd)
+        printer.verbose("cwd:", os.getcwd())
+        printer.verbose("call:", replCmd)
+        inputfileData = None
+        if self.inputfile:
+            with open(pathlib.PurePath(workingDir).joinpath(self.inputfile), "rb") as fh:
+                inputfileData = fh.read()
+        subProcessRes = subprocess.run(cmdNArgs, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, input=inputfileData, cwd = workingDir)
+        if self.outputfile:
+            outputfileFullPath = str(pathlib.PurePath(workingDir).joinpath(self.outputfile))
+            ensure_dir(outputfileFullPath)
+            with open(outputfileFullPath, "wb") as fh:
+                fh.write(subProcessRes.stdout)
+           
+        res = subProcessRes.returncode == self.exitcode
+        if not res:
+            printer.error("different exitcode received:", subProcessRes.returncode, "!=", self.exitcode, "for command '"+ str(replCmd) +"'")
+        return res
+     
