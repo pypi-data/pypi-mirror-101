fa7e3f444367b00ced47a14e7399dc4d2e957ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:09 +0000

    Add package python-ecb-daily/0.1/python-ecb-daily-0.1.tar.gz

---
+++ packages/python-ecb-daily/python-ecb-daily-0.1.tar.gz/python-ecb-daily-0.1/ecb/constants.py
@@ -0,0 +1,38 @@
+"""
+    RSS Mappings for crawling rates from european central bank
+"""
+
+CURRENCY_RSS_MAPS = {
+    "USD": "https://www.ecb.europa.eu/rss/fxref-usd.html",
+    "JPY": "https://www.ecb.europa.eu/rss/fxref-jpy.html",
+    "BGN": "https://www.ecb.europa.eu/rss/fxref-bgn.html",
+    "CZK": "https://www.ecb.europa.eu/rss/fxref-czk.html",
+    "DKK": "https://www.ecb.europa.eu/rss/fxref-dkk.html",
+    "EEK": "https://www.ecb.europa.eu/rss/fxref-eek.html",
+    "GBP": "https://www.ecb.europa.eu/rss/fxref-gbp.html",
+    "HUF": "https://www.ecb.europa.eu/rss/fxref-huf.html",
+    "PLN": "https://www.ecb.europa.eu/rss/fxref-pln.html",
+    "RON": "https://www.ecb.europa.eu/rss/fxref-ron.html",
+    "SEK": "https://www.ecb.europa.eu/rss/fxref-sek.html",
+    "CHF": "https://www.ecb.europa.eu/rss/fxref-chf.html",
+    "ISK": "https://www.ecb.europa.eu/rss/fxref-isk.html",
+    "NOK": "https://www.ecb.europa.eu/rss/fxref-nok.html",
+    "HRK": "https://www.ecb.europa.eu/rss/fxref-hrk.html",
+    "RUB": "https://www.ecb.europa.eu/rss/fxref-rub.html",
+    "TRY": "https://www.ecb.europa.eu/rss/fxref-try.html",
+    "AUD": "https://www.ecb.europa.eu/rss/fxref-aud.html",
+    "BRL": "https://www.ecb.europa.eu/rss/fxref-brl.html",
+    "CAD": "https://www.ecb.europa.eu/rss/fxref-cad.html",
+    "CNY": "https://www.ecb.europa.eu/rss/fxref-cny.html",
+    "HKD": "https://www.ecb.europa.eu/rss/fxref-hkd.html",
+    "IDR": "https://www.ecb.europa.eu/rss/fxref-idr.html",
+    "INR": "https://www.ecb.europa.eu/rss/fxref-inr.html",
+    "KRW": "https://www.ecb.europa.eu/rss/fxref-krw.html",
+    "MXN": "https://www.ecb.europa.eu/rss/fxref-mxn.html",
+    "MYR": "https://www.ecb.europa.eu/rss/fxref-myr.html",
+    "NZD": "https://www.ecb.europa.eu/rss/fxref-nzd.html",
+    "PHP": "https://www.ecb.europa.eu/rss/fxref-php.html",
+    "SGD": "https://www.ecb.europa.eu/rss/fxref-sgd.html",
+    "THB": "https://www.ecb.europa.eu/rss/fxref-thb.html",
+    "ZAR": "https://www.ecb.europa.eu/rss/fxref-zar.html"
+}
