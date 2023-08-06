ba5d9bebb4a1ef08d1db4500ea07acde8a232f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:04 +0000

    Add package djmyframework/0.1.9/djmyframework-0.1.9.tar.gz

---
+++ packages/djmyframework/djmyframework-0.1.9.tar.gz/djmyframework-0.1.9/djmyframework/apps/svn_admin/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020-07-07 15:43
+# @Author  : xzr
+# @File    : settings
+# @Software: PyCharm
+# @Contact : xzregg@gmail.com
+# @Desc    :
+
+import os
+
+SVN_ROOT = '/Users/xzr/Desktop/svn'
+
+# 对应  svnserve.conf  password-db 文件路径
+SVN_PASSWORD_DB_FILE = os.path.join(SVN_ROOT, 'conf', 'password.ini')
+# 对应  svnserve.conf authz-db 文件路径
+SVN_AUTH_DB_FILE = os.path.join(SVN_ROOT, 'conf', 'authz.ini')
+SVN_GROUP_DB_FILE = os.path.join(SVN_ROOT, 'conf', 'groups.ini')
