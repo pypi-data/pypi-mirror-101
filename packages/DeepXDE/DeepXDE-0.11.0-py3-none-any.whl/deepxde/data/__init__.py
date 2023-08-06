03436cb0b51e33b3ac6ec4310900a7fa609b8ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:36 +0000

    Add package DeepXDE/0.11.0/DeepXDE-0.11.0-py3-none-any.whl

---
+++ packages/DeepXDE/DeepXDE-0.11.0-py3-none-any.whl/deepxde/data/__init__.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import
+
+from .dataset import DataSet
+from .fpde import FPDE
+from .fpde import TimeFPDE
+from .func import Func
+from .func_constraint import FuncConstraint
+from .ide import IDE
+from .mf import MfDataSet
+from .mf import MfFunc
+from .mfopdataset import MfOpDataSet
+from .op_dataset import OpDataSet
+from .pde import PDE
+from .pde import TimePDE
+
+
+__all__ = [
+    "DataSet",
+    "FPDE",
+    "Func",
+    "FuncConstraint",
+    "IDE",
+    "MfDataSet",
+    "MfFunc",
+    "MfOpDataSet",
+    "OpDataSet",
+    "PDE",
+    "TimeFPDE",
+    "TimePDE",
+]
---
+++ packages/DeepXDE/DeepXDE-0.11.0-py3-none-any.whl/deepxde/maps/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+from .fnn import FNN
+from .mfnn import MfNN
+from .mfonet import MfONet
+from .opnn import OpNN
+from .pfnn import PFNN
+from .resnet import ResNet
+
+
+__all__ = ["FNN", "MfNN", "MfONet", "OpNN", "PFNN", "ResNet"]
