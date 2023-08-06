a89f4df66e61f16fa606bb3d05724a111b542fe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:29 +0000

    Add package ktextaug/0.1.8rc2/ktextaug-0.1.8rc2-py3-none-any.whl

---
+++ packages/ktextaug/ktextaug-0.1.8rc2-py3-none-any.whl/ktextaug/transformative/utils.py
@@ -0,0 +1,57 @@
+"""
+Author : Jung Hoon, Lee
+Editor : Jin Uk, Cho
+Last update : 12th, Apr, 2020
+"""
+import requests
+from bs4 import BeautifulSoup
+import random
+
+from ..file_utils import open_text
+import os
+# path = os.path.join(os.path.curdir, "../stopwords-ko.txt")
+
+# path = os.path.abspath("ktextaug/stopwords-ko.txt")
+# print("exist?", os.path.exists(path)) # /home/jucho
+# /PythonProjects/textaug/kTextAugmentation/ktextaug/stopwords-ko.txt
+# print("same?", "/home/jucho/PythonProjects/textaug/kTextAugmentation/ktextaug/stopwords-ko.txt" == path)
+# print(path)
+# print(f"cur dir {os.path.curdir}")
+# print(f"list dir of cur dir{os.listdir(os.path.curdir)}")
+# stopwords = open_text(path)
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+
+print(__package__)
+stopwords = pkg_resources.read_text(__package__, 'stopwords-ko.txt')
+print(stopwords)
+
+def isWord(word):
+    return word.isalnum()
+
+def isStopword(word):
+    if word in stopwords:
+        return True
+    else:
+        return False
+
+def get_synonym(word):
+    relate_list = []
+    res = requests.get("https://dic.daum.net/search.do?q=" + word)
+    soup = BeautifulSoup(res.content, "html.parser")
+    try:
+        word_id = soup.find("ul", class_="list_relate")
+    except AttributeError:
+        return word
+    if word_id == None:
+        return word
+    for tag in word_id.find_all("a"):
+        relate_list.append(tag.text)
+
+    return random.choice(relate_list)
+
+if __name__ == "__main__":
+    print(isStopword("아홉"))
