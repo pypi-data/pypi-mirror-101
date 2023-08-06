b43ee014b439f601fe0e9831cfb5cc02bf834f0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:22 +0000

    Add package blankly/0.1.8a0/Blankly-0.1.8a0.tar.gz

---
+++ packages/blankly/Blankly-0.1.8a0.tar.gz/Blankly-0.1.8a0/Blankly/__init__.py
@@ -0,0 +1,27 @@
+"""
+    __init__ file to give the module access to the libraries.
+    Copyright (C) 2021  Emerson Dove
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from Blankly.Coinbase_Pro.Coinbase_Pro import Coinbase_Pro as Coinbase_Pro
+from Blankly.Binance.Binance import Binance as Binance
+from Blankly.API_Interface import APIInterface as Interface
+from Blankly.blankly_bot import BlanklyBot
+
+from Blankly.Coinbase_Pro.Coinbase_Pro_API import API as Direct_Coinbase_Pro_API
+from Blankly.ticker_manager import TickerManager as TickerManager
+from Blankly.Coinbase_Pro.orderbook import OrderBook as Coinbase_Pro_OrderBook
+import Blankly.utils
