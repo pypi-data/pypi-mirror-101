782c79faf8f9757945af7f131feb9eb1ad0fe85c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:19 +0000

    Add package epix/0.1.0/epix-0.1.0.tar.gz

---
+++ packages/epix/epix-0.1.0.tar.gz/epix-0.1.0/epix/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.1.0'
+
+from .common import *
+from .io import *
+from .clustering import *
+from .quanta_generation import *
+from .detector_volumes import *
+from .clustering import *
+from .electric_field_handler import *
+from .event_separation import *
+from .detectors import *
+from .run_epix import *
