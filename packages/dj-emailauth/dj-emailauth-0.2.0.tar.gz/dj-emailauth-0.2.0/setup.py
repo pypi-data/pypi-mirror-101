10b06d1c1e18ae69b690d6e79ff2b62057e0524f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:29 +0000

    Add package dj-emailauth/0.2.0/dj-emailauth-0.2.0.tar.gz

---
+++ packages/dj-emailauth/dj-emailauth-0.2.0.tar.gz/dj-emailauth-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dj_emailauth', 'dj_emailauth.migrations']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Django>=3.0,<4.0']
+
+setup_kwargs = {
+    'name': 'dj-emailauth',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Martin Hloska',
+    'author_email': 'martin.hloska@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
