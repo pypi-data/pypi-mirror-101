cd12dfaff5cb4ca96de8a631e2eae83883147bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:33 +0000

    Add package sql22json/0.0.0/sql22json-0.0.0.tar.gz

---
+++ packages/sql22json/sql22json-0.0.0.tar.gz/sql22json-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sql22json',
+    license='MIT',
+    author='Gabriel Fr. Borges',
+    author_email='gabrielfr.borges@gmail.com',
+    url='https://github.com/gfborges/sql22json',
+    packages=find_packages(),
+    entry_points= {
+        'console_scripts':{
+            'sql22json = sql22json.main:main'
+        }
+    }
+)
