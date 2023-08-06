44073a9aedac71723516e1d33acb03530920be4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:17 +0000

    Add package toolchain.pants.plugin/0.8.0/toolchain.pants.plugin-0.8.0.tar.gz

---
+++ packages/toolchain.pants.plugin/toolchain.pants.plugin-0.8.0.tar.gz/toolchain.pants.plugin-0.8.0/src/toolchain/pants/register.py
@@ -0,0 +1,20 @@
+# Copyright © 2019 Toolchain Labs, Inc. All rights reserved.
+#
+# Toolchain Labs, Inc. CONFIDENTIAL
+#
+# This file includes unpublished proprietary source code of Toolchain Labs, Inc.
+# The copyright notice above does not evidence any actual or intended publication of such source code.
+# Disclosure of this source code or any related proprietary information is strictly prohibited without
+# the express written permission of Toolchain Labs, Inc.
+
+from toolchain.pants.auth.rules import get_auth_rules
+from toolchain.pants.buildsense.reporter import rules as reporter_rules
+from toolchain.pants.util.setup_py import toolchain_setup_rules
+
+
+def rules():
+    return (
+        *toolchain_setup_rules(),
+        *get_auth_rules(),
+        *reporter_rules(),
+    )
