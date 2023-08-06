2b1bb3cc869b030f7757a119f417918e74730859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:36 +0000

    Add package doubt/2.0.2/doubt-2.0.2-py3-none-any.whl

---
+++ packages/doubt/doubt-2.0.2-py3-none-any.whl/doubt/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '2.0.2' # noqa
+
+from .models import QuantileRegressionTree # noqa
+from .models import QuantileRegressionForest # noqa
+from .models import QuantileLinearRegression # noqa
+from .models import Boot # noqa
