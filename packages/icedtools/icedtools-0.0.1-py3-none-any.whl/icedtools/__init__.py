0b17edf4fff121a28e41c2776a65fa891b1d16bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:17 +0000

    Add package icedtools/0.0.1/icedtools-0.0.1-py3-none-any.whl

---
+++ packages/icedtools/icedtools-0.0.1-py3-none-any.whl/icedtools/__init__.py
@@ -0,0 +1,12 @@
+"""Let's write some code"""
+
+__version__ = "0.0.1"
+
+
+def ping(msg):
+    """Silly method!
+    >>> import icedtools
+    >>> icedtools.ping("hi")
+    'hi'
+    """
+    return msg
