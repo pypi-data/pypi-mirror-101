a50b21ae89e846404265433c15c2456d3a77f1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:26 +0000

    Add package boastlabs-cloud-functions/1.0.2/boastlabs-cloud-functions-1.0.2.tar.gz

---
+++ packages/boastlabs-cloud-functions/boastlabs-cloud-functions-1.0.2.tar.gz/boastlabs-cloud-functions-1.0.2/boastlabs/functions/retriable/execution/config.py
@@ -0,0 +1,9 @@
+class Status:
+    NEW = 'new'
+    RUNNING = 'running'
+    FAILED = 'failed'
+    DONE = 'done'
+    SLEEP = 'sleep'
+
+    WAITING_RETRY = 'waiting_retry'
+    WAITING_SLEEP = 'waiting_sleep'
