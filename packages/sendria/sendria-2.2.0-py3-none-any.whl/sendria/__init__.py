c987e2357deadb65e6e2fe351e26ee20d331c22f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:46 +0000

    Add package sendria/2.2.0/sendria-2.2.0-py3-none-any.whl

---
+++ packages/sendria/sendria-2.2.0-py3-none-any.whl/sendria/__init__.py
@@ -0,0 +1,15 @@
+__all__ = ['__version__']
+
+import sys
+from typing import NoReturn
+
+import structlog
+
+__version__ = '2.2.0'
+
+logger = structlog.get_logger()
+
+
+def exit_err(msg: str, exit_code: int = 1, **kwargs) -> NoReturn:
+    logger.error(msg, **kwargs)
+    sys.exit(exit_code)
