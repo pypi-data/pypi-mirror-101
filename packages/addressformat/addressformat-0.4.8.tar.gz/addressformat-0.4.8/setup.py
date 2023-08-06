490f7af8801442238024a15b11f379f486a5f0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:26 +0000

    Add package addressformat/0.4.8/addressformat-0.4.8.tar.gz

---
+++ packages/addressformat/addressformat-0.4.8.tar.gz/addressformat-0.4.8/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+import addressformat
+
+requires=['jieba','triedtree']
+
+setup(name='addressformat',
+      version=addressformat.__version__,
+      install_requires = requires,
+      description='地址省市区解析',
+      long_description="地址解析模块",
+      license="MIT",
+      classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Indexing',
+      ],
+      packages=['addressformat', 'addressformat.resources','addressformat.entitywords'],
+
+      package_dir={'addressformat': 'addressformat', 'addressformat.resources': 'addressformat/resources','addressformat.entitywords':"addressformat/entitywords"},
+      package_data={'': ['*']},
+      include_package_data=True,
+      )
