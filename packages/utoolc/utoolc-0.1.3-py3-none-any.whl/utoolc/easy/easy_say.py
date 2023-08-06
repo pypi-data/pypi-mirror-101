d3b3eb23afc23b308515aebd7eb7f15867049bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:43 +0000

    Add package utoolc/0.1.3/utoolc-0.1.3-py3-none-any.whl

---
+++ packages/utoolc/utoolc-0.1.3-py3-none-any.whl/utoolc/easy/easy_say.py
@@ -0,0 +1,13 @@
+"""
+easy_say.py
+简单说
+"""
+
+
+# 简单说你好,世界
+# @param name 传入的datetime
+# @return msg 要说的你好世界
+def say_hello_world(name):
+    msg = 'Hello World Hello ' + str(name)
+    print(msg)
+    return msg
