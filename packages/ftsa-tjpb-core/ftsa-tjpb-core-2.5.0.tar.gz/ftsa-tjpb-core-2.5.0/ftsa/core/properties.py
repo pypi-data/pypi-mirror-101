054ca0315fa388b2c825c1a4c1777f8ac8077c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:02 +0000

    Add package ftsa-tjpb-core/2.5.0/ftsa-tjpb-core-2.5.0.tar.gz

---
+++ packages/ftsa-tjpb-core/ftsa-tjpb-core-2.5.0.tar.gz/ftsa-tjpb-core-2.5.0/ftsa/core/properties.py
@@ -0,0 +1,12 @@
+PACKAGE_NAME            = 'ftsa-tjpb-core'
+SLEEP_SECONDS           = 5
+REMOTE_VERSION          = '4.0.0-beta-3-prerelease-20210321'
+REC_VIDEO_VERSION       = 'ffmpeg-4.3.1-20210321'
+VERSION                 = '2.5.0'
+TEST_PYPI               = False
+
+FTSA_SELENIUM_LIB_NAME  = 'FTSASeleniumLibrary'
+FTSA_SSH_LIB_NAME       = 'FTSASSHLibrary'
+FTSA_DATABASE_LIB_NAME  = 'FTSADatabaseLibrary'
+FTSA_APPIUM_LIB_NAME    = 'FTSAAppiumLibrary'
+FTSA_REQUESTS_LIB_NAME  = 'FTSARequestsLibrary'
