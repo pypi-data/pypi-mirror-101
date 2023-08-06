799ba520ffc79bafe0d6a9c72ca0d8fa6df379a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:25 +0000

    Add package py-stopwatch/0.0.1/py_stopwatch-0.0.1-py2.py3-none-any.whl

---
+++ packages/py-stopwatch/py_stopwatch-0.0.1-py2.py3-none-any.whl/stopwatch/__init__.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 12 21:22:49 2021
+
+@author: Hrishikesh Terdalkar
+"""
+
+###############################################################################
+
+__author__ = """Hrishikesh Terdalkar"""
+__email__ = 'hrishikeshrt@linuxmail.org'
+__version__ = '0.0.1'
+
+###############################################################################
+
+from .stopwatch import Stopwatch
