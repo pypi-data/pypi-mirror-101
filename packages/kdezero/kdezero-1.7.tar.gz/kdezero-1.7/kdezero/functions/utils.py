751a92c23418fe7fa061cbfdc97c75cd2c4e8c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:57 +0000

    Add package kdezero/1.7/kdezero-1.7.tar.gz

---
+++ packages/kdezero/kdezero-1.7.tar.gz/kdezero-1.7/kdezero/functions/utils.py
@@ -0,0 +1,45 @@
+import kdezero
+from kdezero import as_variable, as_array, Variable, Function
+from kdezero import cuda
+from kdezero.functions.ndarray_functions import matmul, sum_to
+
+
+class Linear(Function):
+    def forward(self, x, W, b):
+        y = x.dot(W)
+        if b is not None:
+            y += b
+        return y
+
+    def backward(self, gy):
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
+
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
+
+
+def accuracy(y, t):
+    y, t = as_variable(y), as_variable(t)
+
+    pred = y.data.argmax(axis=1).reshape(t.shape)
+    result = (pred == t.data)
+    acc = result.mean()
+    return Variable(as_array(acc))
+
+
+def dropout(x, dropout_ratio=0.5):
+    x = as_variable(x)
+
+    if kdezero.Config.train:
+        xp = cuda.get_array_module(x)
+        mask = xp.random.rand(*x.shape) > dropout_ratio
+        scale = xp.array(1.0 - dropout_ratio).astype(x.dtype)
+        y = x * mask / scale
+        return y
+    else:
+        return x
