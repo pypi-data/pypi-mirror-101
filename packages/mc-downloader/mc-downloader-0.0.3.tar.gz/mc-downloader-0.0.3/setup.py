e3fd9539ebe1dec4c007e216101bafc1723eea48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:39 +0000

    Add package mc-downloader/0.0.3/mc-downloader-0.0.3.tar.gz

---
+++ packages/mc-downloader/mc-downloader-0.0.3.tar.gz/mc-downloader-0.0.3/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as fr:
+    requirements = fr.read().splitlines()
+
+setuptools.setup(
+    name="mc-downloader",
+    version="0.0.3",
+    license="MIT",
+
+    author="Couapy",
+    author_email="contact@marchand.cloud",
+
+    description="A downloader tool for Minecraft servers from Mojang services.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/Couapy/mc-downloader",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
