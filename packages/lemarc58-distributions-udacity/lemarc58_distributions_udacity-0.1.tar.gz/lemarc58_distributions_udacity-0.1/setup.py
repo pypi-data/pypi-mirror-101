e1565f8d654101311a4d56cb7b0fa304fc7a7880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:17 +0000

    Add package lemarc58-distributions-udacity/0.1/lemarc58_distributions_udacity-0.1.tar.gz

---
+++ packages/lemarc58-distributions-udacity/lemarc58_distributions_udacity-0.1.tar.gz/lemarc58_distributions_udacity-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='lemarc58_distributions_udacity',
+      version='v0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
