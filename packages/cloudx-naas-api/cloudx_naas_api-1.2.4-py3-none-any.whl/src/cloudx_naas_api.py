521c97e48977b5894bc95cc38764f273311d522c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:15 +0000

    Add package cloudx-naas-api/1.2.4/cloudx_naas_api-1.2.4-py3-none-any.whl

---
+++ packages/cloudx-naas-api/cloudx_naas_api-1.2.4-py3-none-any.whl/src/cloudx_naas_api.py
@@ -0,0 +1,39 @@
+import logging
+import uuid
+
+network = "cloudx-edge-us-8syz4"
+user = "geneleblanc"
+userFullname = "Gene LeBlanc"
+
+logger = logging.getLogger()
+
+def deploy_slice(profile, nw_location, tags=[]):
+    """
+    CloudX NaaS slide deployment function with following parameters:    
+        profil         Name of slice profile e.g. lowlatency, 
+                       highbandwidth. please use CloudX NaaS API to define profiles.
+        nw_location    List of network locations for slice deployment 
+        tags           Custom tags to be configured next to de slice deployment. 
+                       The default tag:Name could be set for naming the deployed instance
+    """
+    message = f'Slice deployed on network {network} with {profile} for user {userFullname}'
+
+    logger.info(message)
+    
+    return uuid.uuid4()
+
+
+def undeploy_slice(id) :
+    """
+    CloudX NaaS slide undeployment function 
+        id    Slice Id in UUID format to undeploy
+    """
+
+    return True
+
+def main_f():
+    deploy_slice(profile="hdvideo")
+
+
+if __name__ == "__main__":
+    main_f()
