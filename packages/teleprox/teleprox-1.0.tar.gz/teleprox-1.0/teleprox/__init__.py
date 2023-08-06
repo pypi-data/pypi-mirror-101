92c0ffd4efe9663def54ce123f3eaf7e56e9c850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:12 +0000

    Add package teleprox/1.0/teleprox-1.0.tar.gz

---
+++ packages/teleprox/teleprox-1.0.tar.gz/teleprox-1.0/teleprox/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016, French National Center for Scientific Research (CNRS)
+# Distributed under the (new) BSD License. See LICENSE for more info.
+
+from .client import RPCClient, RemoteCallException, Future
+from .server import RPCServer
+from .qt_server import QtRPCServer
+from .proxy import ObjectProxy
+from .processspawner import ProcessSpawner
