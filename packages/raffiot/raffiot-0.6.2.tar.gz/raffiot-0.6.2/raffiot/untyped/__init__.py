dd4fe814d45b911e4bed5243687d7f666aac6ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:14 +0000

    Add package raffiot/0.6.2/raffiot-0.6.2.tar.gz

---
+++ packages/raffiot/raffiot-0.6.2.tar.gz/raffiot-0.6.2/raffiot/untyped/__init__.py
@@ -0,0 +1,33 @@
+from raffiot.untyped import io, resource, result
+from raffiot.untyped.io import IO
+from raffiot.untyped.resource import Resource
+from raffiot.untyped.result import Result, Ok, Errors, Panic
+from raffiot.untyped.utils import (
+    MatchError,
+    MultipleExceptions,
+    ComputationStatus,
+    seq,
+    TracedException,
+)
+from raffiot.untyped.val import Val
+from raffiot.untyped.var import Var, UpdateResult
+
+__all__ = [
+    "io",
+    "resource",
+    "result",
+    "TracedException",
+    "MatchError",
+    "MultipleExceptions",
+    "ComputationStatus",
+    "seq",
+    "Result",
+    "Ok",
+    "Errors",
+    "Panic",
+    "IO",
+    "Resource",
+    "Val",
+    "Var",
+    "UpdateResult",
+]
