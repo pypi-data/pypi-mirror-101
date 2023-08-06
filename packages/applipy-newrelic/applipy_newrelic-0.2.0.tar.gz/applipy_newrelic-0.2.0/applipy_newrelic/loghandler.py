13eb8780d9f95c2efa909bd040b71079e69508e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:38 +0000

    Add package applipy-newrelic/0.2.0/applipy_newrelic-0.2.0.tar.gz

---
+++ packages/applipy-newrelic/applipy_newrelic-0.2.0.tar.gz/applipy_newrelic-0.2.0/applipy_newrelic/loghandler.py
@@ -0,0 +1,23 @@
+from logging import Handler
+
+from applipy import Config
+
+from newrelic_telemetry_sdk import (
+    Log,
+    LogClient,
+)
+
+
+class NewRelicLogHandler(Handler):
+
+    _client: LogClient
+
+    def __init__(self, client: LogClient, config: Config) -> None:
+        super().__init__()
+        self._client = client
+        self._app_name = config.get('app.name')
+
+    def emit(self, record):
+        log = Log.from_record(record)
+        log['app_name'] = self._app_name
+        self._client.send(log).raise_for_status()
