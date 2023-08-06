32c4c08d1c4423c7d680d40f2573f2d4d9124f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:11 +0000

    Add package wispyjim/1.3.6/wispyjim-1.3.6.tar.gz

---
+++ packages/wispyjim/wispyjim-1.3.6.tar.gz/wispyjim-1.3.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wispyjim",
+    version="1.3.6",
+    author="JimTheScientist",
+    author_email="aidannpratt@gmail.com",
+    description="A tiny Python package to interect with the WISP.gg API, modified by JimTheScientist",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JimTheScientist/wisp.py",
+    packages=setuptools.find_packages(),
+    keywords = ['wisp', 'api-wrapper', 'api', 'gaming', 'wrapper'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        "Operating System :: OS Independent",
+    ],
+    python_requires='!=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+)
