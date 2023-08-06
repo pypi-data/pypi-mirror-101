341f440a50364a6cd810ef1c11e77d7895d7879b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:00 +0000

    Add package udacity-test-aws-course/0.1/udacity-test-aws-course-0.1.tar.gz

---
+++ packages/udacity-test-aws-course/udacity-test-aws-course-0.1.tar.gz/udacity-test-aws-course-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udacity-test-aws-course',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      zip_safe=False)
