9c018d3cffb6b8b54a7068587ff9eb07deb52bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:20 +0000

    Add package Sellix/1.0.1/Sellix-1.0.1-py3-none-any.whl

---
+++ packages/Sellix/Sellix-1.0.1-py3-none-any.whl/Sellix/Orders.py
@@ -0,0 +1,13 @@
+import requests
+
+class Orders():
+    def __init__(self, Authorization, URL, Headers):
+        self.Authorization = Authorization
+        self.URL = URL
+        self.Headers = Headers
+
+    def Get(self, ID : str):
+        return requests.get(self.URL + "/orders/" + ID, headers=self.Headers).json()
+
+    def List(self, Page : int = 0):
+        return requests.get(self.URL + "/orders?page=" + str(Page), headers=self.Headers).json()
