6b9b8396b406e21e7c284f813cd23778826e78c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:23 +0000

    Add package plus-pkg/0.0.13/plus_pkg-0.0.13-py3-none-any.whl

---
+++ packages/plus-pkg/plus_pkg-0.0.13-py3-none-any.whl/plus_pkg/learning_curve.py
@@ -0,0 +1,66 @@
+import Orange.data
+import numpy
+from Orange.widgets import widget, gui
+from Orange.widgets.utils.signals import Input
+from Orange.widgets.settings import Setting
+from Orange.widgets.widget import OWWidget, Output
+
+class OWDataSamplerB(OWWidget):
+    name = "Data Sampler (C)"
+    description = "Learning curve drawing"
+    icon = "icons/plus.svg"
+
+    class Inputs:
+        data = Input("Data", Orange.data.Table)
+        learner = Input("Learner", Orange.classification.Learner, multiple=True)
+
+    class Outputs:
+        sample = Output("Sampled Data", Orange.data.Table)
+        other = Output("Other Data", Orange.data.Table)
+
+    @Inputs.learner
+    def set_learner(self, learner, id):
+        """Set the input learner for channel id."""
+        if id in self.learners:
+            if learner is None:
+                # remove a learner and corresponding results
+                del self.learners[id]
+                del self.results[id]
+                del self.curves[id]
+            else:
+                # update/replace a learner on a previously connected link
+                self.learners[id] = learner
+                # invalidate the cross-validation results and curve scores
+                # (will be computed/updated in `_update`)
+                self.results[id] = None
+                self.curves[id] = None
+        else:
+            if learner is not None:
+                self.learners[id] = learner
+                # initialize the cross-validation results and curve scores
+                # (will be computed/updated in `_update`)
+                self.results[id] = None
+                self.curves[id] = None
+
+        if len(self.learners):
+            self.infob.setText("%d learners on input." % len(self.learners))
+        else:
+            self.infob.setText("No learners.")
+
+        self.commitBtn.setEnabled(len(self.learners))
+
+    def selection(self):
+        if self.dataset is None:
+            return
+
+        n_selected = int(numpy.ceil(len(self.dataset) * self.proportion / 100.0))
+        indices = numpy.random.permutation(len(self.dataset))
+        indices_sample = indices[:n_selected]
+        indices_other = indices[n_selected:]
+        self.sample = self.dataset[indices_sample]
+        self.otherdata = self.dataset[indices_other]
+        self.infob.setText("%d sampled instances" % len(self.sample))
+
+    def commit(self):
+        self.Outputs.sample.send(self.sample)
+        self.Outputs.sample.send(self.otherdata)
