fa9e330465731866982555dc60331f4c2f78e7f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:47 +0000

    Add package easypyrequests/1.0.0/EasyPyRequests-1.0.0-py3-none-any.whl

---
+++ packages/easypyrequests/EasyPyRequests-1.0.0-py3-none-any.whl/EasyPyRequests.py
@@ -0,0 +1,14 @@
+import flask
+import requests
+
+class EasyPyRequests:
+	def __init__():
+		pass
+
+	def get(url):
+		result = requests.get(url)
+		print(result)
+
+	def post(url):
+		result = requests.post(url)
+		print(result)
