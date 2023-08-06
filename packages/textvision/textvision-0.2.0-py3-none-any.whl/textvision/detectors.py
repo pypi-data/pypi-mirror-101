5fc098f2b14abce1be9351a1946c1f83d8a60e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:57 +0000

    Add package textvision/0.2.0/textvision-0.2.0-py3-none-any.whl

---
+++ packages/textvision/textvision-0.2.0-py3-none-any.whl/textvision/detectors.py
@@ -0,0 +1,36 @@
+import click
+
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
+
+    print('{}'.format(texts[0].description))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+
+@click.command()
+@click.argument('filename')
+def detect(filename):
+    detect_text(filename)
+
+def main():
+    detect()
+
+if __name__ == '__main__':
+    main()
