a389599a62cfe6aabf9e76f3f058a5fa162a20b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:59 +0000

    Add package animec/0.0.87/animec-0.0.87-py3-none-any.whl

---
+++ packages/animec/animec-0.0.87-py3-none-any.whl/animec/__init__.py
@@ -0,0 +1,14 @@
+from .charsearch import *
+from .aninews import *
+from .anicore import *
+from .gs import *
+
+'''
+An unofficial API to get data about anime characters, anime news and more.
+'''
+
+__author__ = "DriftAsimov"
+__title__ = "animec"
+__copyright__ = "Copyright Drift Asimov 2021"
+__version__ = "0.0.87"
+__license__ = "MIT"
