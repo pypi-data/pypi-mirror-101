6cf759c850dc0c2202bc67d872127159f035463f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:43 +0000

    Add package DongshaobingDemo-pkg/1.0/DongshaobingDemo_pkg-1.0.tar.gz

---
+++ packages/DongshaobingDemo-pkg/DongshaobingDemo_pkg-1.0.tar.gz/DongshaobingDemo_pkg-1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+NAME = 'DongshaobingDemo_pkg'
+VERSION = '1.0'
+PACKAGES = ['DongshaobingDemo_pkg']
+setup(name=NAME
+      , version=VERSION
+      , packages=PACKAGES,
+      description='a small pkg demos for xuzhichang and everyone of students',
+      long_description='''
+
+pip install DongshaobingDemo_pkg
+
+'''
+      , long_description_content_type='text/markdown'
+      )
+
+
