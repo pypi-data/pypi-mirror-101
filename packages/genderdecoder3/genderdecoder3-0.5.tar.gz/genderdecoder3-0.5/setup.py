80a0c307c2869f21167675998268faac0ba84355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:02 +0000

    Add package genderdecoder3/0.5/genderdecoder3-0.5.tar.gz

---
+++ packages/genderdecoder3/genderdecoder3-0.5.tar.gz/genderdecoder3-0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='genderdecoder3',
+      version='0.5',
+      description='Check for gender-coded words in job adverts. A package of the service provided at http://gender-decoder.katmatfield.com',
+      url='http://github.com/robbell/genderdecoder',
+      author='Rob Bell',
+      license='MIT',
+      packages=['genderdecoder3'],
+      zip_safe=False)
