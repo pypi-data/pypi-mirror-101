7623627dab2c0937c34f30fe5c7a4b8c381df2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:38 +0000

    Add package samoryad_messenger_server/0.0.1/samoryad_messenger_server-0.0.1.tar.gz

---
+++ packages/samoryad_messenger_server/samoryad_messenger_server-0.0.1.tar.gz/samoryad_messenger_server-0.0.1/log/server_log_config.py
@@ -0,0 +1,46 @@
+import logging
+import os
+import sys
+import logging.handlers
+
+sys.path.append('../')
+
+# Сообщения лога должны иметь следующий формат: "<дата-время>
+# <уровень_важности> <имя_модуля> <сообщение>"
+_log_format = f'%(asctime)s - %(levelname)s - %(module)s - %(message)s '
+# Создаем объект форматирования
+server_formatter = logging.Formatter(_log_format)
+
+# Подготовка имени файла для логирования
+path = os.getcwd()
+path = os.path.join(path, 'server.log')
+
+# создаём потоки вывода логов
+steam = logging.StreamHandler(sys.stderr)
+steam.setFormatter(server_formatter)
+steam.setLevel(logging.INFO)
+# На стороне сервера необходимо настроить ежедневную ротацию лог-файлов.
+log_file = logging.handlers.TimedRotatingFileHandler(
+    path, encoding='utf8', interval=1, when='D')
+log_file.setFormatter(server_formatter)
+
+# Создание именованного логгера
+server_logger = logging.getLogger('server')
+
+# Добавляем в логгер новый обработчик событий и устанавливаем уровень
+# логирования
+server_logger.addHandler(steam)
+server_logger.addHandler(log_file)
+server_logger.setLevel(logging.DEBUG)
+
+if __name__ == '__main__':
+    # проверяем
+    server_logger.info('Тестовый запуск логирования')
+    server_logger.warning('Тестовый запуск логирования')
+
+    # меняем уровень логирования
+    server_logger.setLevel(logging.WARNING)
+
+    # проверяем
+    server_logger.debug('Тестовый запуск логирования')
+    server_logger.critical('Тестовый запуск логирования')
---
+++ packages/samoryad_messenger_server/samoryad_messenger_server-0.0.1.tar.gz/samoryad_messenger_server-0.0.1/unit_tests/test_client.py
@@ -0,0 +1,40 @@
+# import unittest
+#
+# from my_messenger.common.utils import get_configs
+# from my_messenger.client import Client
+#
+#
+# class ClientTestCase(unittest.TestCase):
+#     CONFIGS = get_configs()
+#
+#     bad_message = {
+#         CONFIGS.get('RESPONSE'): 400,
+#         CONFIGS.get('ERROR'): 'Bad request'
+#     }
+#
+#     good_message = {
+#         CONFIGS.get('RESPONSE'): 200,
+#         CONFIGS.get('ALERT'): 'Привет, клиент!'
+#     }
+#
+#     test_error_message = f'400: Bad request'
+#     test_correct_message = f'200: OK, Привет, клиент!'
+#
+#     def test_error_response(self):
+#         self.assertEqual(Client.check_response(
+#             Client(),
+#             self.bad_message,
+#             self.CONFIGS),
+#             self.test_error_message
+#         )
+#
+#     def test_correct_response(self):
+#         self.assertEqual(Client.check_response(
+#             Client(),
+#             self.good_message,
+#             self.CONFIGS
+#         ), self.test_correct_message)
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
