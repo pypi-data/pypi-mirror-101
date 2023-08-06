934a9394a7b3e8fa2550b122f0d352eed5e4dd5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:04 +0000

    Add package django-site-warnings/0.1.2/django-site-warnings-0.1.2.tar.gz

---
+++ packages/django-site-warnings/django-site-warnings-0.1.2.tar.gz/django-site-warnings-0.1.2/django_site_warnings/actions.py
@@ -0,0 +1,16 @@
+
+from django.utils.translation import ugettext_lazy as _
+
+from .models import Warning
+
+
+def django_site_warnings_acknowledge(modeladmin, request, queryset):
+    items = []
+    for item in queryset.all():
+        if not item.ack:
+            item.acknowledge(save=False)
+            items.append(item)
+    Warning.objects.bulk_update(items, fields=["ack", "ack_time"])
+    message = _("{count} warnings acknowledged.").format(count=len(items))
+    modeladmin.message_user(request, message)
+django_site_warnings_acknowledge.short_description = _("Acknowledge selected {model_name}").format(model_name=Warning._meta.verbose_name)
---
+++ packages/django-site-warnings/django-site-warnings-0.1.2.tar.gz/django-site-warnings-0.1.2/django_site_warnings/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+from .models import Warning
+from .models import WaringCategory
+
+from .actions import django_site_warnings_acknowledge
+
+class WaringCategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "code"]
+    ordering = ["display_order"]
+
+class WarningAdmin(admin.ModelAdmin):
+    list_display = ["title", "add_time", "warning_status", "ack"]
+    list_filter = ["category", "status", "ack"]
+    readonly_fields = [] + Warning.SIMPLE_TASK_FIELDS
+
+    def warning_status(self, obj):
+        return obj.get_status_display()
+    warning_status.short_description = _("Warning Status")
+
+    actions = [
+        django_site_warnings_acknowledge,
+    ]
+
+    
+admin.site.register(WaringCategory, WaringCategoryAdmin)
+admin.site.register(Warning, WarningAdmin)
