62039f996698875e21dc0eefa6f281800dac84a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:15 +0000

    Add package xzc/1.0/xzc-1.0.tar.gz

---
+++ packages/xzc/xzc-1.0.tar.gz/xzc-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+NAME = 'xzc'
+VERSION = '1.0'
+PACKAGES = ['xzc']
+setup(name=NAME
+      , version=VERSION
+      , packages=PACKAGES,
+      description='a small pkg demos for everyone of students',
+      long_description='''
+
+pip install xzc
+
+'''
+      , long_description_content_type='text/markdown'
+      )
