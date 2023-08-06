71b570ec1ccec8df33933d9516a55079e8255f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:59 +0000

    Add package singular-dbbackup-sender/0.0.0/singular_dbbackup_sender-0.0.0.tar.gz

---
+++ packages/singular-dbbackup-sender/singular_dbbackup_sender-0.0.0.tar.gz/singular_dbbackup_sender-0.0.0/singular_dbbackup_sender/database_backup.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+
+
+def generate_backup():
+    import os
+    from django.core.mail import EmailMessage
+    from django.core.management import call_command
+
+    try:
+        call_command('dbbackup')
+        backup_path = os.path.join(settings.BACKUP_FOLDER, settings.DBBACKUP_FILENAME_TEMPLATE)
+        backup_name = settings.DBBACKUP_FILENAME_TEMPLATE
+        email = EmailMessage(backup_name, '', settings.DEFAULT_FROM_EMAIL, settings.EMAILS_BACKUP)
+        with open(backup_path, 'r') as file:
+            email.attach(f'{backup_name}', file.read())
+        email.send()
+        os.remove(backup_path)
+    except Exception as e:
+        print(e)
