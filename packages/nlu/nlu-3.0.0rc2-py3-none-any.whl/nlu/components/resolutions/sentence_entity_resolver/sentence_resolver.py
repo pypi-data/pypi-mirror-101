b5f0967dc8db7369d65e910d5ca8afea979e992d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:27:47 +0000

    Add package nlu/3.0.0rc2/nlu-3.0.0rc2-py3-none-any.whl

---
+++ packages/nlu/nlu-3.0.0rc2-py3-none-any.whl/nlu/components/resolutions/sentence_entity_resolver/sentence_resolver.py
@@ -0,0 +1,15 @@
+from sparknlp_jsl.annotator import SentenceEntityResolverModel,SentenceEntityResolverApproach
+
+class SentenceResolver:
+    @staticmethod
+    def get_pretrained_model(name, language, bucket='clinical/models'):
+        return SentenceEntityResolverModel.pretrained(name, language,bucket) \
+            .setInputCols(['entities',"sentence_embeddings"])\
+            .setOutputCol("sentence_resolution")
+
+    @staticmethod
+    def get_default_trainable_model():
+        return SentenceEntityResolverApproach() \
+        .setInputCols("entities","sentence_embeddings") \
+        .setOutputCol("sentence_resolution")\
+        .setLabelCol('label')
