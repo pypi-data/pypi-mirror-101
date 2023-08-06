329fcecf58994a2c9a83fed3532209e690a5bc31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:01 +0000

    Add package terminal-animation/0.3/terminal_animation-0.3-py3-none-any.whl

---
+++ packages/terminal-animation/terminal_animation-0.3-py3-none-any.whl/animations/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__pkg__ = 'terminal_animation'
+__url__ = 'https://github.com/SilasNiewierra/terminal_animation'
+__info__ = 'Decorators for terminal-based wait animations.'
+__author__ = 'Silas Niewierra'
+__email__ = 'niewierra.silas@gmail.com'
+__version__ = '0.3'
+
+
+from .animations import wait, Wait
