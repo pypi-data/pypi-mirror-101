a29027e18cb20b9c11c61cc3b30c965f51d5443e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:36 +0000

    Add package pysak/0.0.4/pysak-0.0.4.tar.gz

---
+++ packages/pysak/pysak-0.0.4.tar.gz/pysak-0.0.4/pysak/time.py
@@ -0,0 +1,25 @@
+def getTimestamp(length=10):
+    """
+        获取当前时刻指定长度的时间戳  10位~17位
+    :param length:
+    :return:
+    """
+
+    if length > 17 or length < 10:
+        return False
+
+    # 计算差值
+    cha = length - 10
+    timestamp = pysak.time()
+
+    if cha == 0:
+        timestamp = int(timestamp)
+    elif cha > 0:
+        timestamp = int(timestamp * pow(10, cha))
+
+    return timestamp
+
+
+def getDate():
+    ltime = time.localtime(getTimestamp(10))
+    return time.strftime("%Y-%m-%d %H:%M:%S", ltime)
