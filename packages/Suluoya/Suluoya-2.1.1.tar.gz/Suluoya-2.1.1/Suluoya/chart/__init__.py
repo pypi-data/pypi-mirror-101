60d962f3d8d473e6d31b7528e164c05d8e2cb8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:24 +0000

    Add package Suluoya/2.1.1/Suluoya-2.1.1.tar.gz

---
+++ packages/Suluoya/Suluoya-2.1.1.tar.gz/Suluoya-2.1.1/Suluoya/chart/__init__.py
@@ -0,0 +1,9 @@
+__name__ = "Suluoya"
+__author__ = 'Suluoya'
+__all__ = ['svg_charts', 'html_charts', 'cute_charts', 'app_charts']
+
+from .svg_charts import *
+from .html_charts import *
+from .cute_charts import *
+from .app_charts import *
+import pretty_errors
