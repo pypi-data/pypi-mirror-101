a5dec399f481dcc61a25743fb3a1b1afe84c1356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:27 +0000

    Add package addressformat/0.4.9/addressformat-0.4.9.tar.gz

---
+++ packages/addressformat/addressformat-0.4.9.tar.gz/addressformat-0.4.9/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+
+requires=['jieba','triedtree']
+
+setup(name='addressformat',
+      version='0.4.9',
+      install_requires = requires,
+      description='地址省市区解析',
+      long_description="地址解析模块",
+      license="MIT",
+      classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Indexing',
+      ],
+      packages=['addressformat', 'addressformat.resources','addressformat.entitywords'],
+
+      package_dir={'addressformat': 'addressformat', 'addressformat.resources': 'addressformat/resources','addressformat.entitywords':"addressformat/entitywords"},
+      package_data={'': ['*']},
+      include_package_data=True,
+      )
