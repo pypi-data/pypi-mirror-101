4a2efb67ca1bd6b475c13bef40c2e6fa95847b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:09 +0000

    Add package halo-app/0.10.72/halo-app-0.10.72.tar.gz

---
+++ packages/halo-app/halo-app-0.10.72.tar.gz/halo-app-0.10.72/halo_app/app/dto_mapper.py
@@ -0,0 +1,17 @@
+import abc
+from mapper.object_mapper import ObjectMapper
+from halo_app.classes import AbsBaseClass
+
+
+class AbsHaloDtoMapper(AbsBaseClass,abc.ABC):
+    mapper = None
+    def __init__(self):
+        self.mapper = ObjectMapper()
+
+    @abc.abstractmethod
+    def map_to_dto(self,object,AbsHaloDto):
+        pass
+
+    @abc.abstractmethod
+    def map_from_dto(self,AbsHaloDto,object):
+        pass
