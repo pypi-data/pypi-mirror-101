d459884f2974ea3f8d4b95633ea440cc692491ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:27 +0000

    Add package sect/1.0.0/sect-1.0.0-py3-none-any.whl

---
+++ packages/sect/sect-1.0.0-py3-none-any.whl/sect/decomposition.py
@@ -0,0 +1,9 @@
+from .core.trapezoidal.abcs import Graph
+from .core.trapezoidal.graph import to_graph_cls
+from .core.trapezoidal.hints import Shuffler
+from .core.trapezoidal.location import Location
+
+Graph = Graph
+Location = Location
+Shuffler = Shuffler
+to_graph_cls = to_graph_cls
---
+++ packages/sect/sect-1.0.0-py3-none-any.whl/sect/triangulation.py
@@ -0,0 +1,7 @@
+from .core.delaunay.abcs import (QuadEdge,
+                                 Triangulation)
+from .core.delaunay.triangulation import to_triangulation_cls
+
+QuadEdge = QuadEdge
+Triangulation = Triangulation
+to_triangulation_cls = to_triangulation_cls
