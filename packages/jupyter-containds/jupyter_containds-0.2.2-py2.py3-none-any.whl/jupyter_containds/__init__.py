9ddf289c5053107df82392d161508ea77dc1a6a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:13 +0000

    Add package jupyter-containds/0.2.2/jupyter_containds-0.2.2-py2.py3-none-any.whl

---
+++ packages/jupyter-containds/jupyter_containds-0.2.2-py2.py3-none-any.whl/jupyter_containds/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Ideonate.
+# Distributed under the terms of the Modified BSD License.
+
+from .user import User
+from ._version import __version__, version_info
+
+from .nbextension import _jupyter_nbextension_paths
+
+
+def _jupyter_labextension_paths():
+    return [{"src": "labextension", "dest": "@ideonate/jupyter-containds"}]
