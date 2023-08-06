ab5a858b2540fa87ef2fbb01ea84b3f53562abd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:36 +0000

    Add package demographic-modeling/0.3.0/demographic_modeling-0.3.0-py3-none-any.whl

---
+++ packages/demographic-modeling/demographic_modeling-0.3.0-py3-none-any.whl/modeling/__init__.py
@@ -0,0 +1,12 @@
+__title__ = 'demographic-modeling-module'
+__version__ = '0.3.0'
+__author__ = 'Joel McCune'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2020 by Joel McCune'
+"""
+The modeling module provides capabilities supporting quantitative geographic analysis.
+"""
+from .country import Country, get_countries
+from .accessor import ModelingAccessor, Business
+
+__all__ = ['get_countries', 'Country', 'Business', 'ModelingAccessor']
