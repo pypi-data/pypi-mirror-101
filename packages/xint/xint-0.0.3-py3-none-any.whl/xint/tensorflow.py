e8064398a214f768300ad462a1fccf8c222843a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:06 +0000

    Add package xint/0.0.3/xint-0.0.3-py3-none-any.whl

---
+++ packages/xint/xint-0.0.3-py3-none-any.whl/xint/tensorflow.py
@@ -0,0 +1,208 @@
+import sys
+from matplotlib import pyplot as plt
+
+import tensorflow as tf
+from tensorflow.keras.callbacks import Callback
+
+from .utils import Accumulator, Animator, Timer
+from .chaos import import_np, load_array as _load_array, load_nn
+
+_name = __name__.split('.')[1]
+
+np = import_np(_name)
+nn = load_nn(_name)
+
+xinet = sys.modules[__name__]
+
+# ======================================
+## 特定于框架的类，函数等
+
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    """下载Fashion-MNIST数据集，然后将其加载到内存中。"""
+    mnist_train, mnist_test = tf.keras.datasets.fashion_mnist.load_data()
+    # 将所有数字除以255，使所有像素值介于0和1之间，在最后添加一个批处理维度，
+    # 并将标签转换为int32。
+
+    def process(X, y): return (tf.expand_dims(X, axis=3) / 255,
+                               tf.cast(y, dtype='int32'))
+
+    def resize_fn(X, y): return (tf.image.resize_with_pad(X, resize, resize)
+                                 if resize else X, y)
+    return (tf.data.Dataset.from_tensor_slices(
+        process(*mnist_train)).batch(batch_size).shuffle(len(
+            mnist_train[0])).map(resize_fn),
+        tf.data.Dataset.from_tensor_slices(
+                process(*mnist_test)).batch(batch_size).map(resize_fn))
+
+
+def accuracy(y_hat, y):
+    """计算预测正确的数量。"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = tf.argmax(y_hat, axis=1)
+    cmp = tf.cast(y_hat, y.dtype) == y
+    return float(tf.reduce_sum(tf.cast(cmp, y.dtype)))
+
+
+def train_epoch(net, train_iter, loss, updater):
+    """训练模型一个迭代周期（定义见第3章）。"""
+    # 训练损失总和、训练准确度总和、样本数
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        # 计算梯度并更新参数
+        with tf.GradientTape() as tape:
+            y_hat = net(X)
+            # Keras内置的损失接受的是（标签，预测），这不同于用户在本书中的实现。
+            # 本书的实现接受（预测，标签），例如我们上面实现的“交叉熵”
+            if isinstance(loss, tf.keras.losses.Loss):
+                l = loss(y, y_hat)
+            else:
+                l = loss(y_hat, y)
+        if isinstance(updater, tf.keras.optimizers.Optimizer):
+            params = net.trainable_variables
+            grads = tape.gradient(l, params)
+            updater.apply_gradients(zip(grads, params))
+        else:
+            updater(X.shape[0], tape.gradient(l, updater.params))
+        # Keras的`loss`默认返回一个批量的平均损失
+        l_sum = l * float(tf.size(y)) if isinstance(
+            loss, tf.keras.losses.Loss) else tf.reduce_sum(l)
+        metric.add(l_sum, accuracy(y_hat, y), tf.size(y))
+    # 返回训练损失和训练准确率
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+def sgd(params, grads, lr, batch_size):
+    """小批量随机梯度下降。"""
+    for param, grad in zip(params, grads):
+        param.assign_sub(lr * grad / batch_size)
+
+
+class Updater:
+    """用小批量随机梯度下降法更新参数。"""
+
+    def __init__(self, params, lr):
+        self.params = params
+        self.lr = lr
+
+    def __call__(self, batch_size, grads):
+        sgd(self.params, grads, self.lr, batch_size)
+
+
+def try_gpu(i=0):
+    """如果存在，则返回gpu(i)，否则返回cpu()。"""
+    if len(tf.config.experimental.list_physical_devices('GPU')) >= i + 1:
+        return tf.device(f'/GPU:{i}')
+    return tf.device('/CPU:0')
+
+
+def try_all_gpus():
+    """返回所有可用的GPU，如果没有GPU，则返回[cpu(),]。"""
+    num_gpus = len(tf.config.experimental.list_physical_devices('GPU'))
+    devices = [tf.device(f'/GPU:{i}') for i in range(num_gpus)]
+    return devices if devices else [tf.device('/CPU:0')]
+
+
+try_gpu(), try_gpu(10), try_all_gpus()
+
+# ======================================
+## 共同 API
+
+
+def load_array(data_arrays, batch_size, is_train=True):
+    return _load_array(_name, data_arrays, batch_size, is_train)
+
+
+def normal(x, mu, sigma):
+    p = 1 / np.sqrt(2 * np.pi * sigma**2)
+    return p * np.exp(-0.5 / sigma**2 * (x - mu)**2)
+
+
+def one_hot(arr, num_classes):
+    return np.eye(num_classes)[arr]
+
+
+def softmax(X):
+    X_exp = np.exp(X)
+    partition = np.sum(X_exp, axis=1, keepdims=True)
+    return X_exp / partition  # 这里应用了广播机制
+
+
+def cross_entropy(y_hat, y):
+    return -np.log(y_hat[range(len(y_hat)), y])
+
+
+def evaluate_accuracy(net, data_iter):
+    """计算在指定数据集上模型的精度"""
+    metric = Accumulator(2)  # 正确预测数、预测总数
+    for X, y in data_iter:
+        metric.add(accuracy(net(X), y), len(y))
+    return metric[0] / metric[1]
+
+
+def evaluate_loss(net, data_iter, loss):
+    """Evaluate the loss of a model on the given dataset."""
+    metric = Accumulator(2)  # Sum of losses, no. of examples
+    for X, y in data_iter:
+        l = loss(net(X), y)
+        metric.add(float(sum(l)), len(l))
+    return metric[0] / metric[1]
+
+
+def train_simple(net, train_iter, test_iter, loss, num_epochs, updater, ylim=None):
+    """训练模型（定义见第3章）。"""
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=ylim,
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    #train_loss, train_acc = train_metrics
+
+
+class TrainCallback(Callback):
+    """A callback to visiualize the training progress."""
+
+    def __init__(self, net, train_iter, test_iter, num_epochs, device_name):
+        self.timer = Timer()
+        self.animator = Animator(
+            xlabel='epoch', xlim=[1, num_epochs],
+            legend=['train loss', 'train acc', 'test acc'])
+        self.net = net
+        self.train_iter = train_iter
+        self.test_iter = test_iter
+        self.num_epochs = num_epochs
+        self.device_name = device_name
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.timer.start()
+
+    def on_epoch_end(self, epoch, logs):
+        self.timer.stop()
+        test_acc = self.net.evaluate(self.test_iter, verbose=0,
+                                     return_dict=True)['accuracy']
+        metrics = (logs['loss'], logs['accuracy'], test_acc)
+        self.animator.add(epoch + 1, metrics)
+        if epoch == self.num_epochs - 1:
+            batch_size = next(iter(self.train_iter))[0].shape[0]
+            num_examples = batch_size * tf.data.experimental.cardinality(
+                self.train_iter).numpy()
+            print(f'loss {metrics[0]:.3f}, train acc {metrics[1]:.3f}, '
+                  f'test acc {metrics[2]:.3f}')
+            print(f'{num_examples / self.timer.avg():.1f} examples/sec on '
+                  f'{str(self.device_name)}')
+
+
+def train(net_fn, train_iter, test_iter, num_epochs, lr, device):
+    """Train a model with a GPU (defined in Chapter 6)."""
+    device_name = device._device_name
+    strategy = tf.distribute.OneDeviceStrategy(device_name)
+    with strategy.scope():
+        optimizer = tf.keras.optimizers.SGD(learning_rate=lr)
+        loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        net = net_fn()
+        net.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
+    callback = TrainCallback(net, train_iter, test_iter, num_epochs,
+                             device_name)
+    net.fit(train_iter, epochs=num_epochs, verbose=0, callbacks=[callback])
+    return net
