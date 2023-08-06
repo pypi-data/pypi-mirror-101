bd97fe4232db2867c5ee927d18b99c494fba9618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:42 +0000

    Add package oort-cloud/1.2.3/oort_cloud-1.2.3-py2.py3-none-any.whl

---
+++ packages/oort-cloud/oort_cloud-1.2.3-py2.py3-none-any.whl/oort/shared/constants.py
@@ -0,0 +1,39 @@
+from copy import deepcopy
+
+OORT_FILENAME = '__oort__'
+
+# We use a custom socket filename because we may need to grep it.
+# A custom name ensure we won't confuse our sicket file with another one.
+OORT_SUPERVISOR_SOCK_FILENAME = 'oort_supervisor.sock'
+
+# As used in QLFits: https://github.com/onekiloparsec/QLFits
+OORT_FITS_EXTENSIONS = [
+    '.fits',
+    '.fit',
+    '.fts',
+    '.ft',
+    '.mt',
+    '.imfits',
+    '.imfit',
+    '.uvfits',
+    '.uvfit',
+    '.pha',
+    '.rmf',
+    '.arf',
+    '.rsp',
+    '.pi'
+]
+
+
+def _extend_list(extensions):
+    for zip in ['.zip', '.gz', '.gzip', '.bz2']:
+        extensions += [e + zip for e in extensions]
+    return extensions
+
+
+def get_all_xisf_extensions():
+    return _extend_list(deepcopy(['.xisf', ]))
+
+
+def get_all_fits_extensions():
+    return _extend_list(deepcopy(OORT_FITS_EXTENSIONS))
