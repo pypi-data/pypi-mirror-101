cab9a1433c7f9436b116675d6ca03a6562c12e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:39 +0000

    Add package mtg-parser/0.0.1a3/mtg_parser-0.0.1a3.tar.gz

---
+++ packages/mtg-parser/mtg_parser-0.0.1a3.tar.gz/mtg_parser-0.0.1a3/src/mtg_parser/parser.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .gramar import LINE
+
+
+__all__ = [
+    'parse_deck',
+]
+
+
+def _collapse_comments(lines):
+    last_comment = None
+    for line in lines:
+        if 'comment' in line:
+            last_comment = line['comment']
+        else:
+            if last_comment:
+                line.setdefault('tags', []).append(last_comment)
+            yield line
+
+
+def _cleanup_tags(line):
+    if 'tags' in line:
+        line['tags'] = list(sorted(set(line['tags'])))
+    return line
+
+
+def parse_deck(decklist):
+    lines = decklist.splitlines()
+    lines = map(str.strip, lines)
+    lines = filter(len, lines)
+    lines = map(LINE.parseString, lines)
+    lines = map(lambda line: line.asDict(), lines)
+    lines = _collapse_comments(lines)
+    lines = map(_cleanup_tags, lines)
+
+    return lines
