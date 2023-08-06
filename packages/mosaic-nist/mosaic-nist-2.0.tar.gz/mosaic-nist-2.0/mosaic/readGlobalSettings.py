e9dc853993255278c06966df14268f8067754851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:53 +0000

    Add package mosaic-nist/2.0/mosaic-nist-2.0.tar.gz

---
+++ packages/mosaic-nist/mosaic-nist-2.0.tar.gz/mosaic-nist-2.0/mosaic/readGlobalSettings.py
@@ -0,0 +1,36 @@
+import json
+import sys
+
+def readGlobalSettings():
+	try:
+		with open("mosaic/global.json", mode="r") as j_object:
+			data = json.load(j_object)
+	except FileNotFoundError:
+		print("Global settings not found. Using default values")
+		data=json.loads(
+				"""
+					{
+						"DeveloperMode"			:	false,
+						"CodeProfiling"			:	"summary",
+						"LogProperties"			:	false,
+						"LogSizeBytes"			:	16777216,
+						"DocumentationURL"		:	"https://pages.nist.gov/mosaic/",
+						"WebHost"				:	"0.0.0.0",
+						"WebServerPort"			:	5000,
+						"WebServerWorkers"		:	1,
+						"WebServerDataLocation"	:	"/",
+						"WebServerMode"			:	"remote"
+
+					}
+				"""
+			)
+
+	return data
+
+if __name__ == '__main__':
+	mosaic_mod = sys.modules[__name__]
+	for (k,v) in readGlobalSettings().items():
+		setattr(mosaic_mod, k, v)
+
+	print(DeveloperMode)
+	print(WebHost)
