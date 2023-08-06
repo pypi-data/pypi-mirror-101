9b0231861572b8dc6f10680ce5bac0bafa89d7fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:13 +0000

    Add package pylexitext/0.2.0/pylexitext-0.2.0.tar.gz

---
+++ packages/pylexitext/pylexitext-0.2.0.tar.gz/pylexitext-0.2.0/README.md
@@ -0,0 +1,22 @@
+# Pylexitext
+
+<img src="https://img.shields.io/github/issues/vicotrbb/pylexitext"> <img src="https://img.shields.io/github/workflow/status/vicotrbb/Pylexitext/Python%20application"> <img src="https://img.shields.io/github/downloads/vicotrbb/Pylexitext/total"> <img src="https://img.shields.io/github/commit-activity/w/vicotrbb/Pylexitext"> [![Upload Python Package](https://github.com/vicotrbb/Pylexitext/actions/workflows/python-publish.yml/badge.svg?branch=main)](https://github.com/vicotrbb/Pylexitext/actions/workflows/python-publish.yml)
+
+Pylexitext is a python library that aggregates a series of NLP methods, text analysis, content converters and other usefull stuff. 
+
+## How to use
+
+## About Creator
+
+My name is Victor Bona, I am a Experienced Back-end Developer and Machine learning enthusiast 🧠 aiming to work with ML engineering and contribute to the development and growing of Artificial intelligence field, professional and academically.
+
+Find me on:
+✉️ victor.bona@hotmail.com
+📞 +55 047 98859-1570
+💡 https://github.com/vicotrbb
+🔍 https://sourcerer.io/vicotrbb
+📝 https://medium.com/cafe-e-tech
+📊 https://www.linkedin.com/in/victorbona/
+✏️ https://leetcode.com/vicotrbb/ 
+
+## Collaborations
---
+++ packages/pylexitext/pylexitext-0.2.0.tar.gz/pylexitext-0.2.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='pylexitext',
+    packages=find_packages(include=['pylexitext']),
+    version='0.2.0',
+    description='Pylexitext is a python library that aggregates a series of NLP methods, text analysis, content converters and other usefull stuff.',
+    author='Victor Bona',
+    license='MIT',
+    author_email='victor.bona@hotmail.com',
+    keywords=['NLP', 'readability', 'nltk', 'text'],
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    install_requires=['numpy', 'nltk', 'networkx', 'matplotlib'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+    python_requires=">=3.5",
+    classifiers=(
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Topic :: Text Processing",
+    ),
+)
