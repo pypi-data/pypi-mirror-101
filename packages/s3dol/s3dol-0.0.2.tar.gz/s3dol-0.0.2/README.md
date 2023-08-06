767e440c383346efc4675851fa7b4827dca8fad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:38 +0000

    Add package s3dol/0.0.2/s3dol-0.0.2.tar.gz

---
+++ packages/s3dol/s3dol-0.0.2.tar.gz/s3dol-0.0.2/README.md
@@ -0,0 +1,6 @@
+
+# s3dol
+s3 (through boto3) with a simple (dict-like or list-like) interface
+
+
+To install:	```pip install s3dol```
---
+++ packages/s3dol/s3dol-0.0.2.tar.gz/s3dol-0.0.2/s3dol/__init__.py
@@ -0,0 +1,10 @@
+"""
+s3 (through boto3) with a simple (dict-like or list-like) interface
+"""
+from s3dol import new_s3
+from s3dol import s3_store
+from s3dol import s3_w_boto3
+
+from s3dol.new_s3 import *
+from s3dol.s3_store import *
+from s3dol.s3_w_boto3 import *
