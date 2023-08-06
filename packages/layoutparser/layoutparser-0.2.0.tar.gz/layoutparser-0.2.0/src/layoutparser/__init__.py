aba542782e6efc8dbba9e8eff0f9489d45b88f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:14 +0000

    Add package layoutparser/0.2.0/layoutparser-0.2.0.tar.gz

---
+++ packages/layoutparser/layoutparser-0.2.0.tar.gz/layoutparser-0.2.0/src/layoutparser/__init__.py
@@ -0,0 +1,23 @@
+__version__ = "0.2.0"
+
+from .elements import (
+    Interval, Rectangle, Quadrilateral, 
+    TextBlock, Layout
+)
+
+from .visualization import (
+    draw_box, draw_text
+)
+
+from .ocr import (
+    GCVFeatureType, GCVAgent, 
+    TesseractFeatureType, TesseractAgent
+)
+
+from .models import (
+    Detectron2LayoutModel
+)
+
+from .io import (
+    load_json
+)
