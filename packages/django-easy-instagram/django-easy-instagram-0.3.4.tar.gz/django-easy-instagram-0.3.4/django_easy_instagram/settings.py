4fd64be73e011155660a851cf079c69482069042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:52:18 +0000

    Add package django-easy-instagram/0.3.4/django-easy-instagram-0.3.4.tar.gz

---
+++ packages/django-easy-instagram/django-easy-instagram-0.3.4.tar.gz/django-easy-instagram-0.3.4/django_easy_instagram/settings.py
@@ -0,0 +1,3 @@
+INSTAGRAM_QUALITY = 80
+INSTAGRAM_UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
+INSTAGRAM_COOKIE = None
