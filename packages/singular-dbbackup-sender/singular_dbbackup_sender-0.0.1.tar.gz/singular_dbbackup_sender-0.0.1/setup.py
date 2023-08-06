b6952193486ab05135f720956055c93adbc3efd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:20 +0000

    Add package singular-dbbackup-sender/0.0.1/singular_dbbackup_sender-0.0.1.tar.gz

---
+++ packages/singular-dbbackup-sender/singular_dbbackup_sender-0.0.1.tar.gz/singular_dbbackup_sender-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='singular_dbbackup_sender',
+    version='0.0.1',
+    author='Singular Sistemas',
+    author_email='ivan@singular.inf.br',
+    description='Server updater',
+    long_description='Server updater',
+    url='https://lucas@bitbucket.org/singular-dev/dbbackup_sender.git',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        'django',
+        'django-crontab',
+        'dbbackup'
+    ]
+)
