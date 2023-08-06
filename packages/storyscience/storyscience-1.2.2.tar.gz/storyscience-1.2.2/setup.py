15c9e139f2580a001f944f98443e5e16f20ec832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:08 +0000

    Add package storyscience/1.2.2/storyscience-1.2.2.tar.gz

---
+++ packages/storyscience/storyscience-1.2.2.tar.gz/storyscience-1.2.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="storyscience",
+    version="1.2.2",
+    author="subbhashit mukherjee",
+    author_email="subbhashitmukherjee@gmail.com",
+    packages=["storyscience"],
+    description="A storyteller",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/23subbhashit/StoryTellar/",
+    license='MIT',
+    python_requires='>=3.6',
+    install_requires=[
+        'numpy>=1.16.6',
+        'pandas>=1.0.5',
+        'seaborn>=0.10.1',
+        'matplotlib>=3.2.2'
+    ]
+)
