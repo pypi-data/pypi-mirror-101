7153e2946011c3f2615aea8bdae803af19e3decf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:59 +0000

    Add package mydatapreprocessing/1.1.32/mydatapreprocessing-1.1.32-py3-none-any.whl

---
+++ packages/mydatapreprocessing/mydatapreprocessing-1.1.32-py3-none-any.whl/mydatapreprocessing/__init__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+.. image:: https://img.shields.io/pypi/pyversions/mydatapreprocessing.svg
+    :target: https://pypi.python.org/pypi/mydatapreprocessing/
+    :alt: Py versions
+
+.. image:: https://badge.fury.io/py/mydatapreprocessing.svg
+    :target: https://badge.fury.io/py/mydatapreprocessing
+    :alt: PyPI package
+
+.. image:: https://img.shields.io/lgtm/grade/python/g/Malachov/mydatapreprocessing.svg?logo=lgtm&logoWidth=18
+    :target: https://lgtm.com/projects/g/Malachov/mydatapreprocessing/context:python
+    :alt: Language grade: Python
+
+.. image:: https://travis-ci.com/Malachov/mydatapreprocessing.svg?branch=master
+    :target: https://travis-ci.com/Malachov/mydatapreprocessing
+    :alt: Build Status
+
+.. image:: https://readthedocs.org/projects/mydatapreprocessing/badge/?version=latest
+    :target: https://mydatapreprocessing.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+
+.. image:: https://img.shields.io/badge/License-MIT-yellow.svg
+    :target: https://opensource.org/licenses/MIT
+    :alt: License: MIT
+
+.. image:: https://codecov.io/gh/Malachov/mydatapreprocessing/branch/master/graph/badge.svg
+    :target: https://codecov.io/gh/Malachov/mydatapreprocessing
+    :alt: Codecov
+
+Library contain 3 modules - preprocessing, inputs and generatedata.
+"""
+__version__ = "1.1.32"
+__author__ = "Daniel Malachov"
+__license__ = "MIT"
+__email__ = "malachovd@seznam.cz"
+
+__all__ = ["preprocessing", "inputs", "generatedata"]
+
+import warnings
+
+with warnings.catch_warnings():
+    warnings.filterwarnings("once")
+
+    from . import preprocessing
+    from . import inputs
+    from . import generatedata
