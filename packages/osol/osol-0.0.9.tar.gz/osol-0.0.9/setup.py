2f387ed7e2c16294f8950c74956016f4b5bf331f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:01 +0000

    Add package osol/0.0.9/osol-0.0.9.tar.gz

---
+++ packages/osol/osol-0.0.9.tar.gz/osol-0.0.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['osol', 'osol.algorithms', 'osol.tools']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.20.2,<2.0.0', 'scipy>=1.6.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'osol',
+    'version': '0.0.9',
+    'description': '',
+    'long_description': None,
+    'author': 'wol4aravio',
+    'author_email': 'panovskiy.v@yandex.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8.0,<3.9.0',
+}
+
+
+setup(**setup_kwargs)
