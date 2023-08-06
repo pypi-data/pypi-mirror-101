5891452bcf7f5aa24ac5e8c579e338abe0ddde8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:09 +0000

    Add package singular-bug-tracker/0.0.1/singular_bug_tracker-0.0.1.tar.gz

---
+++ packages/singular-bug-tracker/singular_bug_tracker-0.0.1.tar.gz/singular_bug_tracker-0.0.1/bug_tracker/bug_tracker.py
@@ -0,0 +1,22 @@
+import sys
+from traceback import print_tb, format_tb
+
+from django.conf import settings
+from django.core.mail import send_mail
+from django.shortcuts import  render
+from django.template.loader import render_to_string
+
+
+def handler500(request, *args, **argv):
+    type_, value, traceback = sys.exc_info()
+    _request = traceback.tb_next.tb_frame.f_locals['request']
+    traceback = format_tb(traceback)
+    send_mail(value.args[0], '', settings.DEFAULT_FROM_EMAIL,
+              [settings.BUGTRACKER_EMAIL],
+              html_message=render_to_string('traceback.html', {
+                  'url': _request.path,
+                  'post': _request.POST,
+                  'get': _request.GET,
+                  'traceback':traceback
+              }))
+    return render('500.html', status=500)
