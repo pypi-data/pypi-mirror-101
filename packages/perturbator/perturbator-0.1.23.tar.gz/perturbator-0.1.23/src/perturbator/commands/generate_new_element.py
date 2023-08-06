e80bf44de68af8c5ce4fcc90c38fc888c75904a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:42 +0000

    Add package perturbator/0.1.23/perturbator-0.1.23.tar.gz

---
+++ packages/perturbator/perturbator-0.1.23.tar.gz/perturbator-0.1.23/src/perturbator/commands/generate_new_element.py
@@ -0,0 +1,10 @@
+import click
+from ..support_modules.perturbator import generate_element
+
+@click.command()
+@click.option('--element-tag', help='Tag of element, e.g. exclusiveGateway, task', required=True)
+@click.option('--element-id', help='Id of the element you want to generate', required=True)
+def generate_new_element(element_tag, element_id):
+    element = generate_element(element_tag, element_id)
+    click.echo('Element generated with id %s!' % element.attrib['id'])
+
