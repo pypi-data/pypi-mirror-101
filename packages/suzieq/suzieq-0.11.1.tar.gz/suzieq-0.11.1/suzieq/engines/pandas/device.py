e4e06262969f380b29bcd52332258d6215fb5288
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:06 +0000

    Add package suzieq/0.11.1/suzieq-0.11.1.tar.gz

---
+++ packages/suzieq/suzieq-0.11.1.tar.gz/suzieq-0.11.1/suzieq/engines/pandas/device.py
@@ -0,0 +1,62 @@
+import numpy as np
+from .engineobj import SqPandasEngine
+
+
+class DeviceObj(SqPandasEngine):
+
+    @staticmethod
+    def table_name():
+        return 'device'
+
+    def get(self, **kwargs):
+        """Get the information requested"""
+        view = kwargs.get('view', self.iobj.view)
+        columns = kwargs.get('columns', ['default'])
+        addnl_fields = kwargs.pop('addnl_fields', [])
+        user_query = kwargs.pop('query_str', '')
+
+        drop_cols = []
+
+        if 'active' not in addnl_fields+columns and columns != ['*']:
+            addnl_fields.append('active')
+            drop_cols.append('active')
+
+        df = super().get(active_only=False, addnl_fields=addnl_fields,
+                         **kwargs)
+        if view == 'latest' and 'status' in df.columns:
+            df['status'] = np.where(df.active, df['status'], 'dead')
+
+        df = self._handle_user_query_str(df, user_query)
+        return df.drop(columns=drop_cols)
+
+    def summarize(self, **kwargs):
+        """Summarize device information across namespace"""
+
+        self._init_summarize(self.iobj.table, **kwargs)
+        if self.summary_df.empty:
+            return self.summary_df
+
+        self._summarize_on_add_field = [
+            ('deviceCnt', 'hostname', 'nunique'),
+        ]
+
+        self._summarize_on_add_with_query = [
+            ('downDeviceCnt', 'status == "dead"', 'hostname', 'nunique')
+        ]
+
+        self._summarize_on_add_list_or_count = [
+            ('vendorCnt', 'vendor'),
+            ('modelCnt', 'model'),
+            ('archCnt', 'architecture'),
+            ('versionCnt', 'version'),
+        ]
+
+        self.summary_df = self.iobj.humanize_fields(self.summary_df)
+
+        self._summarize_on_add_stat = [
+            ('upTimeStat', '', 'uptime')
+        ]
+
+        self._gen_summarize_data()
+        self._post_summarize(check_empty_col='deviceCnt')
+        return self.ns_df.convert_dtypes()
