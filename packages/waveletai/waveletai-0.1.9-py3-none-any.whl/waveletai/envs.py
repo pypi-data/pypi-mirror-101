1ca2df00413f37667ce28555890e10e175d2ca68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:38 +0000

    Add package waveletai/0.1.9/waveletai-0.1.9-py3-none-any.whl

---
+++ packages/waveletai/waveletai-0.1.9-py3-none-any.whl/waveletai/envs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2021/3/11 16:01
+@Author  : WaveletAI-Product-Team Janus
+@license : (C) Copyright 2019-2022, Visionet(Tianjin)Information Technology Co.,Ltd.
+@Site    : plus.xiaobodata.com
+@File    : envs.py
+@Desc    : 
+"""
+
+
+API_KEY = ''
+
+API_TOKEN = ''
+
+USER_NAME = ''
+
+USER_PWD = ''
+
+"""单次请求最大值"""
+CHUNK_SIZE = 1024
+
+"""超过5M的内容显示进度"""
+WARN_SIZE = 1024 * 1024 * 2
