4bbbf499580e85278e4deee65f3df093bf71e15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:24 +0000

    Add package xint/0.0.1/xint-0.0.1.tar.gz

---
+++ packages/xint/xint-0.0.1.tar.gz/xint-0.0.1/xint/torch.py
@@ -0,0 +1,191 @@
+import sys
+from matplotlib import pyplot as plt
+
+import torch
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+
+from .utils import Accumulator, Animator
+from .chaos import import_np, load_array as _load_array, load_nn
+
+_name = __name__.split('.')[1]
+
+np = import_np(_name)
+np.pi = np.acos(np.zeros(1)) * 2
+
+nn = load_nn(_name)
+
+xinet = sys.modules[__name__]
+
+# ======================================
+## 特定于框架的类，函数等
+
+
+def get_dataloader_workers():
+    """在非Windows的平台上，使用4个进程来读取的数据。"""
+    return 0 if sys.platform.startswith('win') else 4
+
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    """下载Fashion-MNIST数据集，然后将其加载到内存中。"""
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(root="../data",
+                                                    train=True,
+                                                    transform=trans,
+                                                    download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(root="../data",
+                                                   train=False,
+                                                   transform=trans,
+                                                   download=True)
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
+                            num_workers=get_dataloader_workers()),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,
+                            num_workers=get_dataloader_workers()))
+
+
+def load_data_mnist(batch_size, resize=None):
+    """下载 MNIST 数据集，然后将其加载到内存中。"""
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.MNIST(root="../data",
+                                             train=True,
+                                             transform=trans,
+                                             download=True)
+    mnist_test = torchvision.datasets.MNIST(root="../data",
+                                            train=False,
+                                            transform=trans,
+                                            download=True)
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
+                            num_workers=get_dataloader_workers()),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,
+                            num_workers=get_dataloader_workers()))
+
+
+def accuracy(y_hat, y):
+    """计算预测正确的数量。"""
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(sum(cmp.type(y.dtype)))
+
+
+def train_epoch(net, train_iter, loss, updater, num_classes=10):
+    """训练模型一个迭代周期（定义见第3章）。"""
+    # 将模型设置为训练模式
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    # 训练损失总和、训练准确度总和、样本数
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        # 计算梯度并更新参数
+        y_hat = net(X)
+        if isinstance(loss, nn.MSELoss):
+            y_ = nn.functional.one_hot(y, num_classes=num_classes)
+            y_ = y_.type(torch.float)
+            l = loss(y_hat, y_)
+        else:
+            l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            # 使用PyTorch内置的优化器和损失函数
+            updater.zero_grad()
+            l.backward()
+            updater.step()
+            metric.add(
+                float(l) * len(y), accuracy(y_hat, y),
+                y.size().numel())
+        else:
+            # 使用PyTorch内置的优化器和损失函数
+            l.sum().backward()
+            updater(X.shape[0])
+            metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+    # 返回训练损失和训练准确率
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+def sgd(params, lr, batch_size):
+    """小批量随机梯度下降。"""
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad / batch_size
+            param.grad.zero_()
+
+
+def updater(params, lr, batch_size):
+    return sgd(params, lr, batch_size)
+
+
+def try_gpu(i=0):
+    """如果存在，则返回gpu(i)，否则返回cpu()。"""
+    if torch.cuda.device_count() >= i + 1:
+        return torch.device(f'cuda:{i}')
+    return torch.device('cpu')
+
+
+def try_all_gpus():
+    """返回所有可用的GPU，如果没有GPU，则返回[cpu(),]。"""
+    devices = [
+        torch.device(f'cuda:{i}') for i in range(torch.cuda.device_count())]
+    return devices if devices else [torch.device('cpu')]
+
+
+try_gpu(), try_gpu(10), try_all_gpus()
+
+# ======================================
+## 共同 API
+
+
+def load_array(data_arrays, batch_size, is_train=True):
+    return _load_array(_name, data_arrays, batch_size, is_train)
+
+
+def normal(x, mu, sigma):
+    p = 1 / np.sqrt(2 * np.pi * sigma**2)
+    return p * np.exp(-0.5 / sigma**2 * (x - mu)**2)
+
+
+def one_hot(arr, num_classes):
+    return np.eye(num_classes)[arr]
+
+
+def softmax(X):
+    X_exp = np.exp(X)
+    partition = np.sum(X_exp, axis=1, keepdims=True)
+    return X_exp / partition  # 这里应用了广播机制
+
+
+def cross_entropy(y_hat, y):
+    return -np.log(y_hat[range(len(y_hat)), y])
+
+
+def evaluate_accuracy(net, data_iter):
+    """计算在指定数据集上模型的精度"""
+    metric = Accumulator(2)  # 正确预测数、预测总数
+    for X, y in data_iter:
+        metric.add(accuracy(net(X), y), len(y))
+    return metric[0] / metric[1]
+
+
+def evaluate_loss(net, data_iter, loss):
+    """Evaluate the loss of a model on the given dataset."""
+    metric = Accumulator(2)  # Sum of losses, no. of examples
+    for X, y in data_iter:
+        l = loss(net(X), y)
+        metric.add(float(sum(l)), len(l))
+    return metric[0] / metric[1]
+
+
+def train(net, train_iter, test_iter, loss, num_epochs, updater, ylim=None):
+    """训练模型（定义见第3章）。"""
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=ylim,
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    #train_loss, train_acc = train_metrics
