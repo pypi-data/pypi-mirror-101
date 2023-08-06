a109175109802cfe97d9ef81a4dc2d61f8066c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:50 +0000

    Add package amo-partner-crawler/2.0.7/amo_partner_crawler-2.0.7.tar.gz

---
+++ packages/amo-partner-crawler/amo_partner_crawler-2.0.7.tar.gz/amo_partner_crawler-2.0.7/amo_partner_crawler/schemas.py
@@ -0,0 +1,34 @@
+import re
+import time
+import datedelta
+from datetime import datetime
+
+from pydantic import BaseModel, validator
+
+
+class BillingSchema(BaseModel):
+    price: int = 0
+    users_count: int = 0
+    tariff: str = 'Неопределен'
+    pay_period: int = int(time.time())
+    is_paid: bool = False
+    is_unrecognized: bool = False
+
+    @validator('price', pre=True)
+    def _price_validator(cls, value):
+        value = int(value)
+        if value != 0:
+            value *= 2
+        return value
+
+    @validator('users_count', pre=True)
+    def _users_count_validator(cls, value):
+        return int(value)
+
+    @validator('pay_period', pre=True)
+    def _pay_period_validator(cls, value):
+        match = re.search(r'(\d{2}.\d{2}.\d{4})', value).group(1)
+        parsed = datetime.strptime(match, "%d.%m.%Y")
+        sub_thirteen_months = parsed - datedelta.datedelta(months=13)
+        correct_pay_period = time.mktime(sub_thirteen_months.timetuple())
+        return correct_pay_period
