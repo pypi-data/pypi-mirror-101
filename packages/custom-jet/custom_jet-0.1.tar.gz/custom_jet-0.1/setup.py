a89a67822b03bf78e16684656dee85d20d7d9cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:43 +0000

    Add package custom-jet/0.1/custom_jet-0.1.tar.gz

---
+++ packages/custom-jet/custom_jet-0.1.tar.gz/custom_jet-0.1/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+setup(
+  name = 'custom_jet',
+  packages = ['custom_jet'],
+  version = '0.1',
+  license='MIT',
+  description = '',
+  author = 'Wassim',
+  author_email = '',
+  url = 'https://github.com/wass6/custom_jet',
+  download_url = 'https://github.com/wass6/custom_jet/archive/pypi-0_1_3.tar.gz',
+  keywords = [],
+  install_requires=[
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+
+    # Pick your license as you wish
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
