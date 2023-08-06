c460c66ffe6ba8752360f96457f0892139f84aad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:42 +0000

    Add package awsee/0.0.6/awsee-0.0.6.tar.gz

---
+++ packages/awsee/awsee-0.0.6.tar.gz/awsee-0.0.6/awsee/general.py
@@ -0,0 +1,16 @@
+import sys
+from os.path import expanduser
+
+TEMP_CACHE_FOLDER = expanduser("~") + "/.awsee/tmp/"
+
+def printPy(message: str, end: str=None):
+    readyMsg = str(message)
+    if end:
+       print(readyMsg, file=sys.stderr, end=end)
+    else:
+       print(readyMsg, file=sys.stderr)
+
+def printOut(message: str):
+    readyMsg = str(message)
+    print(readyMsg)
+    
