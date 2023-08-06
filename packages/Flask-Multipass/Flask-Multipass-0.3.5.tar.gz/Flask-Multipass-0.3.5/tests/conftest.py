5fe8adca1a2a3518a7899d56c2643c234bde04aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:52 +0000

    Add package Flask-Multipass/0.3.5/Flask-Multipass-0.3.5.tar.gz

---
+++ packages/Flask-Multipass/Flask-Multipass-0.3.5.tar.gz/Flask-Multipass-0.3.5/tests/conftest.py
@@ -0,0 +1,16 @@
+# This file is part of Flask-Multipass.
+# Copyright (C) 2015 - 2017 CERN
+#
+# Flask-Multipass is free software; you can redistribute it
+# and/or modify it under the terms of the Revised BSD License.
+
+from __future__ import unicode_literals
+
+import flask_multipass
+
+
+def pytest_configure(config):
+    # Disable the support attr checks while testing
+    attrs = ('AuthProvider', 'Group', 'IdentityProvider')
+    for attr in attrs:
+        getattr(flask_multipass, attr).__support_attrs__ = {}
