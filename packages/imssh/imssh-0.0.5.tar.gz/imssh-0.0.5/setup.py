c129d61c0c3739b30e57d040f9be3d246bb043f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:29 +0000

    Add package imssh/0.0.5/imssh-0.0.5.tar.gz

---
+++ packages/imssh/imssh-0.0.5.tar.gz/imssh-0.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, Extension
+from setuptools import find_packages
+
+if __name__ == "__main__":
+    setup(
+        name="imssh",
+        scripts=["scripts/imssh"],
+        version="0.0.5",
+        description="A paramiko based, ssh automation tool.",
+        long_description_content_type="text/markdown",
+        author="Nitin Rai",
+        author_email="mneonizer@gmail.com",
+        url="https://github.com/imneonizer/imssh",
+        license="MIT License",
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=["paramiko>=2.7.2"],
+        platforms=["linux", "unix"],
+        python_requires=">3.5.2",
+    )
