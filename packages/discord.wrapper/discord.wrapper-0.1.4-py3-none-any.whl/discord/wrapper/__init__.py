a0ddc64346990a9e50aee093665eceb262344ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:11 +0000

    Add package discord.wrapper/0.1.4/discord.wrapper-0.1.4-py3-none-any.whl

---
+++ packages/discord.wrapper/discord.wrapper-0.1.4-py3-none-any.whl/discord/wrapper/__init__.py
@@ -0,0 +1,7 @@
+from .bot import Bot, InvokeError
+from .gateway import Gateway, InvokeError
+from .intents import Intents
+from .http_client import Route, HTTPClient
+from .embed import Embed
+
+__version__ = "0.1.4"
