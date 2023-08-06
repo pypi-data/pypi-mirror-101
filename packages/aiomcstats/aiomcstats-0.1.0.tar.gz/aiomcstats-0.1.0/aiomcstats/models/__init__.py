73cefdb221c802278076c03966322a3b641c4e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:09 +0000

    Add package aiomcstats/0.1.0/aiomcstats-0.1.0.tar.gz

---
+++ packages/aiomcstats/aiomcstats-0.1.0.tar.gz/aiomcstats-0.1.0/aiomcstats/models/__init__.py
@@ -0,0 +1,15 @@
+from .bedrock import BedrockOffline, BedrockStatus
+from .java import Debug, Info, Status, OfflineStatus, Players, Plugins, Mods, Motd
+
+__all__ = [
+    "BedrockOffline",
+    "BedrockStatus",
+    "Debug",
+    "Info",
+    "Status",
+    "OfflineStatus",
+    "Players",
+    "Plugins",
+    "Mods",
+    "Motd",
+]
