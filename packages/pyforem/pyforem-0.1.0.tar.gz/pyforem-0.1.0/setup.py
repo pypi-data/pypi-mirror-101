d546a141754a236e663ff4d70aca70906dcd6cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:43 +0000

    Add package pyforem/0.1.0/pyforem-0.1.0.tar.gz

---
+++ packages/pyforem/pyforem-0.1.0.tar.gz/pyforem-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyforem']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pyforem',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'aahnik',
+    'author_email': 'daw@aahnik.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
