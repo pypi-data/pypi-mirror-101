10c75a4d6becd882450d2e555a6b9fc554a8d7f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:57 +0000

    Add package pack_demo/1.0/pack_demo-1.0.tar.gz

---
+++ packages/pack_demo/pack_demo-1.0.tar.gz/pack_demo-1.0/pack_demo/__init__.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python
+# _*_coding:utf-8 _*_
+import time
+
+
+def demo():
+    for i in range(6):
+        print("*" * i)
+        time.sleep(2)
+
+
+if __name__ == '__main__':
+    demo()
---
+++ packages/pack_demo/pack_demo-1.0.tar.gz/pack_demo-1.0/setup.py
@@ -0,0 +1,9 @@
+# !/usr/bin/python
+# _*_coding:utf-8 _*_
+from setuptools import find_packages, setup
+
+setup(
+    name='pack_demo',  # 程序名称
+    version='1.0',  # 程序版本号
+    packages=find_packages(),  # 包含的包列表
+)
