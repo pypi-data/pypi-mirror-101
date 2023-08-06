9f87d976dcfa3cca9d67f1eb4015eb7e7f8dc762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:53 +0000

    Add package pnno/0.3.7/pnno-0.3.7.tar.gz

---
+++ packages/pnno/pnno-0.3.7.tar.gz/pnno-0.3.7/pnno/anno/lmdb.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2021/4/3 下午3:26
+@file: lmdb.py
+@author: zj
+@description: 
+"""
+
+import os
+import lmdb
+import pickle
+from tqdm import tqdm
+
+from . import registry
+from .base_anno import BaseAnno
+from ..util.logger import setup_logger
+
+
+def dumps_data(obj):
+    """
+    Serialize an object.
+    Returns:
+        Implementation-dependent bytes-like object
+    """
+    return pickle.dumps(obj)
+
+
+@registry.ANNOS.register('lmdb')
+class LMDB(BaseAnno):
+
+    def __init__(self, cfg) -> None:
+        self.name = cfg.LMDB.NAME
+
+        if cfg.ANNO.CREATOR == self.name:
+            self.dst_dir = cfg.OUTPUT.DIR
+            self.image_folder = cfg.OUTPUT.IMAGE_FOLDER
+
+        self.verbose = cfg.ANNO.VERBOSE
+        self.logger = setup_logger(__name__)
+
+    def process(self) -> dict:
+        pass
+
+    def save(self, input_data: dict):
+        verbose = self.verbose
+        logger = self.logger
+
+        lmdb_path = os.path.join(self.dst_dir, f'{self.image_folder}.lmdb')
+        if verbose:
+            logger.info(f"Generate LMDB to {lmdb_path}")
+
+        data_loader = input_data['dataloader']
+        classes = input_data['classes']
+        self.folder2lmdb(lmdb_path, data_loader, classes)
+
+        if self.verbose:
+            logger.info(__name__ + ' done')
+
+    def folder2lmdb(self, dpath, data_loader, classes, write_frequency=5000):
+        verbose = self.verbose
+        logger = self.logger
+
+        db = lmdb.open(dpath, subdir=False,
+                       map_size=1099511627776 * 2, readonly=False,
+                       meminit=False, map_async=True)
+
+        txn = db.begin(write=True)
+        idx = 0
+        for image, label in tqdm(data_loader):
+            txn.put(u'{}'.format(idx).encode('ascii'), dumps_data((image, label)))
+
+            idx += 1
+            if idx % write_frequency == 0:
+                # if verbose:
+                #     logger.info("[%d/%d]" % (idx, len(data_loader)))
+                txn.commit()
+                txn = db.begin(write=True)
+
+        # finish iterating through dataset
+        txn.commit()
+        keys = [u'{}'.format(k).encode('ascii') for k in range(len(data_loader))]
+        with db.begin(write=True) as txn:
+            txn.put(b'__keys__', dumps_data(keys))
+            txn.put(b'__len__', dumps_data(len(keys)))
+            txn.put(b'classes', dumps_data(classes))
+
+        if verbose:
+            logger.info("Flushing database ...")
+        db.sync()
+        db.close()
---
+++ packages/pnno/pnno-0.3.7.tar.gz/pnno-0.3.7/pnno/anno/visdrone_anno.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/7/17 下午7:40
+@file: visdrone_anno.py
+@author: zj
+@description: 
+"""
+
+import os
+import glob
+import numpy as np
+import cv2
+
+from . import registry
+from ..anno.base_anno import BaseAnno
+from ..util.misc import check_image_label, check_input_output_folder
+from ..util.logger import setup_logger
+
+
+@registry.ANNOS.register('visdrone')
+class VisDroneAnno(BaseAnno):
+    """
+    [VisDrone](http://aiskyeye.com/) data set for UAV target detection training
+    Download address: [VisDrone/VisDrone-Dataset](https://github.com/VisDrone/VisDrone-Dataset)
+    Data storage format:
+    root/
+        images/
+        annotations/
+    The annotation file (. Txt) corresponds to the image one by one, and each line represents a annotation object, with a total of 8 fields
+    refer to [Object Detection in Images](http://aiskyeye.com/evaluate/results-format/). The format is as follows
+    <bbox_left>,<bbox_top>,<bbox_width>,<bbox_height>,<score>,<object_category>,<truncation>,<occlusion>
+    Ignore in category ignored-regions/others
+    """
+
+    classmap = {'ignored-regions': 0, 'pedestrian': 1, 'people': 2, 'bicycle': 3, 'car': 4, 'van': 5, 'truck': 6,
+                'tricycle': 7, 'awning-tricycle': 8, 'bus': 9, 'motor': 10, 'others': 11}
+
+    def __init__(self, cfg):
+        self.name = cfg.VISDRONE.NAME
+        self.img_extension = cfg.VISDRONE.IMG_EXTENSION
+        self.anno_extension = cfg.VISDRONE.ANNO_EXTENSION
+
+        if cfg.ANNO.PARSER == self.name:
+            self.src_dir = cfg.INPUT.DIR
+            self.image_folder = cfg.INPUT.IMAGE_FOLDER
+            self.label_folder = cfg.INPUT.LABEL_FOLDER
+        if cfg.ANNO.CREATOR == self.name:
+            self.dst_dir = cfg.OUTPUT.DIR
+            self.image_folder = cfg.OUTPUT.IMAGE_FOLDER
+            self.label_folder = cfg.OUTPUT.LABEL_FOLDER
+
+        self.verbose = cfg.ANNO.VERBOSE
+
+        self.logger = setup_logger(__name__)
+
+    def get_class_name(self, value):
+        return [k for k, v in self.classmap.items() if v == value][0]
+
+    def create_anno(self, anno_line) -> dict:
+        # ignore ignored-regions/others
+        xmin, ymin, width, height, _, cate = anno_line[:6].astype(np.int)
+        xmax = xmin + width
+        ymax = ymin + height
+        if cate == 0 or cate == 11:
+            return dict()
+        name = self.get_class_name(cate)
+        return {'name': name, 'bndbox': [xmin, ymin, xmax, ymax]}
+
+    def parse_anno(self, img_path, anno_path) -> dict:
+        """
+        Parse vistrone image and annotation file
+        :returns: anno_obj
+        """
+        anno_obj = dict()
+
+        img = cv2.imread(img_path)
+        if len(img.shape) == 3:
+            h, w, d = img.shape[:3]
+            anno_obj['size'] = (w, h, d)
+        else:
+            h, w = img.shape[:2]
+            anno_obj['size'] = (w, h, 1)
+
+        anno_array = np.loadtxt(anno_path, dtype=np.str, delimiter=',')
+        objects = list()
+        if len(anno_array.shape) == 1:
+            # Just one annotation object
+            obj = self.create_anno(anno_array)
+            if obj:
+                objects.append(obj)
+        else:
+            for anno_line in anno_array:
+                obj = self.create_anno(anno_line)
+                if obj:
+                    objects.append(obj)
+        if len(objects) == 0:
+            return dict()
+        anno_obj['objects'] = objects
+        return anno_obj
+
+    def process(self) -> dict:
+        src_dir = self.src_dir
+        image_folder = self.image_folder
+        label_folder = self.label_folder
+        img_extension = self.img_extension
+        anno_extension = self.anno_extension
+        verbose = self.verbose
+        logger = self.logger
+
+        image_dir, label_dir = check_input_output_folder(src_dir, image_folder, label_folder, is_input=True)
+
+        img_path_list = sorted(glob.glob(os.path.join(image_dir, '*' + img_extension)))
+        anno_path_list = sorted(glob.glob(os.path.join(label_dir, '*' + anno_extension)))
+        check_image_label(img_path_list, anno_path_list)
+
+        anno_data = dict()
+        for i, (img_path, anno_path) in enumerate(zip(img_path_list, anno_path_list), 1):
+            if verbose:
+                logger.info('parse {}'.format(anno_path))
+
+            anno_obj = self.parse_anno(img_path, anno_path)
+            if anno_obj:
+                anno_data[img_path] = anno_obj
+            else:
+                self.logger.info(
+                    '{} is a annotation object that only contains targets whose annotation type is ignored regions or others'.format(
+                        img_path))
+
+        return {'classmap': self.classmap, 'anno_data': anno_data}
+
+    def save(self, input_data):
+        super(VisDroneAnno, self).save(input_data)
+        pass
