540ad005eef61c028a5c1fc714e5433cc8c42d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:50 +0000

    Add package howdyho/1.0/howdyho-1.0.tar.gz

---
+++ packages/howdyho/howdyho-1.0.tar.gz/howdyho-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    version='1.0',
+    name='howdyho',
+    packages=['howdyho'],
+    description='while True:\n    print("хаудихо помоги")',
+    author='Voronin9032',
+    author_email='voronin9032n3@gmail.com',
+    zip_safe=False
+)
