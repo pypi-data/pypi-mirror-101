c340a9e2fab1e6f4a5ec28ed44e6b47cc43c6226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:07 +0000

    Add package Suluoya/2.1.2/Suluoya-2.1.2.tar.gz

---
+++ packages/Suluoya/Suluoya-2.1.2.tar.gz/Suluoya-2.1.2/Suluoya/stock/__init__.py
@@ -0,0 +1,10 @@
+__name__ = "Suluoya"
+__author__ = 'Suluoya'
+__all__ = ['Markovitz', 'GetGoodStock', 'GetData', 'GetDate', 'gui']
+
+from .GetGoodStock import GetGoodStock
+from .GetDate import GetDate
+from .GetData import StockData, HolidayStockData, ConstituentStock, StockAbility
+from .Markovitz import Markovitz, calculate
+from .gui import gui, StockGui
+import pretty_errors
