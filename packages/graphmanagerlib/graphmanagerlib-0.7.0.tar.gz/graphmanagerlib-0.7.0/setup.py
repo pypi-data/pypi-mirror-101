c13c166ae11352d054086a11ce8e537f8b9e9a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:35 +0000

    Add package graphmanagerlib/0.7.0/graphmanagerlib-0.7.0.tar.gz

---
+++ packages/graphmanagerlib/graphmanagerlib-0.7.0.tar.gz/graphmanagerlib-0.7.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+import graphmanagerlib
+
+setup(
+    name='graphmanagerlib',
+    packages=find_packages(),
+    version='0.7.0',
+    description='My first Python library',
+    author='Cambier Damien',
+    license='MIT',
+    install_requires=['tensorflow', 'pandas', 'networkx', 'torch==1.8.0', 'torch-geometric', 'stellargraph','torch_sparse'],
+)
