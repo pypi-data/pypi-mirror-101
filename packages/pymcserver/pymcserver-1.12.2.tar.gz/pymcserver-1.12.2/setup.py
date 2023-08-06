b4811ffc9f22399e02d32e4f81c8457acaf0738d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:22 +0000

    Add package pymcserver/1.12.2/pymcserver-1.12.2.tar.gz

---
+++ packages/pymcserver/pymcserver-1.12.2.tar.gz/pymcserver-1.12.2/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(
+    name='pymcserver',
+    version='1.12.2',
+    description='MC Server Wrapper',
+    long_description='Add later',
+    install_requires=["pyautogui"],
+    packages=["pymcserver"]
+    )
+
