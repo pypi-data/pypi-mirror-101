8aacf6dd21a13d5edb2cdf973381a9447ae3deb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:24 +0000

    Add package PSM-testbed/0.0.0.26/PSM_testbed-0.0.0.26-py3-none-any.whl

---
+++ packages/PSM-testbed/PSM_testbed-0.0.0.26-py3-none-any.whl/PSM_testbed/WhACC/examples/gui_example.py
@@ -0,0 +1,8 @@
+H5_file_name = '/Users/phil/Dropbox/Autocurator/testing_data/MP4s/AH0667x170317_JON/AH0667x170317-.h5'
+label_key = 'labels'
+
+
+
+from PSM_testbed.WhACC.touch_curation_GUI import touch_gui
+
+touch_gui(H5_file_name, label_key)
