7b9ae65eb876f5c1d4539b429532503c6f904669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:00 +0000

    Add package sciscripts/3.0.3/sciscripts-3.0.3-py3-none-any.whl

---
+++ packages/sciscripts/sciscripts-3.0.3-py3-none-any.whl/sciscripts/Analysis/Units/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: T. Malfatti <malfatti@disroot.org>
+@date: 20170612
+@license: GNU GPLv3 <https://gitlab.com/malfatti/SciScripts/raw/master/LICENSE>
+@homepage: https://gitlab.com/Malfatti/SciScripts
+"""
+
+from sciscripts.Analysis.Units import Circus
+from sciscripts.Analysis.Units import Klusta
+from sciscripts.Analysis.Units import Units
+from sciscripts.Analysis.Units import WaveClus
+
