330a335fedc79132f766d116972cb8a144897594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:15 +0000

    Add package reflective-listening/0.1.3/reflective_listening-0.1.3.tar.gz

---
+++ packages/reflective-listening/reflective_listening-0.1.3.tar.gz/reflective_listening-0.1.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='reflective_listening',
+    version='0.1.3',
+    description='Reflective listening statements via paraphrase generation',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/monmanuela/reflective_listening',
+    author='Monika Manuela Hengki',
+    author_email='e0014971@u.nus.edu',
+    license='MIT',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires='>=3.6.2',
+    install_requires=[
+        'language_tool_python',
+        'nltk',
+        'numpy',
+        'sentence_transformers',
+        'scipy==1.4.1',
+        'sentencepiece',
+        'torch',
+        'transformers',
+    ],
+    zip_safe=False
+)
