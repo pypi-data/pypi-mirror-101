8f3f50dcd2b57c7d0695a0cc41def0a88456c171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:17 +0000

    Add package makeh/0.3.1/makeh-0.3.1.tar.gz

---
+++ packages/makeh/makeh-0.3.1.tar.gz/makeh-0.3.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['makeh']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Jinja2>=2.11.3,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['makeh = makeh:run']}
+
+setup_kwargs = {
+    'name': 'makeh',
+    'version': '0.3.1',
+    'description': 'Online documentation for GNU Makefiles',
+    'long_description': None,
+    'author': 'Jackson Gilman',
+    'author_email': 'jackson.j.gilman@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
