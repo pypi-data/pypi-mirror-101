feac13e0969d78231126fc1f446e13bea0a4d6c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:07 +0000

    Add package obs-scene-transporter/0.9.1/obs-scene-transporter-0.9.1.tar.gz

---
+++ packages/obs-scene-transporter/obs-scene-transporter-0.9.1.tar.gz/obs-scene-transporter-0.9.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="obs-scene-transporter",
+    version="0.9.1",
+    author="Stefan Bethke",
+    author_email="stb@lassitu.de",
+    description="Import and export OBS Studio scenes including all assets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/stblassitude/obs-scene-transporter",
+    project_urls={
+        "Bug Tracker": "https://github.com/stblassitude/obs-scene-transporter/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': ['obs-scene-transporter=obscenetransporter:main']
+    },
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+)
