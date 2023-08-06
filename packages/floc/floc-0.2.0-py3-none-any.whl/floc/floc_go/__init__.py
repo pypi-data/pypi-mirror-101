c5888ea545900dadcc57971b2ee1d849162aba11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:33 +0000

    Add package floc/0.2.0/floc-0.2.0-py3-none-any.whl

---
+++ packages/floc/floc-0.2.0-py3-none-any.whl/floc/floc_go/__init__.py
@@ -0,0 +1,29 @@
+import ctypes
+import sys
+from importlib import resources
+
+from floc.go_types import ApplySortingLshResult, GoSlice, SimulateResult
+
+floc_go_so = f'floc_go-{sys.platform}.so'
+with resources.path('floc.floc_go', floc_go_so) as floc_go_file:
+    FLOC_GO = ctypes.cdll.LoadLibrary(str(floc_go_file))
+
+FLOC_GO.cityHash64V103.argtypes = [ctypes.c_char_p]
+FLOC_GO.cityHash64V103.restype = ctypes.c_uint64
+
+FLOC_GO.cityHash64WithSeedsV103.argtypes = [
+    ctypes.c_char_p, ctypes.c_uint64, ctypes.c_uint64,
+]
+FLOC_GO.cityHash64WithSeedsV103.restype = ctypes.c_uint64
+
+FLOC_GO.cityHash64WithSeedV103.argtypes = [ctypes.c_char_p, ctypes.c_uint64]
+FLOC_GO.cityHash64WithSeedV103.restype = ctypes.c_uint64
+
+FLOC_GO.simHashString.argtypes = [GoSlice]
+FLOC_GO.simHashString.restype = ctypes.c_uint64
+
+FLOC_GO.applySortingLsh.argtypes = [ctypes.c_uint64, ctypes.c_char_p]
+FLOC_GO.applySortingLsh.restype = ApplySortingLshResult
+
+FLOC_GO.simulate.argtypes = [GoSlice, ctypes.c_char_p]
+FLOC_GO.simulate.restype = SimulateResult
