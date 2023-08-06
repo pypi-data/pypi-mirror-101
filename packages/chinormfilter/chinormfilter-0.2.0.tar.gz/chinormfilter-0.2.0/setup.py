952c6a8a3c28f9cd5eeae72480a3f78db2843c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:08 +0000

    Add package chinormfilter/0.2.0/chinormfilter-0.2.0.tar.gz

---
+++ packages/chinormfilter/chinormfilter-0.2.0.tar.gz/chinormfilter-0.2.0/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['chinormfilter']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['sudachidict_core>=20201223.post1,<20201224',
+ 'sudachidict_full>=20201223.post1,<20201224',
+ 'sudachipy>=0.5.2,<0.6.0']
+
+entry_points = \
+{'console_scripts': ['chinormfilter = chinormfilter.cli:cli']}
+
+setup_kwargs = {
+    'name': 'chinormfilter',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
