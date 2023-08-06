a1283017fa211a2a037dc27cbc60c1e1eeab2abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:16 +0000

    Add package Reddit-ChatBot-Python/1.2.6/Reddit_ChatBot_Python-1.2.6.tar.gz

---
+++ packages/Reddit-ChatBot-Python/Reddit_ChatBot_Python-1.2.6.tar.gz/Reddit_ChatBot_Python-1.2.6/Reddit_ChatBot_Python/_utils/ws_utils.py
@@ -0,0 +1,32 @@
+from urllib.parse import urlencode
+from .consts import SB_User_Agent, SB_ai
+
+
+def get_ws_url(user_id, access_token):
+    socket_base = "wss://sendbirdproxyk8s.chat.redditmedia.com"
+    ws_params = {
+        "user_id": user_id,
+        "access_token": access_token,
+        "p": "Android",
+        "pv": 30,
+        "sv": "3.0.144",
+        "ai": SB_ai,
+        "SB-User-Agent": SB_User_Agent,
+        "active": "1"
+    }
+    return f"{socket_base}/?{urlencode(ws_params)}"
+
+
+def pair_channel_and_names(channels: list, user_id: str):
+    channelid_sub_pairs = {}
+    for channel in channels:
+        room_name = None
+        if channel.custom_type == "direct":
+            for member in channel.members:
+                if member.user_id != user_id:
+                    room_name = member.nickname
+                    break
+        else:
+            room_name = channel.channel.name
+        channelid_sub_pairs.update({channel.channel.channel_url: room_name})
+    return channelid_sub_pairs
