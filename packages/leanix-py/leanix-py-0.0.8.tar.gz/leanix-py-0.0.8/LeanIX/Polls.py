7300100343e2290824416c97589a89f8eaf30f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:23 +0000

    Add package leanix-py/0.0.8/leanix-py-0.0.8.tar.gz

---
+++ packages/leanix-py/leanix-py-0.0.8.tar.gz/leanix-py-0.0.8/LeanIX/Polls.py
@@ -0,0 +1,117 @@
+import LeanIX
+import requests
+import datetime 
+import dateutil.parser
+import json
+import uuid
+
+
+class Polls:
+    def __init__(self,lix):
+        self.auth_header = lix._auth_header
+        self.lix = lix
+        self.allpolls=self.getPolls()
+
+    def startPollRun(self,pollrun):
+        apiendpoint = "services/poll/v2/pollRuns?type=LIVE"
+        url = self.lix.baseurl+ apiendpoint
+
+        pr = {}
+        pr['additionalFactSheetCheckEnabled'] = True
+        pr['endTime'] = None
+        pr['id'] = str(uuid.uuid4())
+        pr['legacyId'] = None
+
+        pr['poll'] = pollrun['poll']
+        pr['pollCopy'] = pollrun['pollCopy']
+        pr['pollResultType'] = "PER_FACTSHEET"
+        pr['pollRunType'] = "LIVE"
+        pr['progress'] = None
+        pr['sendChangeNotifications'] = True
+        pr['users'] = None
+        pr['status'] = 'STARTED'
+        pr['startTime'] = datetime.datetime.utcnow().isoformat().split(".")[0]+"Z"
+        
+
+        pollrun['status'] = "STARTED"
+        pollrun['startTime'] = datetime.datetime.utcnow().isoformat().split(".")[0]+"Z"
+        pollrun['endTime'] = None
+        #pollrun['id'] = None
+        retstatus =  requests.post(url,headers=self.lix.header,data=json.dumps(pr))
+        a=1
+
+    
+    def getPolls(self,query=None):
+        apiendpoint = "services/poll/v2/polls"
+        url = self.lix.baseurl+ apiendpoint
+
+        if query:
+            return requests.get(url,headers=self.lix.header,params={"q",query}).json()
+        else:
+            return requests.get(url,headers=self.lix.header).json()
+
+    def getPollRunResults(self,pollrunid):
+        apiendpoint = f"services/poll/v2/pollRuns/{pollrunid}/pollResults"
+        url = self.lix.baseurl+ apiendpoint
+        results = requests.get(url,headers=self.lix.header).json()        
+        
+        return results        
+        a=1
+
+    def getPollRuns(self,pollid):
+        apiendpoint = f"services/poll/v2/polls/{pollid}/pollRuns"
+        url = self.lix.baseurl+ apiendpoint
+        results = requests.get(url,headers=self.lix.header).json()
+        for r in results['data']:
+            r['pollResults'] = self.getPollRunResults(r['id'])['data']
+        return results       
+        a=1
+
+    def getPollRunReminders(self,pollrunid):
+        apiendpoint = f"services/poll/v2/pollRuns/{pollrunid}/reminders"
+        url = self.lix.baseurl+ apiendpoint        
+        return requests.get(url,headers=self.lix.header).json()
+
+    def endPollRun(self,pollrun):
+        apiendpoint = f"services/poll/v2/pollRuns/{pollrun['id']}"
+        url = self.lix.baseurl+ apiendpoint
+        # updatedata = {
+        #         "id": pollrunid,
+        #         "endTime": datetime.datetime.utcnow().isoformat().split(".")[0]+"Z",
+        #         "status": "FINISHED",
+        #         "language": "en"
+
+        # }
+        pollrun['status'] = "FINISHED"
+        pollrun['endTime'] = datetime.datetime.utcnow().isoformat().split(".")[0]+"Z"
+        retstatus =  requests.put(url,headers=self.lix.header,data=json.dumps(pollrun))
+        return retstatus
+
+    def deletePollRun(self,pollrunid):
+        apiendpoint = f"services/poll/v2/pollRuns/{pollrunid}"
+        url = self.lix.baseurl+ apiendpoint
+        updatedata = {
+            "status": "FINISHED",
+            "language": "en",
+            "endTime": datetime.datetime.utcnow().isoformat().split(".")[0]+".000Z"
+        }
+        retstatus =  requests.delete(url,headers=self.lix.header)
+        return retstatus
+    def sendPollReminder(self,pollrun):
+        apiendpoint = f"services/poll/v2/pollRuns/{pollrun['id']}/reminder"
+        url = self.lix.baseurl+ apiendpoint
+
+        pollreminder = {
+            "pollRunId": pollrun['id'],
+            "subject": f"[REMINDER] {pollrun['poll']['introductionSubject']}",
+            "message": pollrun['poll']['introductionText']
+        }
+
+        retval =  requests.post(url=url,headers=self.lix.header,data=json.dumps(pollreminder))
+        return retval
+        aa=1
+
+
+if __name__=="__main__":
+    pass
+
