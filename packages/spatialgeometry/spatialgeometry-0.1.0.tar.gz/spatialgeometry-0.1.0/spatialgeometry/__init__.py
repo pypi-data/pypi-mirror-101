f42a0bd3225fa2b2fc9afe04b2f7b2ec16eac20b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:54 +0000

    Add package spatialgeometry/0.1.0/spatialgeometry-0.1.0.tar.gz

---
+++ packages/spatialgeometry/spatialgeometry-0.1.0.tar.gz/spatialgeometry-0.1.0/spatialgeometry/__init__.py
@@ -0,0 +1,11 @@
+from spatialgeometry.Shape import Shape
+from spatialgeometry.CollisionShape import CollisionShape, Mesh, Cylinder, Box, Sphere
+
+__all__ = [
+    'Shape',
+    'CollisionShape',
+    'Mesh',
+    'Cylinder',
+    'Box',
+    'Sphere'
+]
