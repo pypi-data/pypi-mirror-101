85cc99f96238dc19f027671c60e164a80b41e28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:33 +0000

    Add package tpjl/2021.4.2/tpjl-2021.4.2.tar.gz

---
+++ packages/tpjl/tpjl-2021.4.2.tar.gz/tpjl-2021.4.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="tpjl",
+    version="2021.04.02",
+    author="TechGeeks",
+    author_email="PJSONLang@tgeeks.cf",
+    maintainer="Rajdeep Malakar",
+    maintainer_email="Rajdeep@tgeeks.cf",
+    description="a free & OpenSource Portable version of JSON.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TechGeeks-Dev/PJSONLang",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
