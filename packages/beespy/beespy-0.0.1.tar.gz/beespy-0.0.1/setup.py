28bd5f80f81039c065599fc58cc42ed2acd67d90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:32 +0000

    Add package beespy/0.0.1/beespy-0.0.1.tar.gz

---
+++ packages/beespy/beespy-0.0.1.tar.gz/beespy-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='beespy',
+    packages=['beespy'],
+    version='0.0.1',
+    license='MIT',
+    description='This class provides methodes for using BeeSpy API',
+    author='Andreas Ziegler, Benjamin Dollinger',
+    author_email='ziegler.andi@icloud.com'
+    )
