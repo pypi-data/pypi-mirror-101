31b8a95727e9af23a621835420a26fb09d70a958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:59 +0000

    Add package AppZoo/2021.4.12.19.33.19/appzoo-2021.4.12.19.33.19.tar.gz

---
+++ packages/AppZoo/appzoo-2021.4.12.19.33.19.tar.gz/appzoo-2021.4.12.19.33.19/apps/mongo_item.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AppZoo.
+# @File         : mongo_item
+# @Time         : 2021/4/12 7:17 下午
+# @Author       : yuanjie
+# @WeChat       : 313303303
+# @Software     : PyCharm
+# @Description  :
+
+from meutils.pipe import *
+from meutils.zk_utils import get_zk_config
+from meutils.db.mongo import Mongo
+
+m = Mongo()
+collection_name = 'item'
+c = m.db[collection_name]
+
+
+# df['date'] = datetime.datetime.utcnow()
+
+@lru_cache()
+def insert(**kwargs):
+    # docid = kwargs.get('data', 'id')
+    kwargs.pop('_id', None)
+
+    c.insert(kwargs)
+    return 'ok'
+
+
+@lru_cache()
+def query(**kwargs):
+    key = kwargs.get('key', {})
+    return c.find_one(key)
+
+
+if __name__ == '__main__':
+    from appzoo import App
+
+    app = App()
+    app.add_route('/mongo/item/insert', insert, method="POST")
+    app.add_route('/mongo/item/query', query, method="GET")
+
+    app.run(port=8000, access_log=False)
