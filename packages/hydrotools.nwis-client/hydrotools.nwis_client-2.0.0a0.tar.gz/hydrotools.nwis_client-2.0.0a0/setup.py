06805f45f3f65801083b96e471ce99308b3a4e78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:52 +0000

    Add package hydrotools.nwis-client/2.0.0a0/hydrotools.nwis_client-2.0.0a0.tar.gz

---
+++ packages/hydrotools.nwis-client/hydrotools.nwis_client-2.0.0a0.tar.gz/hydrotools.nwis_client-2.0.0a0/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_namespace_packages
+
+# python namespace subpackage
+# this namespace package follows PEP420
+# https://packaging.python.org/guides/packaging-namespace-packages/#native-namespace-packages
+
+NAMESPACE_PACKAGE_NAME = "hydrotools"
+SUBPACKAGE_NAME = "nwis_client"
+
+# Namespace subpackage slug.
+# Ex: mypkg.a # Where the namespace pkg = `mypkg` and the subpackage = `a`
+SUBPACKAGE_SLUG = f"{NAMESPACE_PACKAGE_NAME}.{SUBPACKAGE_NAME}"
+
+# Subpackage version
+VERSION = "2.0.0-alpha.0"
+URL = "https://github.com/NOAA-OWP/hydrotools"
+
+# Package author information
+AUTHOR = "Austin Raney"
+AUTHOR_EMAIL = "arthur.raney@noaa.gov"
+
+# Short sub-package description
+DESCRIPTION = (
+    "A convenient interface to the USGS NWIS Instantaneous Values (IV) REST Service API"
+)
+
+# Package dependency requirements
+REQUIREMENTS = [
+    "pandas",
+    "numpy",
+    "requests",
+    "requests-cache",
+    "hydrotools.-restclient",
+]
+
+setup(
+    name=SUBPACKAGE_SLUG,
+    version=VERSION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    classifiers=[],
+    description=DESCRIPTION,
+    namespace_packages=[NAMESPACE_PACKAGE_NAME],
+    packages=find_namespace_packages(include=[f"{NAMESPACE_PACKAGE_NAME}.*"]),
+    install_requires=REQUIREMENTS,
+)
