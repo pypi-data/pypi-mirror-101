e33c3442852f70921d76ee324b273218710d648e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:37 +0000

    Add package pyamg/4.1.0/pyamg-4.1.0-cp39-cp39-macosx_11_0_x86_64.whl

---
+++ packages/pyamg/pyamg-4.1.0-cp39-cp39-macosx_11_0_x86_64.whl/pyamg/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '4.1.0'
+version = '4.1.0'
+full_version = '4.1.0'
+git_revision = '7a74ef7f2a18b24d640ba8397822b8c2d2f8aa93'
+release = True
+if not release:
+    version = full_version
