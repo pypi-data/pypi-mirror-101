cf7bc13bff1fef122360b76cd2342f62be724b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:48 +0000

    Add package dolt-integrations/0.1.0/dolt_integrations-0.1.0.tar.gz

---
+++ packages/dolt-integrations/dolt_integrations-0.1.0.tar.gz/dolt_integrations-0.1.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dolt_integrations',
+ 'dolt_integrations.metaflow',
+ 'dolt_integrations.metaflow.examples.hospital_bounty',
+ 'dolt_integrations.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['doltcli>=0.1.4,<0.2.0']
+
+extras_require = \
+{'metaflow': ['pandas>=0.25.2', 'metaflow>=2.2.6,<3.0.0']}
+
+setup_kwargs = {
+    'name': 'dolt-integrations',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Max Hoffman',
+    'author_email': 'max@dolthub.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.6.1,<4.0',
+}
+
+
+setup(**setup_kwargs)
