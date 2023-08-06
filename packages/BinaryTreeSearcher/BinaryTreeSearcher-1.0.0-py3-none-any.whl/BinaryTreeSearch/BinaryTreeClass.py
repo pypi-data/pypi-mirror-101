ab5205e7431e545b0cb5f281ad2c91604b79c670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:34 +0000

    Add package BinaryTreeSearcher/1.0.0/BinaryTreeSearcher-1.0.0-py3-none-any.whl

---
+++ packages/BinaryTreeSearcher/BinaryTreeSearcher-1.0.0-py3-none-any.whl/BinaryTreeSearch/BinaryTreeClass.py
@@ -0,0 +1,43 @@
+import math
+from dataclasses import dataclass
+from .BinaryConverter import BinaryConverter
+from .MergeSortClass import MergeSortClass
+
+
+@dataclass
+class BinaryTreeSearch:
+    # searchInput: int
+    # searchTree: list
+
+    def binarySearch(self, mList, search_input):
+        convertedList = BinaryConverter.convertToList(mList)
+        sortedList = MergeSortClass.runMergeSort(convertedList)
+
+        if type(search_input) != int:
+            return "input entered is not a number"
+
+        if len(sortedList) > 0:
+            middle = math.ceil(len(sortedList) / 2)
+            leftSide = sortedList[:middle]
+            rightSide = sortedList[middle:]
+
+            if (middle - 1) == 1 and search_input not in sortedList:
+                return False
+
+            if search_input == sortedList[middle - 1]:
+                return True
+
+            if search_input > sortedList[middle - 1]:
+                sortedList = rightSide
+                return self.binarySearch(sortedList, search_input)
+            else:
+                sortedList = leftSide
+                return self.binarySearch(sortedList, search_input)
+
+            return sortedList
+
+
+myList = [3, [8, [5, None, 11], 94], [7, 15, 29]]
+tree = BinaryTreeSearch()
+newList = tree.binarySearch(myList, 9)
+print(newList)
