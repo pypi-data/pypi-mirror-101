b9ae1f51e63268fc22f48e07726429c81111ebe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:58 +0000

    Add package pyeafe/1.0.0/pyeafe-1.0.0.tar.gz

---
+++ packages/pyeafe/pyeafe-1.0.0.tar.gz/pyeafe-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from pyeafe import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyeafe",
+    version=__version__,
+    description="Edge-Averaged Finite Elements (EAFE) for FENiCS",
+    url="https://thepnpsolver.github.io",
+    author="The PNP Solver",
+    author_email="thepnpsolver@gmail.com",
+    license="MIT License",
+    packages=["pyeafe"],
+    zip_safe=False,
+    install_requires=["numpy>=1.19.0", "petsc4py>=3.12.0", "typing>=3.7.4.3"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    setup_requires=["pytest-runner", "flake8"],
+    tests_require=["pytest"],
+    project_urls={"Source Code": "https://github.com/thepnpsolver/pyeafe"},
+)
