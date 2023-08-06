ef727977c3f2ae83dcf2f77f95bd8dea35eaca30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:06 +0000

    Add package PYPI-PACKAGE-PRUEBA/0.0.4/pypi-package-prueba-0.0.4.tar.gz

---
+++ packages/PYPI-PACKAGE-PRUEBA/pypi-package-prueba-0.0.4.tar.gz/pypi-package-prueba-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pypi-package-prueba",
+    version="0.0.4",
+    license="MIT",
+    description="Paquete de prueba, clase Python",
+    # En caso de que mi paquete incluya librerias de terceros agrego la siguiente linea...
+    # install_requires=["math", "numpy"],
+    packages=find_packages(),
+    author="Benjamin Schell",
+    url="https://github.com/Benjaprogramado/pypi-package-Prueba"
+)
