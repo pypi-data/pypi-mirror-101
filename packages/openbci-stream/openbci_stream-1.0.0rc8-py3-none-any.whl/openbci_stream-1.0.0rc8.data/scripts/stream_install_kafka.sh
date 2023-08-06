0b6c9d3141414b8a8c1aadb6e78fd023bb185827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:29 +0000

    Add package openbci-stream/1.0.0rc8/openbci_stream-1.0.0rc8-py3-none-any.whl

---
+++ packages/openbci-stream/openbci_stream-1.0.0rc8-py3-none-any.whl/openbci_stream-1.0.0rc8.data/scripts/stream_install_kafka.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+function secure_file {
+    if test -f $1.orig; then
+        rm $1
+        cp $1.orig $1
+    else
+        cp $1 $1.orig
+    fi
+}
+
+echo "Installing Yay"
+cd
+sudo pacman -S --needed
+git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
+sudo rm -r yay
+
+echo "Installing Kafka"
+yay --editmenu -S zookeeper --mflags --skipinteg
+yay --editmenu -S kafka --mflags --skipinteg
+
+
+
+
