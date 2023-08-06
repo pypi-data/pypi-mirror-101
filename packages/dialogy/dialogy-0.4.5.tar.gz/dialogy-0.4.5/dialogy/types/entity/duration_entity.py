ce4791c7c56dd738835692fb467a49af1735915a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:54 +0000

    Add package dialogy/0.4.5/dialogy-0.4.5.tar.gz

---
+++ packages/dialogy/dialogy-0.4.5.tar.gz/dialogy-0.4.5/dialogy/types/entity/duration_entity.py
@@ -0,0 +1,23 @@
+"""
+Module provides access to an entity type (class) to handle locations.
+
+Import classes:
+    - LocationEntity
+"""
+from typing import Any, Dict, List
+
+import attr
+
+from dialogy.types.entity import BaseEntity
+
+
+@attr.s
+class DurationEntity(BaseEntity):
+    """Location Entity Type
+
+    Use this type for handling locations available with reference-ids.
+    This is not meant for (latitude, longitude) values, those will be covered in GeoPointEntity.
+    """
+
+    normalized = attr.ib(type=Dict[str, Any], default=attr.Factory(dict))
+    _meta = attr.ib(type=Dict[str, str], default=attr.Factory(dict))
