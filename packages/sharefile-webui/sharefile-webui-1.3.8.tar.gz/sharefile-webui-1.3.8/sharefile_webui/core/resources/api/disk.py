c7aef367e4039ac61cc8450e62b06c5f29c8b2ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:39 +0000

    Add package sharefile-webui/1.3.8/sharefile-webui-1.3.8.tar.gz

---
+++ packages/sharefile-webui/sharefile-webui-1.3.8.tar.gz/sharefile-webui-1.3.8/sharefile_webui/core/resources/api/disk.py
@@ -0,0 +1,17 @@
+import shutil
+from .base import BaseResource
+from ..web import app_auth
+
+
+class Disk(BaseResource):
+    @app_auth.login_required
+    def get(self):
+        usage = shutil.disk_usage(self.root_path)
+        return {
+            "status": True,
+            "diskUsage": {
+                "total": usage.total,
+                "free": usage.free,
+                "used": usage.used,
+            }
+        }
