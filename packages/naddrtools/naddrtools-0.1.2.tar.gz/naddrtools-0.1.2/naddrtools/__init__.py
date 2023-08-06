d9df0f6a81a4829fc95fd7605bdfddac43a4d111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:28 +0000

    Add package naddrtools/0.1.2/naddrtools-0.1.2.tar.gz

---
+++ packages/naddrtools/naddrtools-0.1.2.tar.gz/naddrtools-0.1.2/naddrtools/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+naddrtools
+~~~~~~~~~~~~~~~~~~~~~
+naddrtools is a library, written in Python, for manipulating strings which represent network IP addresses.
+
+:copyright: (c) 2021
+"""
+
+from .naddrtools import *
