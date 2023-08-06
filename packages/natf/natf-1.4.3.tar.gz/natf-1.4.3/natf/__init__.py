4cd04afcac4f465b722ade67bb70116118098385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:50 +0000

    Add package natf/1.4.3/natf-1.4.3.tar.gz

---
+++ packages/natf/natf-1.4.3.tar.gz/natf-1.4.3/natf/__init__.py
@@ -0,0 +1,10 @@
+import os
+from warnings import warn
+
+
+__version__ = '1.4.3'
+
+if os.name == 'nt':
+    p = os.environ['PATH'].split(';')
+    lib = os.path.join(os.path.split(__file__)[0], 'lib')
+    os.environ['PATH'] = ";".join([lib] + p)
