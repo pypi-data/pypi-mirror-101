988f2480ea7356273eea1fe5ff24871054aaac6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:20 +0000

    Add package pybiolib/0.2.96/pybiolib-0.2.96-py3-none-any.whl

---
+++ packages/pybiolib/pybiolib-0.2.96-py3-none-any.whl/biolib/compute_node/webserver/webserver_config.py
@@ -0,0 +1,6 @@
+
+# Time in minutes before a Compute Node always shuts down
+COMPUTE_NODE_AUTO_SHUTDOWN_TIME_MINUTES = 1440 #  24 hours
+
+# Time in minutes before a Compute Node running a job shuts down
+COMPUTE_NODE_RUNNING_JOB_SHUTDOWN_TIME_MINUTES = 30
