e0bfd4c062a0e8d79bded196ed0efd38f6402db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:58 +0000

    Add package unmanic/0.0.4/unmanic-0.0.4.tar.gz

---
+++ packages/unmanic/unmanic-0.0.4.tar.gz/unmanic-0.0.4/unmanic/webserver/assets/pages/scripts/templates.js
@@ -0,0 +1,35 @@
+/* Read remote template file and return the compiled template */
+Handlebars.getTemplate = function (name) {
+    if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined || UNMANIC_VERSION === "UNKNOWN") {
+        $.ajax({
+            url: '/assets/templates/' + name + '.hbs?version=' + UNMANIC_VERSION,
+            success: function (data) {
+                if (Handlebars.templates === undefined) {
+                    Handlebars.templates = {};
+                }
+                Handlebars.templates[name] = Handlebars.compile(data);
+            },
+            async: false
+        });
+    }
+    return Handlebars.templates[name];
+};
+
+/* HELPERS: */
+Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+Handlebars.registerHelper('breaklines', function(text) {
+    text = Handlebars.Utils.escapeExpression(text);
+    text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
+    return new Handlebars.SafeString(text);
+});
+Handlebars.registerHelper('bbcode', function (text) {
+    let result = XBBCODE.process({
+        text: text,
+        removeMisalignedTags: false,
+        addInLineBreaks: false
+    });
+    text = result.html.replace(/(\r\n|\n|\r)/gm, '<br>');
+    return new Handlebars.SafeString(text);
+});
