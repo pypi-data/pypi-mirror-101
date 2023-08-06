dba71cbd5d42aee10fe678d874a9940cc7f56d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:28 +0000

    Add package pygifconvt-m/1.0.0/pygifconvt_m-1.0.0.tar.gz

---
+++ packages/pygifconvt-m/pygifconvt_m-1.0.0.tar.gz/pygifconvt_m-1.0.0/pygifconvt_m/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Pygifconvt: a very serious Python library for image to gif converter
+
+"""
+__title__ = "Pygifconvt"
+__author__ = "motive"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2021 motive"
