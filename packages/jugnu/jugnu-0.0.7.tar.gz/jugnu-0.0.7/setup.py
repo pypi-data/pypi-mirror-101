d621af9431b580687679e0af96953ef0e01d544c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:36 +0000

    Add package jugnu/0.0.7/jugnu-0.0.7.tar.gz

---
+++ packages/jugnu/jugnu-0.0.7.tar.gz/jugnu-0.0.7/setup.py
@@ -0,0 +1,30 @@
+"""Setup for the jugnu package."""
+
+import setuptools
+import os
+
+
+setuptools.setup(
+    author="Ajay Kulal",
+    author_email="hackerhero@bugcrowdninja.com",
+    name='jugnu',
+    license="MIT",
+    description='jugnu is a amazing package for data handling',
+    version='v0.0.7',
+    long_description="no long decsription given",
+    url='https://github.com/bountyrecon/',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    #dependency_links=['http://github.com/bountyrecon/jugnu/tarball/master#egg=package-0.0.7'],
+    dependency_links=['https://github.com/bountyrecon/pydep/releases/tag/0.0.7'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
