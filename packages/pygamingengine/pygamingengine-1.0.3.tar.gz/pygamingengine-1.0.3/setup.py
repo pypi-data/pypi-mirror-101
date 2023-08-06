dc7b58c0118633f0a7ed1c6673d88e838845e3c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:28 +0000

    Add package pygamingengine/1.0.3/pygamingengine-1.0.3.tar.gz

---
+++ packages/pygamingengine/pygamingengine-1.0.3.tar.gz/pygamingengine-1.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+VERSION = '1.0.3'
+DESCRIPTION = 'A game engine'
+
+requires = open('requirements.txt', 'r').read().splitlines()
+
+# Setting up
+setup(
+    name="pygamingengine",
+    version=VERSION,
+    author="abhra2020smart (Abhradeep De)",
+    author_email="deabhradeep@gmail.com",
+    description=DESCRIPTION,
+    install_requires=requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
