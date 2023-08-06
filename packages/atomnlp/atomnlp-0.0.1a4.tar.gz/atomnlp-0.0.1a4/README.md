5251ce6aaab72dbde04906b80ff79e40f3d0a62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:51 +0000

    Add package atomnlp/0.0.1a4/atomnlp-0.0.1a4.tar.gz

---
+++ packages/atomnlp/atomnlp-0.0.1a4.tar.gz/atomnlp-0.0.1a4/README.md
@@ -0,0 +1,11 @@
+A toy NLP toolkit. Atom stands for **Simple and Fast**.
+
+## usage
+```bash
+pip install atomnlp
+
+# bidirectional-match based segment method, quick but weak.
+atom -seg '服务不要云服务' 
+# output ['服务', '不要', '云服务']
+```
+
---
+++ packages/atomnlp/atomnlp-0.0.1a4.tar.gz/atomnlp-0.0.1a4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="atomnlp",
+    version="0.0.1a4",
+    author="GaoangLau",
+    author_email="byteleap@gmail.com",
+    description="Atomic Natural Language Processing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GaoangLiu/atomnlp",
+    packages=setuptools.find_packages(),
+    package_data={"atomnlp":["data/dict.txt", 'data/oov.txt']},
+    install_requires=['colorlog>=4.6.1', 'tqdm>=4.56.0'],
+    entry_points={
+        'console_scripts': ['atom=atomnlp.entry:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
