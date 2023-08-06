ea41938b6a82fe0a6a427eb4cf469e77dd8a36e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:38 +0000

    Add package cloudx-naas-api/1.2.7/cloudx_naas_api-1.2.7-py3-none-any.whl

---
+++ packages/cloudx-naas-api/cloudx_naas_api-1.2.7-py3-none-any.whl/couldx_naas_api/geolocation.py
@@ -0,0 +1,12 @@
+class GeoLocation:
+    def __init__(self, hosts=None, es=None):
+        """
+        Initialize an GEO Location API       
+        """
+        
+
+    @classmethod
+    def get_location(cls, es_url):
+        "Get an GeoLocation from NaaS url"
+        
+        return ""
