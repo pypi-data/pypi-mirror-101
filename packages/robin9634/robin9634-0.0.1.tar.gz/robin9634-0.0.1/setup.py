56cdb0786c0c56040852a232afde1a303ce32c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:47 +0000

    Add package robin9634/0.0.1/robin9634-0.0.1.tar.gz

---
+++ packages/robin9634/robin9634-0.0.1.tar.gz/robin9634-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="robin9634", # Replace with your own username
+    version="0.0.1",
+    author="Robin Shim",
+    author_email="justforad9634@gmail.com",
+    description="For personal use",
+    long_description='For personal use',
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
