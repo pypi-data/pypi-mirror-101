49a6de5f457fab4d06c35e3b6789418d16e6fd9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:53 +0000

    Add package nester-JZ/0.0.1/nester_JZ-0.0.1.tar.gz

---
+++ packages/nester-JZ/nester_JZ-0.0.1.tar.gz/nester_JZ-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name         ='nester_JZ',
+    version      ='0.0.1',
+    py_modules   =['nester_JZ'],
+    author       = 'JZ33',
+    author_email = 'zhuhan96@gmail.com',
+    url          = 'https://pypi.org/',
+    description  ='A simple printer of nested lists',
+    )
