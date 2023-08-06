a80d5a4fe41b964631ed710164be2359933a22be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:51 +0000

    Add package profile-watcher/0.0.1/profile_watcher-0.0.1-py3-none-any.whl

---
+++ packages/profile-watcher/profile_watcher-0.0.1-py3-none-any.whl/watcher/watcher.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# A python library that provides update when something changes in social profiles.
+# Copyright (C) 2021 Shubhendra Kushwaha
+# @TheShubhendra shubhendrakushwaha94@gmail.com
+# This file is a part of profile-watcher <https://github.com/TheShubhendra/profile-watcher>.
+#
+# profile-watcher is a free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# profile-watcher is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with profile-watcher.  If not, see <http://www.gnu.org/licenses/>.
+from queue import Queue
+from apscheduler.schedulers.background import BackgroundScheduler
+from profiles.quora import User
+from .update import Update
+from .examiner import Examiner
+
+
+class Watcher:
+    def __init__(self):
+        self.scheduler = BackgroundScheduler()
+        self.updateQueue = Queue()
+        self.examiner = Examiner(self)
+
+    def add_quora(self, username):
+        def get_quora(username):
+            user = User(username)
+            self.updateQueue.put(Update("quora", user))
+
+        self.scheduler.add_job(get_quora, "interval", seconds=5, args=(username,))
+
+    def start(self):
+        self.scheduler.start()
+        self.examiner.start()
