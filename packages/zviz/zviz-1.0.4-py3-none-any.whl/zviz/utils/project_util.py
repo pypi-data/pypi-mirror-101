292fb7dd96ec380757d32b916c491985d1f03b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:45 +0000

    Add package zviz/1.0.4/zviz-1.0.4-py3-none-any.whl

---
+++ packages/zviz/zviz-1.0.4-py3-none-any.whl/zviz/utils/project_util.py
@@ -0,0 +1,45 @@
+def join(s=" ", li=[], empty_s=''):
+    li=[l for l in li if l !=None]
+    if li == []:
+        return empty_s
+    elif len(li) == 1:
+        return li[0]
+    else:
+        return s.join(str(l) for l in li)
+
+
+def takefirst(s):
+    if s[0] in ['<', '>', ':']: s = s[1:]
+    return str(s).split()[0]
+
+
+def printgradfn(x):
+    def printnext(t):
+        print(t, hex(id(t)))
+        if hasattr(t, 'variable'):
+            print('V', hex(id(t.variable)), t.variable.shape)
+
+        if t is not None and len(t.next_functions) != 0:
+            print(t.next_functions)
+            for _t in t.next_functions:
+                printnext(_t[0])
+
+    if x.grad_fn:
+        printnext(x.grad_fn)
+        print('')
+
+def getidshapedict(xslist):
+    Idshapes={}
+    Idshapes_nograd={}
+    for i in range(len(xslist)):
+        for numx in range(len(xslist[i])):
+            x=xslist[i][numx]
+            try:
+                shape=xslist[i][numx].shape
+            except:
+                shape='?'
+            if x.grad_fn:
+                Idshapes[hex(id(x.grad_fn))]=shape
+            else:
+                Idshapes_nograd[hex(id(x))] = shape
+    return Idshapes,Idshapes_nograd
