2cc5e1463ea99b7f7650e9c71958bfcfd7bcba62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:21 +0000

    Add package ryzom/0.6.0/ryzom-0.6.0.tar.gz

---
+++ packages/ryzom/ryzom-0.6.0.tar.gz/ryzom-0.6.0/src/ryzom_django_example/urls.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('ryzom_django_example.views')),
+]
+
+if settings.DEBUG:
+    urlpatterns += [path('bundles/', include('ryzom_django.bundle'))]
+
+if settings.CHANNELS_ENABLE:
+    urlpatterns.append(
+        path('reactive/', include('ryzom_django_channels_example.views')),
+    )
+
+if settings.CRUDLFAP_ENABLE:
+    from crudlfap import shortcuts as crudlfap
+    crudlfap.site.urlpath = 'crudlfap'
+    urlpatterns.append(crudlfap.site.urlpattern)
