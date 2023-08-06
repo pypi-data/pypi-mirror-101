25df6f53151563d0d6df1f70f80df479d07db098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:34 +0000

    Add package singular-bug-tracker/0.0.4/singular_bug_tracker-0.0.4.tar.gz

---
+++ packages/singular-bug-tracker/singular_bug_tracker-0.0.4.tar.gz/singular_bug_tracker-0.0.4/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(
+    name='singular_bug_tracker',
+    version='0.0.4',
+    author='Singular Sistemas',
+    author_email='ivan@singular.inf.br',
+    description='Bug tracker',
+    long_description='Bug tracker',
+    url='https://lucas@bitbucket.org/singular-dev/bug_tracker.git',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires=">=3.6",
+    install_requires=[
+        'django',
+    ]
+)
