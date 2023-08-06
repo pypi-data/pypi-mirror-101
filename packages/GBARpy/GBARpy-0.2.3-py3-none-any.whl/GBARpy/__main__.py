94f421ecac48a31a7fbd83a858f526357571ccae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:47 +0000

    Add package GBARpy/0.2.3/GBARpy-0.2.3-py3-none-any.whl

---
+++ packages/GBARpy/GBARpy-0.2.3-py3-none-any.whl/GBARpy/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 15:07:10 2020
+
+@author: samuel.niang@cern.ch
+"""
+
+from GBARpy.Main_window import MainWindow
+#from Main_window import MainWindow
+
+win = MainWindow()            
+win.mainloop()
+win.quit()
