f82b33d66fd78c8886ce2f8587f95d0cec89311a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:37 +0000

    Add package sqimple/0.0.1/sqimple-0.0.1-py2.py3-none-any.whl

---
+++ packages/sqimple/sqimple-0.0.1-py2.py3-none-any.whl/sqimple/server.py
@@ -0,0 +1,41 @@
+import flask
+from subprocess import run
+
+app = flask.Flask(__name__, static_url_path="")
+
+DATABASE = ":memory:"
+
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+
+@app.route("/query", methods=["POST"])
+def runquery():
+    query = flask.request.get_data(as_text=True)
+    result = run(["sqlite3", "-header", "-html", DATABASE, query], capture_output=True)
+    return flask.jsonify(
+        dict(result=result.stdout.decode(), error=result.stderr.decode())
+    )
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "sqlite_db",
+        nargs="?",
+        help="SQLite database to interact with. Defaults to in-memory db.",
+    )
+    args = parser.parse_args()
+
+    if args.sqlite_db:
+        DATABASE = args.sqlite_db
+
+    app.run(debug=True)
+
+
+if __name__ == "__main__":
+    main()
