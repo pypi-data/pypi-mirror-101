e938b37338703e70ac55ea24abd71e0254e203b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:37 +0000

    Add package tscfat/0.0.5/tscfat-0.0.5.tar.gz

---
+++ packages/tscfat/tscfat-0.0.5.tar.gz/tscfat-0.0.5/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 14:06:42 2021
+
+@author: arsii
+"""
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name = 'tscfat',  
+  package_dir = {'':'tscfat'},       
+  #packages = find_packages('tscfat',exclude=["Tests",]),  
+  packages = ['Analysis','Utils'],
+  version = '0.0.5',      
+  license='MIT',        
+  description = 'A time series co-fluctuation analysis toolbox',   
+  author = 'Arsi Ikaheimonen',                   
+  author_email = 'arsi.ikaheimonen@gmail.com',      
+  url = 'https://github.com/ArgonSilicon/tscfat',   
+  download_url = 'https://github.com/ArgonSilicon/tscfat/archive/refs/tags/0.0.3.tar.gz',
+  keywords = ['Timeseries', 'Exploratory', 'Analysis'],
+  include_package_data=True,  
+  install_requires=['nolds',
+                    'sklearn',
+                    'pytest',
+                    'numpy',
+                    'matplotlib',
+                    'pandas',
+                    'statsmodels',
+                    'tslearn',
+                    'seaborn'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Science/Research',      
+    'Topic :: Scientific/Engineering :: Medical Science Apps.', #?
+    'License :: OSI Approved :: MIT License',   #P
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
