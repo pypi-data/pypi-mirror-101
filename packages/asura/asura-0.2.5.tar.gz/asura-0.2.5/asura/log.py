4334e630c22944e652d541e928e234b48f5f0232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:24 +0000

    Add package asura/0.2.5/asura-0.2.5.tar.gz

---
+++ packages/asura/asura-0.2.5.tar.gz/asura-0.2.5/asura/log.py
@@ -0,0 +1,12 @@
+import colored
+
+class Asora:
+    DEBUG = colored.fg(107)
+    INFO = colored.fg(111)
+    WARNING = colored.fg(130)
+    ERROR = colored.fg(160)
+
+    reset = colored.attr('reset')
+
+def aprint(text: str, clr: 'Asora', auto: bool = False):
+    print(clr + text + Asora.reset, end="\r" if auto else None)
