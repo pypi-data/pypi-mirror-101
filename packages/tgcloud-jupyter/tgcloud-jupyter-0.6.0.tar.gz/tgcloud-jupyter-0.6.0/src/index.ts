9b8a30a9268a5e078394d230b458e9b586dbd277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:29 +0000

    Add package tgcloud-jupyter/0.6.0/tgcloud-jupyter-0.6.0.tar.gz

---
+++ packages/tgcloud-jupyter/tgcloud-jupyter-0.6.0.tar.gz/tgcloud-jupyter-0.6.0/src/index.ts
@@ -0,0 +1,49 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { MainAreaWidget } from '@jupyterlab/apputils';
+
+import { ILauncher } from '@jupyterlab/launcher';
+
+import { CounterWidget } from './widget';
+/**
+ * The command IDs used by the react-widget plugin.
+ */
+namespace CommandIDs {
+  export const create = 'create-react-widget';
+}
+
+/**
+ * Initialization data for the react-widget extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'react-widget',
+  autoStart: true,
+  optional: [ILauncher],
+  activate: (app: JupyterFrontEnd, launcher: ILauncher) => {
+    const { commands } = app;
+
+    const command = CommandIDs.create;
+    commands.addCommand(command, {
+      caption: 'tgCloud Cloud platform',
+      label: 'TigerGraph Cloud',
+      iconClass: 'jp-ReactIcon',
+      execute: () => {
+        const content = new CounterWidget();
+        const widget = new MainAreaWidget<CounterWidget>({ content });
+        widget.title.label = 'TigerGraph Cloud';
+        app.shell.add(widget, 'main');
+      }
+    });
+
+    if (launcher) {
+      launcher.add({
+        command
+      });
+    }
+  }
+};
+
+export default extension;
