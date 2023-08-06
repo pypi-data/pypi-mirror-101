6dc9d02949898fe226a74521fcbcb98c9404b624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:31 +0000

    Add package EelForkExcludeFiles/0.14.8/EelForkExcludeFiles-0.14.8.tar.gz

---
+++ packages/EelForkExcludeFiles/EelForkExcludeFiles-0.14.8.tar.gz/EelForkExcludeFiles-0.14.8/setup.py
@@ -0,0 +1,42 @@
+from io import open
+from setuptools import setup, find_packages
+
+with open("README.md") as read_me:
+    long_description = read_me.read()
+
+setup(
+    name="EelForkExcludeFiles",
+    version="0.14.8",
+    author="Chris Knott",
+    packages=find_packages(),
+    package_data={
+        "": ["eel.js"],
+    },
+    include_package_data=True,
+    install_requires=[
+        "bottle",
+        "bottle-websocket",
+        "future",
+        "pyparsing",
+        "whichcraft",
+    ],
+    extras_require={"jinja2": ["jinja2>=2.10"]},
+    python_requires=">=3.6",
+    description="For little HTML GUI applications, with easy Python/JS interop",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["gui", "html", "javascript", "electron"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
