04f1b44dd269564ecc1a32c92eb670c92ba76f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:20:52 +0000

    Add package pyHMT2D/1.0.1/pyHMT2D-1.0.1-py3-none-any.whl

---
+++ packages/pyHMT2D/pyHMT2D-1.0.1-py3-none-any.whl/pyHMT2D/__init__.py
@@ -0,0 +1,25 @@
+
+from .Hydraulic_Models_Data import *
+from .Calibration import *
+from .Misc import *
+
+from .__about__ import __version__
+from .__common__ import *
+
+__all__ = [
+    "Hydraulic_Models_Data_Base",
+    "Backwater_1D",
+    "SRH_2D",
+    "RAS_2D",
+    "Calibration",
+    "Misc",
+    "gVerbose",
+    "gMax_Nodes_per_Element",
+    "gMax_Elements_per_Node",
+    "__version__"
+]
+
+__version_info__ = (1, 0, 0)
+__version__ = '.'.join(map(str, __version_info__))
+
+
