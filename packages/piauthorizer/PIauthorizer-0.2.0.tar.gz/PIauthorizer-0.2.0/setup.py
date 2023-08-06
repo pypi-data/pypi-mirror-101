fd90c7b5e1203db59ec77546834967afcc49bd96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:26 +0000

    Add package piauthorizer/0.2.0/PIauthorizer-0.2.0.tar.gz

---
+++ packages/piauthorizer/PIauthorizer-0.2.0.tar.gz/PIauthorizer-0.2.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['piauthorizer',
+ 'piauthorizer.authorization',
+ 'piauthorizer.autorest',
+ 'piauthorizer.functional']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyJWT>=2.0.1,<3.0.0', 'aiohttp>=3.7.4,<4.0.0', 'fastapi>=0.6.0,<0.7.0']
+
+setup_kwargs = {
+    'name': 'piauthorizer',
+    'version': '0.2.0',
+    'description': 'A package to create a uniform authorization and autorest standard for FastAPI',
+    'long_description': None,
+    'author': 'David Berenstein',
+    'author_email': 'david.berenstein@pandoraintelligence.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
