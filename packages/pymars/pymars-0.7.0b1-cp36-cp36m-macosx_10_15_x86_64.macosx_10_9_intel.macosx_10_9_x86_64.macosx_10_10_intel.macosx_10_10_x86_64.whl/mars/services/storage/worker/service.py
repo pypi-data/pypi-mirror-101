e34cd525f68ef7ebb242a0b5a0c051e0fd1889a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:05 +0000

    Add package pymars/0.7.0b1/pymars-0.7.0b1-cp36-cp36m-macosx_10_15_x86_64.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/pymars/pymars-0.7.0b1-cp36-cp36m-macosx_10_15_x86_64.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl/mars/services/storage/worker/service.py
@@ -0,0 +1,44 @@
+# Copyright 1999-2020 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .... import oscar as mo
+from ..core import StorageManagerActor
+
+
+async def start(config: dict, address: str):
+    """
+    Start cluster service on supervisor
+    Parameters
+    ----------
+    config
+        storage service config.
+        {
+            "storage":
+                {
+                    "backends": ["plasma"],
+                    "<storage backend name>"： "<setup params>",
+                }
+        }
+    address
+        address of actor pool
+    """
+    storage_configs = config['storage']
+    backends = storage_configs.get('backends')
+    backend_config = {backend: storage_configs.get(backend)
+                      for backend in backends}
+
+    await mo.create_actor(StorageManagerActor,
+                          backend_config,
+                          uid=StorageManagerActor.default_uid(),
+                          address=address)
