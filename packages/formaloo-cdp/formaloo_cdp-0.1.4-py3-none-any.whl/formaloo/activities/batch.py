0817979894058c3ea9e1d577040905ab44d990ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:43 +0000

    Add package formaloo-cdp/0.1.4/formaloo_cdp-0.1.4-py3-none-any.whl

---
+++ packages/formaloo-cdp/formaloo_cdp-0.1.4-py3-none-any.whl/formaloo/activities/batch.py
@@ -0,0 +1,31 @@
+from formaloo import constants, client
+
+
+class ActivityBatch:
+
+    def __init__(self, activities=[]):
+        self.client = client.Client()
+        self.activities = activities
+
+    def add_activity(self, activity):
+        self.activities.append(activity)
+
+    def create(self):
+        body = self.get_body()
+
+        response = self.client.post(
+            constants.V_1_0_ACTIVITY_BATCH_ENDPOINT,
+            body=body
+        )
+
+        return response
+
+    def get_body(self):
+        body = []
+
+        for activity in self.activities:
+            body.append(
+                activity.get_body()
+            )
+
+        return body
