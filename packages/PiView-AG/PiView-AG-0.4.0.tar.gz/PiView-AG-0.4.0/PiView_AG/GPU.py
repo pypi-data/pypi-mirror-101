1061e66b85970c305ac25c86c720fc608dc9f546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:03 +0000

    Add package PiView-AG/0.4.0/PiView-AG-0.4.0.tar.gz

---
+++ packages/PiView-AG/PiView-AG-0.4.0.tar.gz/PiView-AG-0.4.0/PiView_AG/GPU.py
@@ -0,0 +1,19 @@
+import subprocess
+
+
+class GPU:
+    def temperature(self):
+        """
+        Requests the GPU temperature from the thermal zone details
+
+        :return string: GPU temperature to 2DP
+        """
+        # Extract CPU temp
+        try:
+            temp = subprocess.check_output(
+                ['cat', '/sys/class/thermal/thermal_zone0/temp'])
+            temp = float(temp) / 1000
+        except:
+            temp = 0.0
+        temp = round(temp, 2)
+        return temp
