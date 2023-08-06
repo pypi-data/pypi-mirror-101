2b8a656fcd992c0d0a2e7deaaeb802b107380c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:27 +0000

    Add package sqla_ext/0.0.5/sqla_ext-0.0.5.tar.gz

---
+++ packages/sqla_ext/sqla_ext-0.0.5.tar.gz/sqla_ext-0.0.5/src/sqla_ext/inspect/_to_schema_name.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from functools import lru_cache
+from typing import Optional
+
+from sqla_ext.inspect._to_core_table import to_core_table
+from sqla_ext.protocols import TableCoercable
+
+
+@lru_cache()
+def to_schema_name(entity: TableCoercable) -> Optional[str]:
+    r"""Get the schema of a sqlalchemy table-like entity
+
+    :param entity: A sqlalchemy :class:`Table`, :class:`DeclarativeBase` or :class:`Mapper`
+
+    :return: :class:`Optional[str]`
+    """
+
+    schema_name = to_core_table(entity).schema
+    return str(schema_name) if schema_name is not None else None
---
+++ packages/sqla_ext/sqla_ext-0.0.5.tar.gz/sqla_ext-0.0.5/src/sqla_ext/types/postgresql/__init__.py
@@ -0,0 +1,3 @@
+from .citext import CITEXT
+
+__all__ = ["CITEXT"]
