7cc7cb3db809b50de57bd94e9dc17febf68eb944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:15 +0000

    Add package tkinterweb/3.2.0/tkinterweb-3.2.0.tar.gz

---
+++ packages/tkinterweb/tkinterweb-3.2.0.tar.gz/tkinterweb-3.2.0/tkinterweb/__init__.py
@@ -0,0 +1,45 @@
+"""
+TkinterWeb v3.2
+
+This is a wrapper for the Tkhtml3 widget from http://tkhtml.tcl.tk/tkhtml.html, which displays styled HTML documents in Tkinter.
+"""
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+from htmlwidgets import HtmlFrame, HtmlLabel
+from bindings import TkinterWeb
+    
+__title__ = 'TkinterWeb'
+__version__ = '3.2'
+__all__ = ['HtmlFrame', 'HtmlLabel', 'TkinterWeb', 'Demo']
+
+class Demo():
+    "TkinterWeb Demo"
+    
+    def __init__(self):
+        
+        try:
+            import tkinter as tk
+        except ImportError:
+            import Tkinter as tk
+        
+        self.root = root = tk.Tk()
+        
+        self.frame = frame = HtmlFrame(root)
+        
+        frame.on_title_change(self.change_title)
+        frame.on_link_click(self.load_new_page)
+        frame.load_website("http://tkhtml.tcl.tk")
+        
+        frame.pack()
+        root.mainloop()
+
+    def change_title(self, title):
+        self.root.title(title)
+
+    def load_new_page(self, url):
+        self.frame.load_website(url)
+        
+if __name__ == "__main__":
+    Demo()
