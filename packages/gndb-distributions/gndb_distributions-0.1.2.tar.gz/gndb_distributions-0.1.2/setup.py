9a51f68567104c6e48ccad8482432b3d32c771b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:06 +0000

    Add package gndb-distributions/0.1.2/gndb_distributions-0.1.2.tar.gz

---
+++ packages/gndb-distributions/gndb_distributions-0.1.2.tar.gz/gndb_distributions-0.1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gndb_distributions',
+      version='0.1.2',
+      description='Gaussian and Binomial gndb_distributions',
+      packages=['gndb_distributions'],
+      author='Ufuoma Ubor',
+      author_email='foskynew@gmail.com',
+      zip_safe=False)
