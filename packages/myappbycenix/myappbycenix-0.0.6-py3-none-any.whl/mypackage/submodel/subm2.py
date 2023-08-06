7e7ade908eec766feeaebce0f9334e10518260f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:23 +0000

    Add package myappbycenix/0.0.6/myappbycenix-0.0.6-py3-none-any.whl

---
+++ packages/myappbycenix/myappbycenix-0.0.6-py3-none-any.whl/mypackage/submodel/subm2.py
@@ -0,0 +1,9 @@
+#这个地方相对目录引用只能这样用，在main里面会出错
+#with open('./ssubmodel/useful.txt','r') as f:#./submodel/ssubmodel
+try:
+    with open('./ssubmodel/useful.txt','r') as f:
+        info=f.readline()
+except:info='cant read txt'
+
+if __name__ == '__main__':
+    print('info',info)
